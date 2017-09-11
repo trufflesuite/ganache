e973b43a277389dea8ce1cb7b6be4e9cefbe148a
jakub-wojciechowski <jakub@alice.si>
2017-09-11 14:48:30 +0200

    Mine a block exactly at the given timestamp

--- test/time_adjust.js
+++ test/time_adjust.js
@@ -64,4 +64,21 @@ describe('Time adjustment', function() {
       })
     })
   })
+
+  it('should mine a block at the given timestamp', function(done) {
+      // Adjust time
+      var expectedMinedTimestamp = 1000000;
+      send("evm_mineAtTimestamp", [expectedMinedTimestamp], function(err, result) {
+          if (err) return done(err);
+
+          web3.eth.getBlock('latest', function(err, block){
+              if(err) return done(err)
+              assert(block.timestamp == expectedMinedTimestamp)
+              done()
+          })
+      })
+  })
+
+
+
 })
