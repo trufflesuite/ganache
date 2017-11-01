417fd26040b2f17ebe51262c1a30e21078c59ed4
Ben Burns <benjamin.c.burns@gmail.com>
2017-11-01 19:52:11 -0400

    apparently contract.new calls its callback twice :-/

--- test/vm.js
+++ test/vm.js
@@ -52,15 +52,17 @@ describe("revert opcode", function() {
   it("should return a transaction receipt with status 0 on REVERT", function(done) {
     var revertCode = testContext.revertContract.binary;
     var revertAbi = JSON.parse(testContext.revertContract.abi);
+    var callCount = 0;
 
     var RevertContract = web3.eth.contract(revertAbi);
     RevertContract._code = revertCode;
     RevertContract.new({ data: revertCode, from: testContext.accounts[0], gas: 3141592 }, function (err, instance) {
+      callCount++;
       if (err) {
         return done(err);
       }
 
-      var testCall = function(instance) {
+      if (instance.address) {
         instance.alwaysReverts(5, { from: testContext.accounts[0] }, function(err, result) {
           assert(err, "Expected error result not returned.");
           var txHash = err.hashes[0];
@@ -76,22 +78,6 @@ describe("revert opcode", function() {
           });
         });
       }
-
-      if (!instance.address) {
-        web3.eth.getTransactionReceipt(instance.transactionHash, function(err, receipt) {
-          if (err) {
-            return done(err);
-          }
-
-          assert.notEqual(receipt, null, "Transaction receipt shouldn't be null");
-          assert.notEqual(receipt.contractAddress, null, "Transaction did not create a contract");
-            assert.equal(receipt.status, 1, "Successful transactions should have a status of 1.");
-          testCall(RevertContract.at(receipt.contractAddress));
-        });
-      } else {
-        testCall(instance);
-      }
-
     });
   });
 });
