f611d5f377dcabe2d7ba4fc2a45fd116c36d9692
William Hua <william@attente.ca>
2017-11-27 12:50:29 -0500

    Test for leading zeros in JSON-RPC response
    From https://github.com/ethereum/wiki/wiki/JSON-RPC#hex-value-encoding:
    
    "When encoding QUANTITIES (integers, numbers): encode as hex, prefix
    with '0x', the most compact representation (slight exception: zero
    should be represented as '0x0'). Examples:"
    
    "WRONG: 0x0400 (no leading zeroes allowed)"

--- test/hex.js
+++ test/hex.js
@@ -1,4 +1,6 @@
 var assert = require("assert");
+var Web3 = require("web3");
+var TestRPC = require("../index.js");
 var to = require("../lib/utils/to.js");
 
 describe("to.hexWithoutLeadingZeroes", function() {
@@ -25,5 +27,82 @@ describe("to.hexWithoutLeadingZeroes", function() {
   it("should print '0x20' for input '0x0020'", function(done) {
     assert.equal(to.hexWithoutLeadingZeroes("0x0020"), "0x20");
     done();
+  });
+});
+
+function noLeadingZeros(result) {
+  if (typeof result === "string") {
+    if (/^0x/.test(result)) {
+      assert.equal(result, to.hexWithoutLeadingZeroes(result));
+    }
+  } else if (typeof result === "object") {
+    for (var key in result) {
+      if (result.hasOwnProperty(key)) {
+        noLeadingZeros(result[key]);
+      }
+    }
+  }
+}
+
+describe("JSON-RPC Response", function() {
+  var web3 = new Web3();
+  var provider = TestRPC.provider();
+  web3.setProvider(provider);
+
+  var accounts;
+  before(function(done) {
+    web3.eth.getAccounts(function(err, accs) {
+      if (err) return done(err);
+      accounts = accs;
+      done();
+    });
+  });
+
+  it("should not have leading zeros in hex strings", function(done) {
+    var request = {
+      "jsonrpc": "2.0",
+      "method": "eth_getTransactionCount",
+      "params": [
+        accounts[0],
+        "pending"
+      ],
+      "id": 1
+    };
+
+    provider.sendAsync(request, function(err, result) {
+      noLeadingZeros(result);
+
+      request = {
+        "jsonrpc": "2.0",
+        "method": "eth_sendTransaction",
+        "params": [
+          {
+            "from": accounts[0],
+            "to": accounts[1],
+            "value": "0x100000000"
+          }
+        ],
+        "id": 2
+      };
+
+      provider.sendAsync(request, function(err, result) {
+        noLeadingZeros(result);
+
+        request = {
+          "jsonrpc": "2.0",
+          "method": "eth_getTransactionCount",
+          "params": [
+            accounts[0],
+            "pending"
+          ],
+          "id": 3
+        };
+
+        provider.sendAsync(request, function(err, result) {
+          noLeadingZeros(result);
+          done();
+        });
+      });
+    });
   });
 });
