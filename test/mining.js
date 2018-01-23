d4b8c7e36a10ed20e951a62f532affa6b0189bbe
Ben Burns <benjamin.c.burns@gmail.com>
2018-01-24 09:06:10 +1300

    Change new mining test to use assert

--- test/mining.js
+++ test/mining.js
@@ -112,7 +112,7 @@ describe("Mining", function() {
         id: new Date().getTime()
       }, function(err, result) {
         if (err) return reject(err);
-        if(result.result !== '0x0') return reject(new Error("evm_mine should return result = '0x0':" + JSON.stringify(result)))
+        assert.notDeepEqual(result.result, "0x0", "evm_mine should return result = '0x0'");
         accept(result);
       })
     });
