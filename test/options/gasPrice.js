95dbb5ef0dd992030bee415716dc0e6ff3e1e0bf
Ben Burns <benjamin.c.burns@gmail.com>
2018-08-13 23:33:33 +0000

    Test to gaurd zero gas price config
    closes #120

--- test/options/gasPrice.js
+++ test/options/gasPrice.js
@@ -8,42 +8,72 @@ const compileAndDeploy = require ('../helpers/contracts').compileAndDeploy
 
 const mnemonic = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
 
-describe('options:gasLimit', function() {
-  let options = {mnemonic}
-  let provider = null
-  let web3 = null
-  let accounts = []
-  let contractArtifact = {}
-  let instance = null
+function setUp(options = {mnemonic}, contractName = 'Example') {
+  let context = {
+    options: options,
+    provider: null,
+    web3: null,
+    accounts: [],
+    contractArtifact: {},
+    instance: null
+  }
 
   before ('setup web3', async function() {
-    provider = new Ganache.provider(options)
-    web3 = new Web3(provider)
+    context.provider = new Ganache.provider(context.options)
+    context.web3 = new Web3(context.provider)
   })
 
   before('get accounts', async function() {
-    accounts = await web3.eth.getAccounts()
+    context.accounts = await context.web3.eth.getAccounts()
   })
 
   before("compile source", async function() {
     this.timeout(10000)
-    let contractName = 'Example'
-    contractArtifact = await compileAndDeploy(path.join(__dirname, '..', `${contractName}.sol`), contractName, web3)
-    instance = contractArtifact.instance
+    context.contractArtifact = await compileAndDeploy(path.join(__dirname, '..', `${contractName}.sol`), contractName, context.web3)
+    context.instance = context.contractArtifact.instance
   })
 
-  it('should respect the default gasPrice', async function() {
-    let assignedGasPrice = provider.engine.manager.state.gasPriceVal;
+  return context
+}
 
-    let receipt = await instance.methods.setValue('0x10').send({from: accounts[0], gas: 3141592})
+describe('options:gasPrice', function() {
+  describe('default gasPrice', function() {
 
-    let transactionHash = receipt.transactionHash;
-    let tx = await web3.eth.getTransaction(transactionHash)
-    let gasPrice = tx.gasPrice
+    let context = setUp()
 
-    assert.deepEqual(to.hex(gasPrice), to.hex(assignedGasPrice))
+    it('should respect the default gasPrice', async function() {
+      let assignedGasPrice = context.provider.engine.manager.state.gasPriceVal;
+
+      let receipt = await context.instance.methods.setValue('0x10').send({from: context.accounts[0], gas: 3141592})
+
+      let transactionHash = receipt.transactionHash;
+      let tx = await context.web3.eth.getTransaction(transactionHash)
+      let gasPrice = tx.gasPrice
+
+      assert.deepEqual(to.hex(gasPrice), to.hex(assignedGasPrice))
+    })
+
   })
 
+  describe('zero gasPrice', function() {
+
+    let context = setUp({mnemonic, gasPrice: 0})
+
+    it('should be possible to set a zero gas price', async function() {
+      let assignedGasPrice = context.provider.engine.manager.state.gasPriceVal;
+
+      assert.deepEqual(assignedGasPrice, '0x0')
+
+      let receipt = await context.instance.methods.setValue('0x10').send({from: context.accounts[0], gas: 3141592})
+
+      let transactionHash = receipt.transactionHash;
+      let tx = await context.web3.eth.getTransaction(transactionHash)
+      let gasPrice = tx.gasPrice
+
+      assert.deepEqual(to.hex(gasPrice), to.hex(assignedGasPrice))
+    })
+
+  })
 });
 
 
