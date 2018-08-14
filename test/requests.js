c95b9a8ea941089f574e1951ec2d6a90d38f59c5
Richard Meissner <msc.meissner@gmail.com>
2018-08-14 10:13:26 +0200

    Add eth_signTypedData

--- test/requests.js
+++ test/requests.js
@@ -382,6 +382,59 @@ var tests = function(web3) {
 
   });
 
+  describe("eth_signTypedData", function() {
+    var accounts;
+    var signingWeb3;
+
+    // Account based on https://github.com/ethereum/EIPs/blob/master/assets/eip-712/Example.js
+    var acc = {
+      balance: "0x00",
+      secretKey: web3.utils.sha3('cow')
+    };
+
+    // Load account.
+    before(function( done ){
+      signingWeb3 = new Web3();
+      signingWeb3.setProvider(Ganache.provider({
+        accounts: [ acc ]
+      }));
+      signingWeb3.eth.getAccounts(function(err, accs) {
+        if (err) return done(err);
+        accounts = accs.map(function(val) {
+          return val.toLowerCase();
+        });
+        done();
+      });
+    });
+
+    it("should produce a signature whose signer can be recovered", function(done) {
+      var typedData = {"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Person":[{"name":"name","type":"string"},{"name":"wallet","type":"address"}],"Mail":[{"name":"from","type":"Person"},{"name":"to","type":"Person"},{"name":"contents","type":"string"}]},"primaryType":"Mail","domain":{"name":"Ether Mail","version":"1","chainId":1,"verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"},"message":{"from":{"name":"Cow","wallet":"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"},"to":{"name":"Bob","wallet":"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"},"contents":"Hello, Bob!"}}
+  	  var msg = utils.toBuffer("asparagus");
+      var msgHash = utils.hashPersonalMessage(msg);
+
+      signingWeb3.currentProvider.sendAsync({
+        jsonrpc: "2.0", 
+        method: "eth_signTypedData",
+        params: [accounts[0], typedData],
+        id: new Date().getTime()
+      }, function(err, response) {
+        if (err) { 
+          return done(err);
+        }
+        console.log(response);
+        assert.equal(response.result, "0x4355c47d63924e8a72e509b65029052eb6c299d53a04e167c5775fd466751c9d07299936d304c153f6443dfa05f40ff007d72911b6f72307f996231605b915621c");
+        done();
+      });
+  	});
+
+    after("shutdown", function(done) {
+      let provider = signingWeb3._provider;
+      signingWeb3.setProvider()
+      provider.close(done)
+    });
+
+  });
+
   describe('eth_sendRawTransaction', () => {
 
     it("should fail with bad nonce (too low)", function(done) {
@@ -1278,7 +1331,7 @@ describe("WebSockets Server:", function(done) {
     });
     server.listen(port, function(err) {
       var provider = new Web3WsProvider("ws://localhost:" + port);
-  var Web3 = require('web3');
+      var Web3 = require('web3');
       web3.setProvider(provider);
       done();
     });
