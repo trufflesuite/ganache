a1e1cfafd82fdf85ea01317ee44e1df0fabc28b7
Chris C <chris@ccwebdevelopment.com>
2018-11-03 15:14:07 -0400

    Confirmed that Geth and Parity ignores the block gas limit when running a pure/view function

--- test/blockgaslimit.js
+++ test/blockgaslimit.js
@@ -17,16 +17,37 @@ function setUp(options = { mnemonic }, contractName = "BlockGasLimit") {
   };
 
   before("setup web3", async function() {
-    // eslint-disable-next-line new-cap
-    context.provider = new Ganache.provider(context.options);
+    const TARGET = "ganache";
+
+    // Ganache provider
+    if (TARGET === "ganache") {
+      // eslint-disable-next-line new-cap
+      context.provider = new Ganache.provider(context.options);
+      context.options.blockTime = 2000;
+      context.web3 = new Web3(context.provider);
+    }
 
     // Enable Geth provider
-    // context.provider = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    if (TARGET === "geth") {
+      context.options.blockTime = 2000;
+      context.provider = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+      context.web3 = new Web3(context.provider);
+    }
 
     // Parity setup
+    if (TARGET === "parity") {
+      // const address = "0x268918bf9b500219d2823216fdae375fa5e4c3c6";
+      // const password = "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0df";
 
-    context.options.blockTime = 2000;
-    context.web3 = new Web3(context.provider);
+      context.options.blockTime = 2000;
+      context.provider = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+      context.web3 = new Web3(context.provider);
+
+      const address = "0x00a329c0648769A73afAc7F9381E08FB43dBEA72";
+      await context.web3.eth.personal.unlockAccount(address, "", null);
+      // const accounts = await context.web3.eth.personal.getAccounts();
+      // console.log(accounts[0]);
+    }
   });
 
   before("compile source", async function() {
@@ -76,10 +97,20 @@ describe("Exceeding block gas limitations", function() {
 
   // Enable if running a Geth or Parity node
   it.skip("GETH/PARITY ONLY: when calling a 'pure' function should generate an error", async function() {
-    const iterations = 10 ** 0;
-    const clientGasLimit = 21780;
-
-    const status = await context.instance.methods.pureExpensiveOperation(iterations).call({ gas: clientGasLimit });
+    const status = await context.instance.methods.pureExpensiveOperation(iterations).call({
+      gas: clientGasLimit
+    });
     assert.strictEqual(status, true);
+  });
+
+  // Enable if running a Geth or Parity node
+  it.skip("GETH/PARITY ONLY: when calling a 'view' function should generate an error", async function() {
+    try {
+      // Attempt to run an expensive view function
+      await context.instance.methods.expensiveOperation(iterations).call({ gas: clientGasLimit });
+      assert.fail("Expecting a block gas limit error when executing a expensive 'view' function");
+    } catch (error) {
+      assert.strictEqual(error.message, "Expecting a block gas limit error when executing a expensive 'view' function");
+    }
   });
 });
