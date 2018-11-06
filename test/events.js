be0434751ff71344a064196647381a3b30dfc929
Erin Shaben <eshaben@icloud.com>
2018-11-06 16:07:08 -0700

    clean up test

--- test/events.js
+++ test/events.js
@@ -160,6 +160,7 @@ var tests = function(web3, EventTest) {
         });
     });
 
+    // NOTE! This test relies on the events triggered in the tests above.
     it("should return logs with correctly formatted logIndex and transactionIndex", function(done) {
       var provider = web3.currentProvider;
 
@@ -175,8 +176,7 @@ var tests = function(web3, EventTest) {
                 "0xc54307031d9aa93e0568c363be84a9400dce343fef6a2851d55662a6af1a29da",
                 "0x0000000000000000000000000000000000000000000000000000000000000001",
                 "0x0000000000000000000000000000000000000000000000000000000000000006"
-              ],
-              blockHash: "0xfe9ad4215b4674633b7ff33fec583e49316be2aa6f211c976e6685e7888de695"
+              ]
             }
           ],
           id: new Date().getTime()
@@ -189,9 +189,9 @@ var tests = function(web3, EventTest) {
           const transactionIndex = result.result[0].transactionIndex;
           assert.strictEqual(logIndex, "0x0");
           assert.strictEqual(transactionIndex, "0x0");
+          done();
         }
       );
-      done();
     });
 
     it("always returns a change for every new block subscription when instamining", function(done) {
