9335d3a63a503ba76793d7921f709a1135f2cd53
Chris C <chris@ccwebdevelopment.com>
2018-11-08 16:50:29 -0500

    Destructured accounts and contract instance

--- test/solidity_dynamic_strings.js
+++ test/solidity_dynamic_strings.js
@@ -4,19 +4,28 @@ const pretest = require("./helpers/pretest_setup");
 describe("Solidity Strings", function() {
   describe("modifying public strings", function() {
     const contractName = "DynamicStringLength";
-    const context = pretest.setup(contractName);
+    const services = pretest.setup(contractName);
 
     it("replacing a long string with a short string", async function() {
+      /**
+       * Enable access to:
+       * accounts - randomly generated test accounts
+       * instance - contract instance
+       * provider - Ganache, Geth or Parity
+       * web3 - web3 interface
+       */
+      const { accounts, instance } = services;
+
       const gas = 500000;
 
       const text = "1234567890".repeat(13);
-      await context.instance.methods.set(text).send({ from: context.accounts[0], gas });
-      let response = await context.instance.methods.testString().call();
+      await instance.methods.set(text).send({ from: accounts[0], gas });
+      let response = await instance.methods.testString().call();
       assert.strictEqual(response, text);
 
       const text2 = "123";
-      await context.instance.methods.set(text2).send({ from: context.accounts[0], gas });
-      response = await context.instance.methods.testString().call();
+      await instance.methods.set(text2).send({ from: accounts[0], gas });
+      response = await instance.methods.testString().call();
       assert.strictEqual(response, text2);
     });
   });
