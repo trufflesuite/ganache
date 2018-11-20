631e236ab7d0d64d37d4f182b49808658e2c146f
Chris C <chris@ccwebdevelopment.com>
2018-11-20 15:29:18 -0500

    Added error checking to get unclec count by hash reference

--- test/eth_getUncles.js
+++ test/eth_getUncles.js
@@ -8,20 +8,35 @@ describe("Uncle support", function() {
 
     it("by hash", async function() {
       const { web3 } = services;
-      const { hash } = await web3.eth.getBlock("latest");
+
+      // Validate a good hash lookup
+      const { hash, uncles } = await web3.eth.getBlock("latest");
       const method = "eth_getUncleCountByBlockHash";
       const params = [hash];
       const { result } = await rpcSend(method, params, web3);
       assert.notStrictEqual(result, "0x0");
+      assert.strictEqual(result, uncles.length);
+
+      // Validate a bad hash lookup
+      try {
+        const method = "eth_getUncleCountByBlockHash";
+        const invalidHash = `0x${"f".repeat(64)}`;
+        const params = [invalidHash];
+        await rpcSend(method, params, web3);
+      } catch (error) {
+        const expectedErrorMessage = "Unknown block number";
+        assert.strictEqual(error.message, expectedErrorMessage);
+      }
     });
 
     it("by block number", async function() {
       const { web3 } = services;
-      const { number } = await web3.eth.getBlock("latest");
+      const { number, uncles } = await web3.eth.getBlock("latest");
       const method = "eth_getUncleCountByBlockNumber";
       const params = [number];
       const { result } = await rpcSend(method, params, web3);
       assert.notStrictEqual(result, "0x0");
+      assert.strictEqual(result, uncles.length);
     });
   });
 });
