a0a9b913c43f426d156b31f2d4e304abd93b460a
komninoschat <komninoschat@gmail.com>
2018-11-25 13:44:00 +0200

    Add Typescript typings

---
+++ index.d.ts
@@ -0,0 +1,44 @@
+declare module 'ganache-core' {
+  import {Provider as Web3Provider} from 'web3/providers';
+  import * as http from 'http';
+
+  namespace Ganache {
+    type ProviderArgs = Partial<{
+      accounts: {
+        balance: string,
+        secretKey?: string,
+      }[]
+      debug: boolean,
+      logger: {
+        log: (message?: any, ...optionalParams: any[]) => void
+      },
+      mnemonic: string,
+      port: number,
+      seed: string,
+      default_balance_ether: number,
+      total_accounts: number,
+      fork: string|Web3Provider
+      fork_block_number: string|number
+      network_id: number,
+      time: Date,
+      locked: boolean,
+      unlocked_accounts: (string|number)[],
+      db_path: string,
+      db: object,
+      ws: boolean,
+      vmErrorsOnRPCResponse: boolean,
+      hdPath: string,
+      allowUnlimitedContractSize: boolean,
+      gasPrice: string, // 2 gwei
+    }>;
+
+    type ServerArgs = Partial<ProviderArgs & {
+      keepAliveTimeout: number,
+    }>
+
+    export function provider(obj: ProviderArgs): Web3Provider;
+    export function server(obj: ServerArgs): http.Server;
+  }
+
+  export = Ganache
+}
