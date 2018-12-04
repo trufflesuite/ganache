c15e1a6313f64656d8400dd4ce8002a8188fbe41
Nicholas J. Paterno <cashlion@protonmail.com>
2018-12-03 19:21:24 -0500

    WIP added tests and fix for eth_subscribe/unsubscribe in httpServer.js

--- test/subscriptions.js
+++ test/subscriptions.js
@@ -1,22 +1,25 @@
-// const assert = require("assert");
-const Web3WsProvider = require("web3-providers-ws");
-const Ganache = require(process.env.TEST_BUILD
-  ? "../build/ganache.core." + process.env.TEST_BUILD + ".js"
-  : "../index.js");
-const pify = require("pify");
+const assert = require("assert");
+// const Ganache = require(process.env.TEST_BUILD
+//   ? "../build/ganache.core." + process.env.TEST_BUILD + ".js"
+//   : "../index.js");
+// const pify = require("pify");
 // const { readFileSync } = require("fs");
-const { compileAndDeploy } = require("./helpers/contracts");
-const CONTRACTNAME = "Example";
+// const { compileAndDeploy } = require("./helpers/contracts");
+// const CONTRACTNAME = "Example";
+const PORT = 8545;
+const HTTPADDRESS = "http://127.0.0.1:" + PORT;
+// const WSADDRESS = "ws://127.0.0.1:" + PORT;
+// const to = require("../lib/utils/to");
 
 // const getJSON = function(relativePath) {
 //   return JSON.parse(readFileSync(`${__dirname}/${relativePath}`).toString());
 // };
 
-const tests = function(web3) {
-  // let accounts;
+const tests = function(web3, newWeb3) {
+  let accounts;
 
   before("create and fund personal account", async function() {
-    // accounts = await web3.eth.getAccounts();
+    accounts = await web3.eth.getAccounts();
     // accounts = accounts.map(function(val) {
     //   return val.toLowerCase();
     // });
@@ -27,55 +30,77 @@ const tests = function(web3) {
     // const bytecode = getJSON("subscriptions_bytecode.json")[0];
 
     it("should not crash", async function() {
-      const ctx = await compileAndDeploy(`${__dirname}/${CONTRACTNAME}.sol`, CONTRACTNAME, web3);
+      // var exampleSocket = new WebSocket(WSADDRESS, "data");
+      // const ctx = await compileAndDeploy(`${__dirname}/${CONTRACTNAME}.sol`, CONTRACTNAME, web3);
       // const block = await web3.eth.getBlock("latest", false);
       // const gasLimit = block.gasLimit;
 
-      let value = await ctx.instance.methods.value().call();
-      console.log(value);
+      // let value = await ctx.instance.methods.value().call();
+      // console.log(value);
 
-      // const subscription = await web3.eth.subscribe("newBlockHeaders", function(error, result) {
-      //   if (!error) {
-      //     console.log(result);
-      //   }
-      // });
+      // const subscription = await newWeb3.eth.subscribe("pendingTransactions");
+      // const unsubscription = await subscription.unsubscribe();
+      // const block = await newWeb3.eth.getBlock("latest", false);
+      console.log(accounts[0]);
+      console.log(1);
+      web3.currentProvider.send(
+        {
+          jsonrpc: "2.0",
+          method: "eth_subscribe",
+          params: ["newHeads", {}],
+          id: new Date().getTime()
+        },
+        function name(err, result) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(result);
+          assert(!!result);
+          try {
+            web3.currentProvider.send(
+              {
+                jsonrpc: "2.0",
+                method: "eth_sendTransaction",
+                params: { from: accounts[0], value: "0x1" },
+                id: new Date().getTime()
+              },
+              function name(err, res) {
+                console.log(err);
+                console.log(res);
+                assert(!!res);
+              }
+            );
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      );
     });
     // contract.methods.buyTokens(accounts[0]).send({from: accounts[0], value: web3.utils.toWei('0.00005', 'ether')});
   });
 };
 
-const logger = {
-  log: function(message) {
-    // console.log(message);
-  }
-};
+// const logger = {
+//   log: function(message) {
+//     // console.log(message);
+//   }
+// };
 
-describe("WebSockets Server:", function() {
+describe.skip("WebSockets Server:", function() {
   const Web3 = require("web3");
-  const web3 = new Web3();
-  const port = 12345;
-  let server;
+  const web3 = new Web3(new Web3.providers.HttpProvider(HTTPADDRESS));
+  // const newWeb3 = new Web3(new Web3.providers.WebsocketProvider(WSADDRESS));
 
-  before("Initialize Ganache server", async function() {
-    server = Ganache.server({
-      logger: logger,
-      seed: "1337",
-      verbose: true
-      // so that the runtime errors on call test passes
-    });
-    await pify(server.listen)(port);
-    const provider = new Web3WsProvider("ws://localhost:" + port);
-    web3.setProvider(provider);
-  });
+  console.log(web3.currentProvider);
 
-  tests(web3);
+  tests(web3, null);
 
   after("Shutdown server", async function() {
     let provider = web3._provider;
     web3.setProvider();
     if (provider) {
-      provider.connection.close();
-      await pify(server.close)();
+      // provider.connection.close();
+      // await pify(server.close)();
     }
   });
 });
