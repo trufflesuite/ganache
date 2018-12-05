9340a5a674615d87f3358257e5806c803bcf7be1
Chris Cowell <chris@trufflesuite.com>
2018-12-05 15:24:09 -0500

    Refactored `callbacks` (required linting rule override) Auto increment ids for `send()` Added hexadecimal uncle index support Removed a duplicate file

--- test/eth_getUncles.js
+++ test/eth_getUncles.js
@@ -1,20 +1,23 @@
 const assert = require("assert");
 const Web3 = require("web3");
-const { provider } = require("../index");
-const { rpcSend } = require("./helpers/rpc");
+var Ganache = require(process.env.TEST_BUILD
+  ? "../build/ganache.core." + process.env.TEST_BUILD + ".js"
+  : "../index.js");
+const { send } = require("./helpers/rpc");
 
 describe("Uncle support", function() {
   describe("checking uncle id through a forked blockchain", function() {
-    // Set up forking from mainnet
+    // Set up a mainnet fork
     const forkedWeb3 = new Web3(
-      provider({
-        fork: "https://mainnet.infura.io"
+      Ganache.provider({
+        fork: "https://mainnet.infura.io/v3/f6194ba837b640bba3db3d29cc658b07"
       })
     );
 
-    // Block numbers on mainnet containing uncles
+    // Block numbers on mainnet containing uncle(s)
     const oneUncleBlockNumber = 6790228;
     const twoUncleBlockNumber = 6795996;
+
     const validSingleUncleId = {
       position0: "0x7df1b5ba9ec1ec40ce7c3a288d5a207250cb6281471dc5265cf12ecb1acf254e"
     };
@@ -23,87 +26,177 @@ describe("Uncle support", function() {
       position1: "0xa91b05c06b58bd7ce5015c52a51e7d49de2b892c617fc5da395e88f32bcaf152"
     };
 
-    it("should validate a block with a SINGLE uncle id by hash or block number reference", async function() {
+    let forkedBlocks = {};
+
+    before("Retrieve forked blocks", async function() {
+      this.timeout(20000);
+
+      const [oneUncleBlock, twoUncleBlock] = await Promise.all([
+        forkedWeb3.eth.getBlock(oneUncleBlockNumber),
+        forkedWeb3.eth.getBlock(twoUncleBlockNumber)
+      ]);
+
+      // Precheck blocks containing uncle(s)
+      assert.strictEqual(oneUncleBlock.uncles[0], validSingleUncleId.position0);
+      assert.strictEqual(twoUncleBlock.uncles[0], validDoubleUncleIds.position0);
+      assert.strictEqual(twoUncleBlock.uncles[1], validDoubleUncleIds.position1);
+
+      forkedBlocks = Object.assign(forkedBlocks, {
+        oneUncleBlock,
+        twoUncleBlock
+      });
+    });
+
+    it("should validate a block with a ONE uncle id by block number reference", async function() {
       this.timeout(10000);
-      // Directly validate a single uncle id from a know block number on Mainnet
-      let testBlock = await forkedWeb3.eth.getBlock(oneUncleBlockNumber);
-      assert.strictEqual(testBlock.uncles[0], validSingleUncleId.position0);
+      const method = "eth_getUncleByBlockNumberAndIndex";
 
-      // Validate single uncle id using Ganache rpc methods
-      let method = "eth_getUncleByBlockNumberAndIndex";
+      // Validate single uncle id at position 0
       let params = [oneUncleBlockNumber, 0];
-      let { result } = await rpcSend(method, params, forkedWeb3);
+      let { result } = await send(method, params, forkedWeb3);
       assert.strictEqual(result, validSingleUncleId.position0);
 
-      method = "eth_getUncleByBlockHashAndIndex";
-      params = [testBlock.hash, 0];
-      ({ result } = await rpcSend(method, params, forkedWeb3));
+      // Validate single uncle id at position 0 (HEXADECIMAL INDEX)
+      params = [oneUncleBlockNumber, "0x0"];
+      ({ result } = await send(method, params, forkedWeb3));
+      assert.strictEqual(result, validSingleUncleId.position0);
+    });
+
+    it("should validate a block with a ONE uncle id by hash reference", async function() {
+      this.timeout(10000);
+      const { oneUncleBlock } = forkedBlocks;
+      const method = "eth_getUncleByBlockHashAndIndex";
+
+      let params = [oneUncleBlock.hash, 0];
+      let { result } = await send(method, params, forkedWeb3);
       assert.strictEqual(result, validSingleUncleId.position0);
 
-      // Access index greater than position 1 by block hash
-      try {
-        method = "eth_getUncleByBlockHashAndIndex";
-        params = [testBlock.hash, 2];
-        ({ result } = await rpcSend(method, params, forkedWeb3));
-        assert.fail("Should not be able to access an uncle index greater than 1");
-      } catch (error) {
-        assert.strictEqual(error.message, "Uncle array index must be 0 or 1");
-      }
+      // Validate single uncle id (HEXADECIMAL index) at position 0 using block hash reference
+      params = [oneUncleBlock.hash, "0x0"];
+      ({ result } = await send(method, params, forkedWeb3));
+      assert.strictEqual(result, validSingleUncleId.position0);
+    });
 
-      // Access index greater than position 1 by block number
-      try {
-        method = "eth_getUncleByBlockNumberAndIndex";
-        params = [testBlock.hash, 2];
-        ({ result } = await rpcSend(method, params, forkedWeb3));
-        assert.fail("Should not be able to access an uncle index greater than 1");
-      } catch (error) {
-        assert.strictEqual(error.message, "Uncle array index must be 0 or 1");
-      }
+    it("should validate a block with a TWO uncle ids by block number reference", async function() {
+      this.timeout(10000);
+      const method = "eth_getUncleByBlockNumberAndIndex";
 
-      // Attempt to access an out of range index
+      // Validate uncle id at position 0
+      let params = [twoUncleBlockNumber, 0];
+      let { result } = await send(method, params, forkedWeb3);
+      assert.strictEqual(result, validDoubleUncleIds.position0);
+
+      // Validate uncle id at position 1 with HEX index
+      params = [twoUncleBlockNumber, "0x0"];
+      ({ result } = await send(method, params, forkedWeb3));
+      assert.strictEqual(result, validDoubleUncleIds.position0);
+
+      // Validate uncle id at position 1
+      params = [twoUncleBlockNumber, 1];
+      ({ result } = await send(method, params, forkedWeb3));
+      assert.strictEqual(result, validDoubleUncleIds.position1);
+
+      // Validate uncle id at position 1 with HEX index
+      params = [twoUncleBlockNumber, "0x1"];
+      ({ result } = await send(method, params, forkedWeb3));
+      assert.strictEqual(result, validDoubleUncleIds.position1);
+    });
+
+    it("should validate a block with a TWO uncle ids by hash reference", async function() {
+      this.timeout(10000);
+      const { twoUncleBlock } = forkedBlocks;
+      const method = "eth_getUncleByBlockHashAndIndex";
+
+      let params = [twoUncleBlock.hash, 0];
+      let { result } = await send(method, params, forkedWeb3);
+      assert.strictEqual(result, validDoubleUncleIds.position0);
+
+      params = [twoUncleBlock.hash, 1];
+      ({ result } = await send(method, params, forkedWeb3));
+      assert.strictEqual(result, validDoubleUncleIds.position1);
+    });
+
+    it("should fail to access an invalid index", async function() {
+      this.timeout(10000);
+      // Fail to access DECIMAL based index greater than position 1 by block hash
       try {
-        method = "eth_getUncleByBlockHashAndIndex";
-        params = [testBlock.hash, 1];
-        ({ result } = await rpcSend(method, params, forkedWeb3));
+        const { oneUncleBlock } = forkedBlocks;
+        const method = "eth_getUncleByBlockHashAndIndex";
+        const params = [oneUncleBlock.hash, 1];
+        await send(method, params, forkedWeb3);
         assert.fail("Should not be able to access an out of range index");
       } catch (error) {
         assert.strictEqual(error.message, "Uncle array index is out of range");
       }
-    });
 
-    it("should validate a block with a DOUBLE uncle id by hash or block number reference", async function() {
-      this.timeout(10000);
-      // Directly validate a double uncle count from a know block number on Mainnet
-      const testBlock = await forkedWeb3.eth.getBlock(twoUncleBlockNumber);
-      assert.strictEqual(testBlock.uncles.length, 2);
-
-      // Validate single uncle id using Ganache rpc methods
-      let method = "eth_getUncleByBlockNumberAndIndex";
-      let params = [twoUncleBlockNumber, 0];
-      let { result } = await rpcSend(method, params, forkedWeb3);
-      assert.strictEqual(result, validDoubleUncleIds.position0);
-
-      method = "eth_getUncleByBlockHashAndIndex";
-      params = [testBlock.hash, 0];
-      ({ result } = await rpcSend(method, params, forkedWeb3));
-      assert.strictEqual(result, validDoubleUncleIds.position0);
-
-      // Validate single uncle id using Ganache rpc methods
-      method = "eth_getUncleByBlockNumberAndIndex";
-      params = [twoUncleBlockNumber, 1];
-      ({ result } = await rpcSend(method, params, forkedWeb3));
-      assert.strictEqual(result, validDoubleUncleIds.position1);
-
-      method = "eth_getUncleByBlockHashAndIndex";
-      params = [testBlock.hash, 1];
-      ({ result } = await rpcSend(method, params, forkedWeb3));
-      assert.strictEqual(result, validDoubleUncleIds.position1);
-
-      // Access index greater than position 1
+      // Fail to access DECIMAL based index greater than position 1 by block number
       try {
-        method = "eth_getUncleByBlockHashAndIndex";
-        params = [testBlock.hash, 2];
-        ({ result } = await rpcSend(method, params, forkedWeb3));
+        const method = "eth_getUncleByBlockNumberAndIndex";
+        const params = [oneUncleBlockNumber, 1];
+        await send(method, params, forkedWeb3);
+        assert.fail("Should not be able to access an out of range index");
+      } catch (error) {
+        assert.strictEqual(error.message, "Uncle array index is out of range");
+      }
+
+      // Fail to access HEXADECIMAL based index greater than position 1 by block hash
+      try {
+        const { oneUncleBlock } = forkedBlocks;
+        const method = "eth_getUncleByBlockHashAndIndex";
+        const params = [oneUncleBlock.hash, "0x1"];
+        await send(method, params, forkedWeb3);
+        assert.fail("Should not be able to access an out of range index");
+      } catch (error) {
+        assert.strictEqual(error.message, "Uncle array index is out of range");
+      }
+
+      // Fail to access HEXADECIMAL based index greater than position 1 by block number
+      try {
+        const method = "eth_getUncleByBlockNumberAndIndex";
+        const params = [oneUncleBlockNumber, "0x1"];
+        await send(method, params, forkedWeb3);
+        assert.fail("Should not be able to access an out of range index");
+      } catch (error) {
+        assert.strictEqual(error.message, "Uncle array index is out of range");
+      }
+
+      // Fail to access DECIMAL based index greater than position 1 by block hash
+      try {
+        const { oneUncleBlock } = forkedBlocks;
+        const method = "eth_getUncleByBlockHashAndIndex";
+        const params = [oneUncleBlock.hash, 2];
+        await send(method, params, forkedWeb3);
+        assert.fail("Should not be able to access an uncle index greater than 1");
+      } catch (error) {
+        assert.strictEqual(error.message, "Uncle array index must be 0 or 1");
+      }
+
+      // Acttempt to acess DECIMAL based index greater than position 1 by block number
+      try {
+        const method = "eth_getUncleByBlockNumberAndIndex";
+        const params = [oneUncleBlockNumber, 2];
+        await send(method, params, forkedWeb3);
+        assert.fail("Should not be able to access an uncle index greater than 1");
+      } catch (error) {
+        assert.strictEqual(error.message, "Uncle array index must be 0 or 1");
+      }
+
+      // Fail to access HEXADECIMAL based index greater than position 1 by block hash
+      try {
+        const { oneUncleBlock } = forkedBlocks;
+        const method = "eth_getUncleByBlockHashAndIndex";
+        const params = [oneUncleBlock.hash, "0x2"];
+        await send(method, params, forkedWeb3);
+        assert.fail("Should not be able to access an uncle index greater than 1");
+      } catch (error) {
+        assert.strictEqual(error.message, "Uncle array index must be 0 or 1");
+      }
+
+      // Fail to access HEXADECIMAL based index greater than position 1 by block number
+      try {
+        const method = "eth_getUncleByBlockNumberAndIndex";
+        const params = [oneUncleBlockNumber, "0x2"];
+        await send(method, params, forkedWeb3);
         assert.fail("Should not be able to access an uncle index greater than 1");
       } catch (error) {
         assert.strictEqual(error.message, "Uncle array index must be 0 or 1");
--- test/helpers/rpc.js
+++ test/helpers/rpc.js
@@ -1,4 +1,5 @@
 const pify = require("pify");
+let id = 0;
 
 /**
  * Generic RPC interface
@@ -8,12 +9,10 @@ const pify = require("pify");
  * @param {Object} web3 Web3 interface
  */
 
-const rpcSend = (method = "", params = [], web3) => {
+const send = (method = "", params = [], web3) => {
   const send = pify(web3._provider.send.bind(web3._provider));
-  // an "id" is required here because the web3 websocket provider (v1.0.0-beta.35) throws if it is
-  // missing (it's probably just a bug on their end)
   return send({
-    id: "1",
+    id: `${++id}`,
     jsonrpc: "2.0",
     method,
     params
@@ -21,5 +20,5 @@ const rpcSend = (method = "", params = [], web3) => {
 };
 
 module.exports = {
-  rpcSend
+  send
 };
