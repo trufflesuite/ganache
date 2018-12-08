1c401848f5b249dc8854a541e8e4faf4d791b26d
David Murdoch <david@davidmurdoch.com>
2018-12-07 19:16:45 -0500

    Only set chainId (and v) when it's there.

--- test/ethers.js
+++ test/ethers.js
@@ -18,7 +18,7 @@ describe("ethers", async(done) => {
   const wallet = new ethers.Wallet(privateKey);
   const provider = new ethers.providers.Web3Provider(g);
 
-  it("ether.js transaction hash matches ganache transaction hash", async() => {
+  it("ether.js transaction hash matches ganache transaction hash for chainId 1", async() => {
     // This tx mostly matches EIP-155 example except for the nonce
     // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md
     const transaction = {
@@ -29,6 +29,24 @@ describe("ethers", async(done) => {
       value: 10 ** 18,
       data: "",
       chainId: 1 // EIP 155 chainId - mainnet: 1, ropsten: 3
+    };
+    const signedTransaction = await wallet.sign(transaction);
+    const ethersTxHash = ethers.utils.keccak256(signedTransaction);
+
+    const txHash = await provider.sendTransaction(signedTransaction);
+    assert.deepStrictEqual(txHash.hash, ethersTxHash, "Transaction hash doesn't match etherjs signed transaction hash");
+  });
+
+  it("ether.js transaction hash matches ganache transaction hash for auto chainId", async() => {
+    // This tx mostly matches EIP-155 example except for the nonce and chainId
+    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md
+    const transaction = {
+      nonce: 0,
+      to: "0x3535353535353535353535353535353535353535",
+      gasPrice: 20 * 10 ** 9,
+      gasLimit: 21000,
+      value: 10 ** 18,
+      data: ""
     };
     const signedTransaction = await wallet.sign(transaction);
     const ethersTxHash = ethers.utils.keccak256(signedTransaction);
