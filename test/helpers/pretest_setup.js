adb6a3b514c835f8a7a129a67fb87b7cf7955ff8
Chris Cowell <chris@trufflesuite.com>
2018-12-18 19:54:39 -0500

    Added a setUp module that sets up the following for each test:   - abi   - accounts   - bytecode   - contract   - instance   - provider   - sources   - web3
    Updated undefined.js to use new pretest setUp module.

---
+++ test/helpers/pretest_setup.js
@@ -0,0 +1,39 @@
+const Web3 = require("web3");
+const Ganache = require("../../index");
+const { join } = require("path");
+const { compileAndDeploy } = require("./compile_deploy");
+
+const setUp = (mainContractName = "", subContractNames = [], options, contractPath = "../contracts/") => {
+  const context = {};
+
+  before("Setting up web3 and contract", async function() {
+    this.timeout(10000);
+
+    const provider = Ganache.provider(options);
+    const web3 = new Web3(provider);
+
+    const { abi, accounts, bytecode, contract, instance, sources } = await compileAndDeploy(
+      join(__dirname, contractPath),
+      mainContractName,
+      subContractNames,
+      web3
+    );
+
+    Object.assign(context, {
+      abi,
+      accounts,
+      bytecode,
+      contract,
+      instance,
+      provider,
+      sources,
+      web3
+    });
+  });
+
+  return context;
+};
+
+module.exports = {
+  setUp
+};
