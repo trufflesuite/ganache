91b2e67b0ada66ceb12ae7f334f4ab2853ea03f6
Chris Cowell <chris@trufflesuite.com>
2018-12-21 10:15:31 -0500

    Decomposed compileAndDeploy into compile and deploy modules

--- test/helpers/compileDeploy.js
+++ test/helpers/compileDeploy.js
@@ -1,15 +1,14 @@
-const { compile } = require("solc");
+const solc = require("solc");
 const { readFileSync } = require("fs");
 
 /**
- * Compile and deploy the specified contract(s)
- * @param {String} contractPath  Path to contracts directory
+ * Compile the specified contract(s)
  * @param {String} mainContractName  Name of the main contract (without .sol extension)
  * @param {Array|String} contractFileNames List of imported contracts
- * @param {Object} web3 Web3 interface
- * @returns {Object} context: abi, accounts, bytecode, contract, instance, sources
+ * @param {String} contractPath  Path to contracts directory
+ * @returns {Object} context: abi, bytecode
  */
-async function compileAndDeploy(mainContractName, contractFileNames = [], contractPath, web3) {
+async function compile(mainContractName, contractFileNames = [], contractPath) {
   const selectedContracts = [mainContractName].concat(contractFileNames);
 
   const contractSources = selectedContracts.map((contractName) => {
@@ -19,12 +18,27 @@ async function compileAndDeploy(mainContractName, contractFileNames = [], contra
 
   const sources = Object.assign({}, ...contractSources);
 
-  const { contracts } = compile({ sources });
+  const { contracts } = solc.compile({ sources });
 
   const _mainContractName = mainContractName.replace(/\.sol$/i, "");
   const compiledMainContract = contracts[`${_mainContractName}.sol:${_mainContractName}`];
   const bytecode = `0x${compiledMainContract.bytecode}`;
   const abi = JSON.parse(compiledMainContract.interface);
+
+  return {
+    abi,
+    bytecode
+  };
+}
+
+/**
+ * Deploy a compiled contract
+ * @param {String} abi  contract ABI
+ * @param {String} bytecode  contract bytecode
+ * @param {Object} web3 Web3 interface
+ * @returns {Object} context: abi, accounts, bytecode, contract, instance
+ */
+async function deploy(abi, bytecode, web3) {
   const contract = new web3.eth.Contract(abi);
 
   const accounts = await web3.eth.getAccounts();
@@ -36,11 +50,25 @@ async function compileAndDeploy(mainContractName, contractFileNames = [], contra
     accounts,
     bytecode,
     contract,
-    instance,
-    sources
+    instance
   };
 }
 
-exports = module.exports = {
-  compileAndDeploy
+/**
+ * Compile and deploy the specified contract(s)
+ * @param {String} contractPath  Path to contracts directory
+ * @param {String} mainContractName  Name of the main contract (without .sol extension)
+ * @param {Array|String} contractFileNames List of imported contracts
+ * @param {Object} web3 Web3 interface
+ * @returns {Object} context: abi, accounts, bytecode, contract, instance, sources
+ */
+async function compileAndDeploy(mainContractName, contractFileNames = [], contractPath, web3) {
+  const { abi, bytecode } = await compile(mainContractName, contractFileNames, contractPath);
+  return deploy(abi, bytecode, web3);
+}
+
+module.exports = {
+  compile,
+  compileAndDeploy,
+  deploy
 };
