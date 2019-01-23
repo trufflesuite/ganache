9a3f87a59118ba3e6f39849efc9610485d3c87b0
Dominick Gendill <dgendill@gmail.com>
2019-01-22 20:11:50 -0700

    Added CORS tests. Added some comments about the different headers for CORS. Now showing an error message for OPTIONS requests that are not preflight requests.

--- test/cors.js
+++ test/cors.js
@@ -7,12 +7,14 @@ const customRequestHeader = "X-PINGOTHER";
 
 function test(host, port) {
   describe("CORS", () => {
-    it("should request headers equals to response headers in a preflight request", (done) => {
+    it("should set request headers equals to response headers in a preflight request", (done) => {
       let req = request.options(
         {
           url: "http://" + host + ":" + port,
           headers: {
-            "Access-Control-Request-Headers": customRequestHeader
+            "Access-Control-Request-Headers": customRequestHeader,
+            "Access-Control-Request-Method": "POST",
+            "Origin": "https://localhost:3000"
           }
         },
         function(error, response) {
@@ -30,6 +32,92 @@ function test(host, port) {
             allowHeader,
             customRequestHeader,
             "Access-Control-Allow-Headers should be equals to Access-Control-Request-Headers"
+          );
+
+          done();
+        }
+      );
+
+      req.destroy();
+    });
+
+    it("should return an error message if the OPTIONS request is not a valid preflight request.", (done) => {
+      let origin = "https://localhost:3000";
+      let req = request.options(
+        {
+          url: "http://" + host + ":" + port,
+          headers: {
+            "Origin": origin
+          }
+        },
+        function(error, response) {
+          if (error) {
+            return done(error);
+          }
+
+          assert.strictEqual(
+            response.statusCode,
+            400,
+            "A an OPTIONS request that isn't a preflight request should return an error."
+          );
+
+          done();
+        }
+      );
+
+      req.destroy();
+    });
+
+    it("should set response.Access-Control-Allow-Origin to equal request.Origin if request.Origin is set", (done) => {
+      let origin = "https://localhost:3000";
+      let req = request.options(
+        {
+          url: "http://" + host + ":" + port,
+          headers: {
+            "Origin": origin
+          }
+        },
+        function(error, response) {
+          if (error) {
+            return done(error);
+          }
+
+          let accessControlAllowOrigin = "";
+
+          if (response.headers.hasOwnProperty("access-control-allow-origin")) {
+            accessControlAllowOrigin = response.headers["access-control-allow-origin"];
+          }
+
+          assert.strictEqual(
+            accessControlAllowOrigin,
+            origin,
+            "response.Access-Control-Allow-Origin should equal request.Origin if request.Origin is set."
+          );
+
+          done();
+        });
+
+      req.destroy();
+    });
+
+    it("should set Access-Control-Allow-Credentials=true if the Origin is set.", (done) => {
+      let origin = "https://localhost:3000";
+      let req = request.options(
+        {
+          url: "http://" + host + ":" + port,
+          headers: {
+            "Origin": origin
+          }
+        },
+        function(error, response) {
+          if (error) {
+            return done(error);
+          }
+
+          assert.strictEqual(
+            response.headers["access-control-allow-credentials"],
+            "true",
+            "response.Access-Control-Allow-Origin should equal request.Origin if request.Origin is set."
           );
 
           done();
