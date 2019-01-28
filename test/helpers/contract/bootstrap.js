29817fae951ec2ab1df49333cd63b9f82be6020f
Chris Cowell <chris@trufflesuite.com>
2019-01-28 16:18:40 -0500

    Documented setUp module

--- test/helpers/contract/bootstrap.js
+++ test/helpers/contract/bootstrap.js
@@ -2,10 +2,16 @@ const Ganache = require(process.env.TEST_BUILD
   ? "../build/ganache.core." + process.env.TEST_BUILD + ".js"
   : "../../../index.js");
 const Web3 = require("web3");
-
 const { join } = require("path");
 const { compileAndDeploy } = require("./compileAndDeploy");
 
+/**
+ * @param {string} mainContractName Main contract filename (withouth file extension)
+ * @param {string|Array} subContractNames Array of supporting contract filenames (without file extension)
+ * @param {Object} options Provider options
+ * @param {string} contractPath Relative path to contract
+ * @returns {Object} abi, accounts, bytecode, contract, instance, provider, sources, web3
+ */
 const setUp = (mainContractName = "", subContractNames = [], options = {}, contractPath = "../../contracts/") => {
   const context = {};
 
