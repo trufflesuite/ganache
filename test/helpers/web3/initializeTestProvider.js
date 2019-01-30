e1a8af7d6ef05822bbe042547d71c379c4f97d12
Chris Cowell <chris@trufflesuite.com>
2019-01-29 21:44:33 -0500

    Adding renamed web3 helpers

---
+++ test/helpers/web3/initializeTestProvider.js
@@ -0,0 +1,18 @@
+const Ganache = require(process.env.TEST_BUILD
+  ? "../build/ganache.core." + process.env.TEST_BUILD + ".js"
+  : "../../../index.js");
+const Web3 = require("web3");
+
+const initializeTestProvider = async(options = {}) => {
+  const provider = Ganache.provider(options);
+  const web3 = new Web3(provider);
+  const accounts = await web3.eth.getAccounts();
+
+  return {
+    accounts,
+    provider,
+    web3
+  };
+};
+
+module.exports = initializeTestProvider;
