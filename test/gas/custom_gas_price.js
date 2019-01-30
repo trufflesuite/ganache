b362e1f94ab58fb289e9355f6b4cac02cfd52f94
Chris Cowell <chris@trufflesuite.com>
2019-01-29 21:16:27 -0500

    Testing gasPrice when specified as a string

--- test/gas/custom_gas_price.js
+++ test/gas/custom_gas_price.js
@@ -19,4 +19,22 @@ describe("Custom Gas Price", function() {
     const result = await web3.eth.getGasPrice();
     assert.strictEqual(parseInt(result), 15);
   });
+
+  it("should return gas price of 15 when specified as hex (string)", async function() {
+    const options = {
+      gasPrice: "0xf"
+    };
+    const { web3 } = await getWeb3(options);
+    const result = await web3.eth.getGasPrice();
+    assert.strictEqual(parseInt(result), 15);
+  });
+
+  it("should return gas price of 15 when specified as decimal (string)", async function() {
+    const options = {
+      gasPrice: "15"
+    };
+    const { web3 } = await getWeb3(options);
+    const result = await web3.eth.getGasPrice();
+    assert.strictEqual(parseInt(result), 15);
+  });
 });
