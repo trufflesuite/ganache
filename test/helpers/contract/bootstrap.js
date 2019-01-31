16c01eedef219be114d855586557910ae58fbe32
Chris Cowell <chris@trufflesuite.com>
2019-01-30 20:09:42 -0500

    Renamed setUp to bootstrap Changed a documented contractPath to contractSubdirectory

--- test/helpers/contract/bootstrap.js
+++ test/helpers/contract/bootstrap.js
@@ -9,10 +9,10 @@ const { compileAndDeploy } = require("./compileAndDeploy");
  * @param {string} mainContractName Main contract filename (withouth file extension)
  * @param {string|Array} subContractNames Array of supporting contract filenames (without file extension)
  * @param {Object} options Provider options
- * @param {string} contractPath Relative path to contract
+ * @param {string} contractSubdirectory relative subdirectory under contract (Ex test/contracts/${contractSubdirectory})
  * @returns {Object} abi, accounts, bytecode, contract, instance, provider, sources, web3
  */
-const setUp = (mainContractName = "", subContractNames = [], options = {}, contractPath = "") => {
+const bootstrap = (mainContractName = "", subContractNames = [], options = {}, contractSubdirectory = "") => {
   const context = {};
 
   before("Setting up web3 and contract", async function() {
@@ -25,7 +25,7 @@ const setUp = (mainContractName = "", subContractNames = [], options = {}, contr
     const { abi, accounts, bytecode, contract, instance, sources } = await compileAndDeploy(
       mainContractName,
       subContractNames,
-      join(__dirname, "..", "..", "contracts", `${contractPath}/`),
+      join(__dirname, "..", "..", "contracts", `${contractSubdirectory}/`),
       web3
     );
 
@@ -44,4 +44,4 @@ const setUp = (mainContractName = "", subContractNames = [], options = {}, contr
   return context;
 };
 
-module.exports = setUp;
+module.exports = bootstrap;
