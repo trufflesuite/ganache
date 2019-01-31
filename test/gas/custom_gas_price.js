53099f7def70c964032a9535bb359248480ffb9e
Chris Cowell <chris@trufflesuite.com>
2019-01-31 15:55:37 -0500

    Removed a redundant gas price test.

--- test/gas/custom_gas_price.js
+++ test/gas/custom_gas_price.js
@@ -11,15 +11,6 @@ describe("Custom Gas Price", function() {
     assert.strictEqual(parseInt(result), 15);
   });
 
-  it("should return gas price of 15 when specified as hex", async function() {
-    const options = {
-      gasPrice: 0xf
-    };
-    const { web3 } = await initializeTestProvider(options);
-    const result = await web3.eth.getGasPrice();
-    assert.strictEqual(parseInt(result), 15);
-  });
-
   it("should return gas price of 15 when specified as hex (string)", async function() {
     const options = {
       gasPrice: "0xf"
