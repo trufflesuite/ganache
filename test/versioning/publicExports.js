1cf69db80147d1eb1a6ee0e2fc814c7fb3c31353
Chris Cowell <chris@trufflesuite.com>
2019-02-19 14:13:39 -0500

    Updated publicExports.js trufflesuite/ganache-core#307
    Note:
    - No async/await patterns were added
    - Updated filename, file location, lib references and other minor changes

--- test/versioning/publicExports.js
+++ test/versioning/publicExports.js
@@ -1,10 +1,10 @@
-var Ganache = require(process.env.TEST_BUILD
-  ? "../build/ganache.core." + process.env.TEST_BUILD + ".js"
-  : "../index.js");
-var assert = require("assert");
+const assert = require("assert");
+const Ganache = require(process.env.TEST_BUILD
+  ? "../../build/ganache.core." + process.env.TEST_BUILD + ".js"
+  : "../../index.js");
 
 describe("BuildType", function() {
-  it("Tests that we are using the right version", () => {
+  it("should be using the right Ganache version", function() {
     assert(process.env.TEST_BUILD ? Ganache._webpacked === true : Ganache._webpacked === false);
   });
 });
