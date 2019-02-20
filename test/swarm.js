cc30091c42ae32ef1456050fb2bcb8df7e394c66
Chris Cowell <chris@trufflesuite.com>
2019-02-20 12:59:01 -0500

    Moved test up to `/test`

--- test/swarm.js
+++ test/swarm.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const initializeTestProvider = require("../helpers/web3/initializeTestProvider");
+const initializeTestProvider = require("./helpers/web3/initializeTestProvider");
 
 describe("Swarm", function() {
   const context = initializeTestProvider();
