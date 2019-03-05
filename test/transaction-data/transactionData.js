ab06e067eff2ac11a6ed1609d22d3919705086cb
Erin Shaben <eshaben@icloud.com>
2019-03-05 15:19:28 -0700

    update tests for tx data

--- test/transaction-data/transactionData.js
+++ test/transaction-data/transactionData.js
@@ -1,4 +1,4 @@
-// const assert = require("assert");
+const assert = require("assert");
 const bootstrap = require("../helpers/contract/bootstrap");
 
 describe.only("Transaction Data", () => {
@@ -11,37 +11,39 @@ describe.only("Transaction Data", () => {
       contractSubdirectory: "transaction-data"
     };
     context = await bootstrap(contractRef);
-    console.log(context);
   });
 
-  it("should do thingsss", async() => {
-    // let { instance, accounts, web3 } = context;
-    // const result = await web3.eth.sendTransaction({
-    //   from: accounts[0],
-    //   to: instance._address,
-    //   gas: 31000,
-    //   // correctly formatted vs incorrectly formmated "0x1"
-    //   data: "0x1",
-    //   value: 1
-    // });
-    // console.log(result);
-    // assert.strictEqual(result, "smething");
+  it("should revert with correctly formatted input for data", async() => {
+    let { instance, accounts, web3 } = context;
+
+    try {
+      await web3.eth.sendTransaction({
+        from: accounts[0],
+        to: instance._address,
+        gas: 31000,
+        data: "0x01",
+        value: 1
+      });
+      assert.fail("Call did not fail execution like it was supposed to");
+    } catch (err) {
+      assert.strictEqual(err.message, "VM Exception while processing transaction: revert");
+    }
   });
 
-  //   it("should handle incorrectly formatted data", async() => {
-  //     let { accounts, web3 } = context;
-  //     const result = await web3.eth.sendTransaction({
-  //       from: accounts[0],
-  //       // correctly formatted vs incorrectly formmated "0x1"
-  //       data: "0x1",
-  //       gas: 31000,
-  //       value: 1
-  //     });
+  it("should revert with incorrectly formatted input for data", async() => {
+    let { instance, accounts, web3 } = context;
 
-  //     // this tx should not go thru, but it does
-  //     console.log(result);
-
-  //     // const msgData = await contractInstance.methods.getMsgData().call();
-  //     // console.log(msgData);
-  //   });
+    try {
+      await web3.eth.sendTransaction({
+        from: accounts[0],
+        to: instance._address,
+        gas: 31000,
+        data: "0x1",
+        value: 1
+      });
+      assert.fail("Call did not fail execution like it was supposed to");
+    } catch (err) {
+      assert.strictEqual(err.message, "VM Exception while processing transaction: revert");
+    }
+  });
 });
