0327ce42c552a415f353cb4ab542c0cdf38afa27
Nicholas J. Paterno <cashlion@protonmail.com>
2019-03-05 22:23:57 -0500

    Renovate TestDepth.sol, ensure revert occurs during inner delegatecall

--- test/contracts/gas/TestDepth.sol
+++ test/contracts/gas/TestDepth.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.4.13;
 contract TestDepth {
     uint256 public x;
 
-    function depth(uint256 y) {
+    function depth(uint256 y) public {
+        // bool result;
         if (y > 0) {
-            this.delegatecall(bytes4(sha3('depth(uint256)')), --y);
+            bool result = this.delegatecall(bytes4(keccak256("depth(uint256)")), --y);
+            require(result);
         }
         else {
              // Save the remaining gas in storage so that we can access it later
-             x = msg.gas;
+            x = msg.gas;
         }
     }
 }
