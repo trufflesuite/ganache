d6426b8d2e421d802be705750051a5a5e8cb84e3
Nicholas J. Paterno <cashlion@protonmail.com>
2019-03-07 10:53:28 -0500

    Cleanup create-signed-tx.js, check buffer length

--- test/helpers/utils/create-signed-tx.js
+++ test/helpers/utils/create-signed-tx.js
@@ -1,24 +1,18 @@
 const Transaction = require("../../../lib/utils/transaction");
 
-const createSignedTx = (privateKey, params) => {
+module.exports = function createSignedTx(privateKey, params) {
   function _createAndSign(params) {
-    const signedTx = new Transaction(params);
-    signedTx.sign(privateKey);
-    return signedTx;
+    const tx = new Transaction(params);
+    tx.sign(privateKey);
+    return tx;
   }
 
-  const validBuffer = privateKey && Buffer.isBuffer(privateKey);
+  const validBuffer = privateKey && Buffer.isBuffer(privateKey) && privateKey.length === 32;
   const validParams = params && typeof params === "object";
 
   if (validBuffer) {
-    if (validParams) {
-      return _createAndSign(params);
-    } else {
-      return (params) => _createAndSign(params);
-    }
+    return validParams ? _createAndSign(params) : (params) => _createAndSign(params);
   }
 
   throw new Error("Please use args: privateKey(Buffer), params(Object) ");
 };
-
-module.exports = createSignedTx;
