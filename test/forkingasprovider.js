906c8a99974d24a0f69816e1e7bcdcbff7a567ab
Chris Cowell <chris@trufflesuite.com>
2019-03-11 11:14:10 -0400

    Using compile module

--- test/forkingasprovider.js
+++ test/forkingasprovider.js
@@ -1,12 +1,10 @@
 const Ganache = require(process.env.TEST_BUILD
   ? "../build/ganache.core." + process.env.TEST_BUILD + ".js"
   : "../index.js");
-const solc = require("solc");
 const Web3 = require("web3");
-const fs = require("fs");
 const assert = require("assert");
 const intializeTestProvider = require("./helpers/web3/initializeTestProvider");
-// const { compile } = require("./helpers/contract/compileAndDeploy");
+const { compile } = require("./helpers/contract/compileAndDeploy");
 
 const logger = {
   log: function(msg) {}
@@ -30,18 +28,15 @@ describe("Forking using a Provider", function() {
   let forkedAccounts;
   let contractAddress;
 
-  before("set up test data", function() {
+  before("set up test data", async function() {
     this.timeout(5000);
-    const source = fs.readFileSync("./test/contracts/examples/Example.sol", { encoding: "utf8" });
-    const result = solc.compile(source, 1);
+    const { bytecode } = await compile("Example", ["Example"], "examples");
 
     // Note: Certain properties of the following contract data are hardcoded to
     // maintain repeatable tests. If you significantly change the solidity code,
     // make sure to update the resulting contract data with the correct values.
     contract = {
-      solidity: source,
-      abi: result.contracts[":Example"].interface,
-      binary: "0x" + result.contracts[":Example"].bytecode,
+      bytecode,
       position_of_value: "0x0000000000000000000000000000000000000000000000000000000000000000",
       expected_default_value: 5,
       call_data: {
@@ -66,7 +61,7 @@ describe("Forking using a Provider", function() {
   before("Deploy initial contracts", async function() {
     const receipt = await forkedWeb3.eth.sendTransaction({
       from: forkedAccounts[0],
-      data: contract.binary,
+      data: contract.bytecode,
       gas: 3141592
     });
 
@@ -79,9 +74,9 @@ describe("Forking using a Provider", function() {
     assert.notStrictEqual(code, "0x0");
   });
 
-  let context;
+  let mainContext;
   before("Set up main provider and web3 instance", async function() {
-    context = await intializeTestProvider({
+    mainContext = await intializeTestProvider({
       fork: forkedProvider,
       logger,
       seed: "forked provider"
@@ -93,7 +88,7 @@ describe("Forking using a Provider", function() {
   // grab data from the forked chain when a provider instance is passed (instead of a URL). If this
   // one passes, it should follow that the rest of the forking features should work as normal.
   it("gets code correctly via the main chain (i.e., internally requests it from forked chain)", async function() {
-    const { web3: mainWeb3 } = context;
+    const { web3: mainWeb3 } = mainContext;
     const code = await mainWeb3.eth.getCode(contractAddress);
 
     // Ensure there's *something* there.
