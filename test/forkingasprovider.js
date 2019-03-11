a481c8df8e74955545825ce3e10ea21c9c657237
Chris Cowell <chris@trufflesuite.com>
2019-03-11 11:41:09 -0400

    Using bootstrap to deploy contract

--- test/forkingasprovider.js
+++ test/forkingasprovider.js
@@ -1,14 +1,6 @@
-const Ganache = require(process.env.TEST_BUILD
-  ? "../build/ganache.core." + process.env.TEST_BUILD + ".js"
-  : "../index.js");
-const Web3 = require("web3");
 const assert = require("assert");
+const bootstrap = require("./helpers/contract/bootstrap");
 const intializeTestProvider = require("./helpers/web3/initializeTestProvider");
-const { compile } = require("./helpers/contract/compileAndDeploy");
-
-const logger = {
-  log: function(msg) {}
-};
 
 /**
  * NOTE: Naming in these tests is a bit confusing. Here, the "main chain"
@@ -18,64 +10,31 @@ const logger = {
  * network) and the fork chaing being "the fork".
  */
 
-describe("Forking using a Provider", function() {
-  let contract;
-  const forkedProvider = Ganache.provider({
-    logger: logger,
-    seed: "main provider"
-  });
-  const forkedWeb3 = new Web3(forkedProvider);
-  let forkedAccounts;
-  let contractAddress;
-
-  before("set up test data", async function() {
-    this.timeout(5000);
-    const { bytecode } = await compile("Example", ["Example"], "examples");
-
-    // Note: Certain properties of the following contract data are hardcoded to
-    // maintain repeatable tests. If you significantly change the solidity code,
-    // make sure to update the resulting contract data with the correct values.
-    contract = {
-      bytecode,
-      position_of_value: "0x0000000000000000000000000000000000000000000000000000000000000000",
-      expected_default_value: 5,
-      call_data: {
-        gas: "0x2fefd8",
-        gasPrice: "0x1", // This is important, as passing it has exposed errors in the past.
-        to: null, // set by test
-        data: "0x3fa4f245"
-      },
-      transaction_data: {
-        from: null, // set by test
-        gas: "0x2fefd8",
-        to: null, // set by test
-        data: "0x552410770000000000000000000000000000000000000000000000000000000000000019" // sets value to 25 (base 10)
-      }
-    };
-  });
-
-  before("Gather forked accounts", async function() {
-    forkedAccounts = await forkedWeb3.eth.getAccounts();
-  });
-
-  before("Deploy initial contracts", async function() {
-    const receipt = await forkedWeb3.eth.sendTransaction({
-      from: forkedAccounts[0],
-      data: contract.bytecode,
-      gas: 3141592
-    });
-
-    contractAddress = receipt.contractAddress;
-
-    const code = await forkedWeb3.eth.getCode(contractAddress);
-    // Ensure there's *something* there.
-    assert.notStrictEqual(code, null);
-    assert.notStrictEqual(code, "0x");
-    assert.notStrictEqual(code, "0x0");
-  });
-
+describe("Forking using a Provider", () => {
+  let forkedContext;
   let mainContext;
+  const logger = {
+    log: function(msg) {}
+  };
+
+  before("Set up forked provider with web3 instance and deploy a contract", async function() {
+    this.timeout(5000);
+
+    const contractRef = {
+      contractFiles: ["Example"],
+      contractSubdirectory: "examples"
+    };
+
+    const ganacheProviderOptions = {
+      logger,
+      seed: "main provider"
+    };
+
+    forkedContext = await bootstrap(contractRef, ganacheProviderOptions);
+  });
+
   before("Set up main provider and web3 instance", async function() {
+    const { provider: forkedProvider } = forkedContext;
     mainContext = await intializeTestProvider({
       fork: forkedProvider,
       logger,
@@ -87,11 +46,13 @@ describe("Forking using a Provider", function() {
   // with good tests, this one simply ensures the forked feature still works by testing that we can
   // grab data from the forked chain when a provider instance is passed (instead of a URL). If this
   // one passes, it should follow that the rest of the forking features should work as normal.
-  it("gets code correctly via the main chain (i.e., internally requests it from forked chain)", async function() {
+  it("gets code correctly via the main chain (i.e., internally requests it from forked chain)", async() => {
+    const { instance } = forkedContext;
     const { web3: mainWeb3 } = mainContext;
-    const code = await mainWeb3.eth.getCode(contractAddress);
 
-    // Ensure there's *something* there.
+    const code = await mainWeb3.eth.getCode(instance._address);
+
+    // Ensure that a contract is at the address
     assert.notStrictEqual(code, null);
     assert.notStrictEqual(code, "0x");
     assert.notStrictEqual(code, "0x0");
