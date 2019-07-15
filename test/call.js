f606f475d496ed1a220837c7dc9f1217d5301c02
David Murdoch <davidmurdoch@users.noreply.github.com>
2019-07-15 11:43:24 -0400

    fix: `eth_call` and `eth_estimate` gas limits (mostly) (#449)
    * Allow a reaaallyyy high gasLimit for eth_call and eth_estimateGas
    * Use MAX_SAFE_INTEGER instead of something bigger

--- test/call.js
+++ test/call.js
@@ -2,19 +2,16 @@ const assert = require("assert");
 const bootstrap = require("./helpers/contract/bootstrap");
 
 describe("eth_call", function() {
-  let context;
-
-  before("Setting up web3 and contract", async function() {
-    this.timeout(10000);
+  it("should use the call gas limit if no call gas limit is specified in the call", async function() {
     const contractRef = {
       contractFiles: ["EstimateGas"],
       contractSubdirectory: "gas"
     };
+    let context;
 
-    context = await bootstrap(contractRef);
-  });
-
-  it("should use the block gas limit if no gas limit is specified", async function() {
+    context = await bootstrap(contractRef, {
+      callGasLimit: "0x6691b7"
+    });
     const { accounts, instance } = context;
 
     const name = "0x54696d"; // Byte code for "Tim"
@@ -23,6 +20,27 @@ describe("eth_call", function() {
 
     // this call uses more than the default transaction gas limit and will
     // therefore fail if the block gas limit isn't used for calls
+    const status = await instance.methods.add(name, description, value).call({ from: accounts[0] });
+
+    assert.strictEqual(status, true);
+  });
+
+  it("should use maxUInt64 call gas limit if no gas limit is specified in the provider or the call", async function() {
+    const contractRef = {
+      contractFiles: ["EstimateGas"],
+      contractSubdirectory: "gas"
+    };
+    let context;
+
+    context = await bootstrap(contractRef);
+    const { accounts, instance } = context;
+
+    const name = "0x54696d"; // Byte code for "Tim"
+    const description = "0x4120677265617420677579"; // Byte code for "A great guy"
+    const value = 5;
+
+    // this call uses more than the default transaction gas limit and will
+    // therefore fail if the maxUInt64 limit isn't used for calls
     const status = await instance.methods.add(name, description, value).call({ from: accounts[0] });
 
     assert.strictEqual(status, true);
