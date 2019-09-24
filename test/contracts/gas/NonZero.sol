92c4cccc0ee29c0ec7e6ff4fe2fe1799e92f3c97
Nicholas J. Paterno <cashlion@protonmail.com>
2019-09-24 18:15:43 -0400

    Fix contract to produce bug

--- test/contracts/gas/NonZero.sol
+++ test/contracts/gas/NonZero.sol
@@ -1,8 +1,20 @@
 pragma solidity ^0.5.0;
+contract Target{
+    function () external payable { }//fallback function
+}
 contract NonZero{
-        function () external payable {
-        // address(msg.sender).send(msg.value);
-        // address(msg.sender).transfer(msg.value);
-        address(msg.sender).call.value(msg.value).gas(123456)("");
+    Target private theInstance;
+    constructor() public{
+        theInstance=new Target();
     }
+    function doCall() external payable {
+        address(theInstance).call.value(msg.value).gas(123456)("");
+    }
+    function doTransfer() external payable {
+        address(theInstance).transfer(msg.value);
+    }
+    function doSend() external payable {
+        address(theInstance).send(msg.value);
+    }
+    function () external payable { }
 }
