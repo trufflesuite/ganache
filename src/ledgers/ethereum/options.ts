6f5464bf682f0b09f20f226dd0cb6e505896ca2c
David Murdoch <david@davidmurdoch.com>
2020-01-06 18:50:34 -0500

    Improvie typing in EthereumOptions

--- src/ledgers/ethereum/options.ts
+++ src/ledgers/ethereum/options.ts
@@ -1,22 +1,12 @@
-import ILedgerOptions from "../../interfaces/ledger-options"
+import Options from "../../options/provider-options";
 import Account from "../../types/account";
-import { Quantity } from "../../types/json-rpc";
 
-type EthereumOptions = ILedgerOptions & {
-    net_version?: string | number,
-    gasPrice?: Quantity,
-    accounts?: Account[],
-    unlocked_accounts?: string[] | number[],
-    allowUnlimitedContractSize?: boolean,
-    hardfork?: string,
-    gasLimit?: Quantity,
-    timestamp?: Date,
-    db?: object,
-    db_path?: string,
-    secure?: boolean
-}
+type EthereumOptions = Pick<Options, "net_version" | "gasPrice" | "unlocked_accounts" | "allowUnlimitedContractSize" | "hardfork" | "gasLimit" | "db" | "db_path" | "secure"> & {
+  timestamp: Date,
+  accounts: Account[]
+};
 
 export default EthereumOptions;
-export const getDefaultOptions : () => EthereumOptions = () => {
-    return {} as EthereumOptions;
+export const getDefaultOptions: () => EthereumOptions = () => {
+  return {} as EthereumOptions;
 }
