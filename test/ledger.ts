d590bcee7a7934d6defd549fbd45fe0c384c2fcd
Nicholas J. Paterno <cashlion@protonmail.com>
2020-01-09 18:43:43 -0500

    Add tests for eth_get*

--- test/ledger.ts
+++ test/ledger.ts
@@ -1,48 +1,66 @@
 import assert from "assert";
 import GetProvider, { Provider } from "./helpers/getProvider";
+import sleep from "./helpers/sleep";
 
-describe("ledger", () => {
-  // const expectedAddress = "0x604a95C9165Bc95aE016a5299dd7d400dDDBEa9A";
+describe.only("ledger", () => {
   let provider: Provider;
+  let accounts: string[];
 
-  beforeEach(() => {
+  beforeEach(async () => {
     provider = GetProvider();
+    accounts = await provider.send("eth_accounts");
   });
 
   it("eth_getBlockByNumber", async() => {
-    // @TODO: Fix this horrible test
-    const accounts = await provider.send("eth_accounts");
     await provider.send("eth_sendTransaction", [{
       from: accounts[0],
       to: accounts[1],
       value: 1
     }]);
     // TODO: remove and replace with something that detects with the block is "mined"
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    await sleep();
     const blocks = await Promise.all([provider.send("eth_getBlockByNumber", ["0x1", true]), provider.send("eth_getBlockByNumber", ["0x1"])]);
-    console.log(JSON.stringify(blocks));
-
-    const counts = await Promise.all([provider.send("eth_getBlockTransactionCountByNumber", ["0x1"]), provider.send("eth_getBlockTransactionCountByHash", [blocks[0].hash])]);
-    console.log(counts);
-    
-    assert(true);
+    assert(blocks[0].hash, blocks[1].hash);
   });
 
-  it("eth_getBlockByNumber", async() => {
-    // @TODO: Fix this horrible test
-    const accounts = await provider.send("eth_accounts");
+  it("eth_getBlockByHash", async() => {
     await provider.send("eth_sendTransaction", [{
       from: accounts[0],
       to: accounts[1],
       value: 1
     }]);
     // TODO: remove and replace with something that detects with the block is "mined"
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    await sleep();
     const block = await provider.send("eth_getBlockByNumber", ["0x1"]);
 
-    const blocks2 = await Promise.all([provider.send("eth_getBlockByHash", [block.hash, true]), provider.send("eth_getBlockByHash", [block.hash])]);
-    console.log(JSON.stringify(blocks2));
-    
-    assert(true);
+    const blocks = await Promise.all([provider.send("eth_getBlockByHash", [block.hash, true]), provider.send("eth_getBlockByHash", [block.hash])]);
+    assert(blocks[0].hash, blocks[1].hash);
+  });
+
+  it("eth_getBlockTransactionCountByHash", async() => {
+    await provider.send("eth_sendTransaction", [{
+      from: accounts[0],
+      to: accounts[1],
+      value: 1
+    }]);
+    // TODO: remove and replace with something that detects with the block is "mined"
+    await sleep();
+    const block = await provider.send("eth_getBlockByNumber", ["0x1"]);
+
+    const count = await provider.send("eth_getBlockTransactionCountByHash", [block.hash]);
+    assert(count, "1");
+  });
+
+  it("eth_getBlockTransactionCountByNumber", async() => {
+    await provider.send("eth_sendTransaction", [{
+      from: accounts[0],
+      to: accounts[1],
+      value: 1
+    }]);
+    // TODO: remove and replace with something that detects with the block is "mined"
+    await sleep();
+
+    const count = await provider.send("eth_getBlockTransactionCountByNumber", ["0x1"]);
+    assert(count, "1");
   });
 })
