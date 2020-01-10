ed3d1727e17fa7a9620c54f429c75ceba65782db
David Murdoch <david@davidmurdoch.com>
2020-01-10 17:21:39 -0500

    Whitespace fix

--- src/ledgers/ethereum/components/transaction-manager.ts
+++ src/ledgers/ethereum/components/transaction-manager.ts
@@ -7,18 +7,18 @@ import Blockchain from "../blockchain";
 export type TransactionManagerOptions = TransactionPoolOptions;
 
 export default class TransactionManager extends Manager<Transaction> {
-    public transactionPool: TransactionPool;
+  public transactionPool: TransactionPool;
 
-    constructor(blockchain: Blockchain, base: levelup.LevelUp, options: TransactionManagerOptions) {
-        super(blockchain, base, Transaction);
+  constructor(blockchain: Blockchain, base: levelup.LevelUp, options: TransactionManagerOptions) {
+    super(blockchain, base, Transaction);
 
-        this.transactionPool = new TransactionPool(blockchain, options);
-        this.transactionPool.on("drain", (transactions: Transaction[]) => {
-            // TODO: create pending block?
-        });
-    }
+    this.transactionPool = new TransactionPool(blockchain, options);
+    this.transactionPool.on("drain", (transactions: Transaction[]) => {
+      // TODO: create pending block?
+    });
+  }
 
-    public push(transaction: Transaction): Promise<void> {
-        return this.transactionPool.insert(transaction);
-    }
+  public push(transaction: Transaction): Promise<void> {
+    return this.transactionPool.insert(transaction);
+  }
 }
