e363b7eaed97b5eb25025f821574af1ada51a9d9
David Murdoch <david@davidmurdoch.com>
2020-01-10 17:17:24 -0500

    Remove bad exports

--- src/types/json-rpc/index.ts
+++ src/types/json-rpc/index.ts
@@ -1,4 +1,4 @@
 import {IndexableJsonRpcType, JsonRpcType, BaseJsonRpcType} from "./json-rpc-base-types";
-import Quantity, {IndexableQuantity} from "./json-rpc-quantity";
+import Quantity from "./json-rpc-quantity";
 import Data, {IndexableData} from "./json-rpc-data";
-export {Data, Quantity, IndexableJsonRpcType, JsonRpcType, BaseJsonRpcType, IndexableQuantity, IndexableData};
+export {Data, Quantity, IndexableJsonRpcType, JsonRpcType, BaseJsonRpcType, IndexableData};
