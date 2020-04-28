5f493a6c5670656eec6e7d9f4837ad8cab8f8c60
David Murdoch <david@davidmurdoch.com>
2020-04-28 17:25:32 -0400

    Some repo cleanup

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import ProviderOptions from "./src/options/provider-options";
 import ServerOptions from "./src/options/server-options";
 import Server from "./src/server";
 
-// `server` and `provider` are here for backwards compatability
 export default {
   server: (options?: ServerOptions) => new Server(options),
   provider: (options?: ProviderOptions) => Provider.initialize(options),
