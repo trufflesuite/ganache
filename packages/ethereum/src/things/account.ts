03aca4c77f4d6c4b6a5793d54c49ad0dfc06dcf6
David Murdoch <david@davidmurdoch.com>
2020-05-07 17:28:57 -0400

    Simply account constructor logic

--- packages/ethereum/src/things/account.ts
+++ packages/ethereum/src/things/account.ts
@@ -13,16 +13,16 @@ export default class Account {
   constructor(address: Address);
   constructor(buffer: Buffer);
   constructor(arg: Address | Buffer) {
-    if (arg instanceof Address) {
-      this.address = arg;
-      this.balance = new Quantity(0n);
-      this.nonce = new Quantity(0n);
-    } else if (Buffer.isBuffer(arg)) {
+    if (Buffer.isBuffer(arg)) {
       const arr = (rlp.decode(arg) as any) as Buffer[];
       this.nonce = Quantity.from(arr[0]);
       this.balance = Quantity.from(arr[1]);
       this.stateRoot = arr[2];
       this.codeHash = arr[3];
+    } else {
+      this.address = arg;
+      this.balance = new Quantity(0n);
+      this.nonce = new Quantity(0n);
     }
   }
   public serialize() {
