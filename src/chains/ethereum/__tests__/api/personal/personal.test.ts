e9bc8035214b11f4ebbf8916890ddfa2c1288d4d
David Murdoch <david@davidmurdoch.com>
2020-07-21 18:12:49 -0400

    Ad tests for `personal_importRawKey`

--- src/chains/ethereum/__tests__/api/personal/personal.test.ts
+++ src/chains/ethereum/__tests__/api/personal/personal.test.ts
@@ -1,6 +1,7 @@
 import assert from "assert";
 import getProvider from "../../helpers/getProvider";
 import { Quantity } from "@ganache/utils/src/things/json-rpc";
+import EthereumProvider from "../../../src/provider";
 
 describe("api", () => {
   describe("personal", () => {
@@ -12,6 +13,90 @@ describe("api", () => {
         assert.deepStrictEqual(personalAccounts, accounts);
       });
     });
+
+    async function testLockedAccountWithPassphraseViaEth_SendTransaction(provider: EthereumProvider, newAccount: string, passphrase: string) {
+      const transaction = {
+        from: newAccount,
+        to: newAccount,
+        gasLimit: 21000,
+        gasPrice: 0,
+        value: 0,
+        nonce: 0
+      };
+
+      // make sure we can't use the account via eth_sendTransaction
+      await assert.rejects(provider.send("eth_sendTransaction", [transaction]), {
+        message: "signer account is locked"
+      }, "eth_sendTransaction should have rejected due to locked from account without its passphrase");
+
+      // unlock the account indefinitely
+      const unlocked = await provider.send("personal_unlockAccount", [newAccount, passphrase, 0]);
+      assert.strictEqual(unlocked, true);
+
+      await provider.send("eth_subscribe", ["newHeads"]);
+
+      // send a normal transaction
+      const transactionHash = await provider.send("eth_sendTransaction", [transaction]);
+      await provider.once("message");
+
+      // ensure sure it worked
+      const receipt = await provider.send("eth_getTransactionReceipt", [transactionHash]);
+      assert.strictEqual(receipt.status, 1, "Transaction failed when it should have succeeded");
+
+      // lock the account
+      const accountLocked = await provider.send("personal_lockAccount", [newAccount]);
+      assert.strictEqual(accountLocked, true);
+
+      // make sure it is locked
+      await assert.rejects(provider.send("eth_sendTransaction", [Object.assign({}, transaction, {nonce: 1})]), {
+        message: "signer account is locked"
+      }, "personal_lockAccount didn't work");
+    }
+
+    async function testLockedAccountWithPassphraseViaPersonal_SendTransaction(provider: EthereumProvider, newAccount: string, passphrase: string){
+      const transaction = {
+        from: newAccount,
+        to: newAccount,
+        gasLimit: 21000,
+        gasPrice: 0,
+        value: 0,
+        nonce: 0
+      };
+
+      // make sure we can't use the account via personal_sendTransaction and no passphrase
+      await assert.rejects(provider.send("personal_sendTransaction", [transaction]), {
+        message: "Invalid password"
+      }, "personal_sendTransaction should have rejected due to locked from account without its passphrase");
+
+      // make sure we can't use the account with bad passphrases
+      const invalidPassphrases = ["this is not my passphrase", null, undefined, Buffer.allocUnsafe(0), 1, 0, Infinity, NaN];
+      await Promise.all(invalidPassphrases.map(invalidPassphrase => {
+        return assert.rejects(provider.send("personal_sendTransaction", [transaction, invalidPassphrase]), {
+          message: "Invalid password"
+        }, "Transaction should have rejected due to locked from account with wrong passphrase")
+      }));
+
+      // use personal_sendTransaction with the valid passphrase
+      await provider.send("eth_subscribe", ["newHeads"]);
+      const transactionHashPromise = provider.send("personal_sendTransaction", [transaction, passphrase]);
+      const msgPromise = transactionHashPromise.then(() => provider.once("message"));
+
+      await assert.rejects(provider.send("eth_sendTransaction", [Object.assign({}, transaction, {nonce: 1})]), {
+        message: "signer account is locked"
+      }, "personal_sendTransaction should not unlock the while transaction is bring processed");
+
+      const transactionHash = await transactionHashPromise
+      await msgPromise;
+
+      const receipt = await provider.send("eth_getTransactionReceipt", [transactionHash]);
+      assert.strictEqual(receipt.status, 1, "Transaction failed when it should have succeeded");
+
+      // ensure the account is still locked
+      await assert.rejects(provider.send("eth_sendTransaction", [Object.assign({}, transaction, {nonce: 1})]), {
+        message: "signer account is locked"
+      }, "personal_sendTransaction should still be locked the after the transaction is processed");
+    }
+
     describe("newAccount", () => {
       it("generates deterministic accounts", async () => {
         const controlProvider = await getProvider();
@@ -64,42 +149,7 @@ describe("api", () => {
           // generate an account
           const newAccount = await provider.send("personal_newAccount", [passphrase]);
 
-          const transaction = {
-            from: newAccount,
-            to: newAccount,
-            gasLimit: 21000,
-            gasPrice: 0,
-            value: 0,
-            nonce: 0
-          };
-
-          // make sure we can't use the account via eth_sendTransaction
-          await assert.rejects(provider.send("eth_sendTransaction", [transaction]), {
-            message: "signer account is locked"
-          }, "eth_sendTransaction should have rejected due to locked from account without its passphrase");
-
-          // unlock the account indefinitely
-          const unlocked = await provider.send("personal_unlockAccount", [newAccount, passphrase, 0]);
-          assert.strictEqual(unlocked, true);
-
-          await provider.send("eth_subscribe", ["newHeads"]);
-
-          // send a normal transaction
-          const transactionHash = await provider.send("eth_sendTransaction", [transaction]);
-          await provider.once("message");
-
-          // ensure sure it worked
-          const receipt = await provider.send("eth_getTransactionReceipt", [transactionHash]);
-          assert.strictEqual(receipt.status, 1, "Transaction failed when it should have succeeded");
-
-          // lock the account
-          const accountLocked = await provider.send("personal_lockAccount", [newAccount]);
-          assert.strictEqual(accountLocked, true);
-
-          // make sure it is locked
-          await assert.rejects(provider.send("eth_sendTransaction", [Object.assign({}, transaction, {nonce: 1})]), {
-            message: "signer account is locked"
-          }, "personal_lockAccount didn't work");
+          testLockedAccountWithPassphraseViaEth_SendTransaction(provider, newAccount, passphrase);
         });
       });
 
@@ -110,47 +160,39 @@ describe("api", () => {
           // generate an account
           const newAccount = await provider.send("personal_newAccount", [passphrase]);
 
-          const transaction = {
-            from: newAccount,
-            to: newAccount,
-            gasLimit: 21000,
-            gasPrice: 0,
-            value: 0,
-            nonce: 0
-          };
+          testLockedAccountWithPassphraseViaPersonal_SendTransaction(provider, newAccount, passphrase);
+        });
+      });
+    });
 
-          // make sure we can't use the account via personal_sendTransaction and no passphrase
-          await assert.rejects(provider.send("personal_sendTransaction", [transaction]), {
-            message: "Invalid password"
-          }, "personal_sendTransaction should have rejected due to locked from account without its passphrase");
+    describe("personal_importRawKey", () => {
+      const secretKey = "0x0123456789012345678901234567890123456789012345678901234567890123";
+      const passphrase = "this is my passphrase";
 
-          // make sure we can't use the account with bad passphrases
-          const invalidPassphrases = ["this is not my passphrase", null, undefined, Buffer.allocUnsafe(0), 1, 0, Infinity, NaN];
-          await Promise.all(invalidPassphrases.map(invalidPassphrase => {
-            return assert.rejects(provider.send("personal_sendTransaction", [transaction, invalidPassphrase]), {
-              message: "Invalid password"
-            }, "Transaction should have rejected due to locked from account with wrong passphrase")
-          }));
+      it("should return the known account address", async () => {
+        const provider = await getProvider();
+        const newAccount = await provider.send("personal_importRawKey", [secretKey, passphrase]);
+        assert.strictEqual(newAccount, "0x14791697260e4c9a71f18484c9f997b308e59325", "Raw account not imported correctly");
+      });
 
-          // use personal_sendTransaction with the valid passphrase
-          await provider.send("eth_subscribe", ["newHeads"]);
-          const transactionHashPromise = provider.send("personal_sendTransaction", [transaction, passphrase]);
-          const msgPromise = transactionHashPromise.then(() => provider.once("message"));
+      describe("personal_unlockAccount ➡ eth_sendTransaction ➡ personal_lockAccount", () => {
+        it("generates locked accounts with passphrase", async () => {
+          const provider = await getProvider({gasPrice: Quantity.from(0x0)});
+          const passphrase = "this is my passphrase";
+          // generate an account
+          const newAccount = await provider.send("personal_importRawKey", [secretKey, passphrase]);
 
-          await assert.rejects(provider.send("eth_sendTransaction", [Object.assign({}, transaction, {nonce: 1})]), {
-            message: "signer account is locked"
-          }, "personal_sendTransaction should not unlock the while transaction is bring processed");
+          testLockedAccountWithPassphraseViaEth_SendTransaction(provider, newAccount, passphrase);
+        });
+      });
 
-          const transactionHash = await transactionHashPromise
-          await msgPromise;
+      describe("personal_sendTransaction", () => {
+        it("generates locked accounts with passphrase", async () => {
+          const provider = await getProvider({gasPrice: Quantity.from(0x0)});
+          // generate an account
+          const newAccount = await provider.send("personal_importRawKey", [secretKey, passphrase]);
 
-          const receipt = await provider.send("eth_getTransactionReceipt", [transactionHash]);
-          assert.strictEqual(receipt.status, 1, "Transaction failed when it should have succeeded");
-
-          // ensure the account is still locked
-          await assert.rejects(provider.send("eth_sendTransaction", [Object.assign({}, transaction, {nonce: 1})]), {
-            message: "signer account is locked"
-          }, "personal_sendTransaction should still be locked the after the transaction is processed");
+          testLockedAccountWithPassphraseViaPersonal_SendTransaction(provider, newAccount, passphrase);
         });
       });
     });
