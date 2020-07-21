f2ff7118bb8ecd8886eabbb800b5f2a3133a08a4
David Murdoch <david@davidmurdoch.com>
2020-07-20 21:13:05 -0400

    Add web3 tests

---
+++ src/chains/ethereum/__tests__/api/web3/web3.test.ts
@@ -0,0 +1,26 @@
+
+import getProvider from "../../helpers/getProvider";
+import assert from "assert";
+import EthereumProvider from "../../../src/provider";
+import { version } from "../../../../../packages/core/package.json";
+
+describe("api", () => {
+  describe("web3", () => {
+    let provider: EthereumProvider;
+    before(async () => {
+      provider = await getProvider();
+    });
+
+    it("web3_clientVersion returns the client version", async () => {
+      const result = await provider.send("web3_clientVersion");
+      assert.deepStrictEqual(result, "Ganache/v" + version);
+    });
+
+    it("web3_sha should hash the given input", async () => {
+      const input = "Tim is a swell guy.";
+      const result = await provider.send("web3_sha3", [input]);
+
+      assert.strictEqual(result, "0xee80d4ac03202e2246d51a596c76a18e1a6d899bed9f05246d998fb656d9bd1f");
+    });
+  });
+});
