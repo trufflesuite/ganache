3b172dd8e04bdd40ec8d0d3ca2931c8e631cb274
David Murdoch <david@davidmurdoch.com>
2020-07-31 17:30:47 -0400

    Add comment about why the index.ts file is present in each package

--- scripts/create.ts
+++ scripts/create.ts
@@ -76,7 +76,11 @@ describe("${packageName}", () => {
 }
 `;
 
-const rootIndexFile = `export * from "./src/index";
+    const rootIndexFile = `// ************************************************************************* //
+// This file is necessary to "trick" typescript into using our ./src/**/*.ts //
+//            files when developing, debugging, and running tests            //
+// ************************************************************************* //
+export * from "./src/index";
 `;
 
     const dir = join("./src/packages", name);
--- src/chains/tezos/index.ts
+++ src/chains/tezos/index.ts
@@ -1,1 +1,6 @@
+// ************************************************************************* //
+// This file is necessary to "trick" typescript into using our ./src/**/*.ts //
+//            files when developing, debugging, and running tests            //
+// ************************************************************************* //
+
 export * from "./src/index";
