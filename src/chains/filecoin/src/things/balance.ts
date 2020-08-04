464ba062e04ea165ec99e5397964df92745def2e
Tim Coulter <tim@timothyjcoulter.com>
2020-08-04 15:54:28 -0700

    Add a nice toFIL helper

--- src/chains/filecoin/src/things/balance.ts
+++ src/chains/filecoin/src/things/balance.ts
@@ -17,6 +17,10 @@ class Balance extends SerializableLiteral<BalanceConfig>  {
   sub(val:string | number):Balance {
     return new Balance(new BN(this.value).sub(new BN(val)).toString(10));
   }
+
+  toFIL():number {
+    return new BN(this.value).div(new BN(10).pow(new BN(21))).toNumber()
+  }
 }
 
 export default Balance;
