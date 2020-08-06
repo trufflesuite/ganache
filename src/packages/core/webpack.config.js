5f77f8deac03f07cf14f491eb3355cde4146419c
David Murdoch <david@davidmurdoch.com>
2020-08-06 14:29:10 -0400

    create a webpack that seems to work

--- src/packages/core/webpack.config.js
+++ src/packages/core/webpack.config.js
@@ -1,15 +1,9 @@
-const { IgnorePlugin } = require("webpack");
-
 const path = require('path');
 
 module.exports = {
   target: "node",
   entry: path.join(__dirname, 'lib/index.js'),
   devtool: 'inline-source-map',
-  plugins: [
-    // ignore these plugins completely
-    new IgnorePlugin(/^(?:electron)$/)
-  ],
   resolve: {
     extensions: [ '.js', '.json'],
     modules: [
@@ -22,7 +16,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, "build"),
     library: 'Ganache',
     libraryTarget: "commonjs2",
     libraryExport: "default"
@@ -31,21 +25,9 @@ module.exports = {
     "uWebSockets.js": "commonjs2 uWebSockets.js",
     "bigint-buffer": "commonjs2 bigint-buffer",
     "leveldown": "commonjs2 leveldown",
-    "dlv": "commonjs2 dlv",
-    "ws": "commonjs2 ws"
+    "ipfs": "commonjs2 ipfs",
   },
-    // (_context, request, callback) => {
-    //   if (["uWebSockets.js", "bigint-buffer", "leveldown"].includes(request)) {
-    //     return callback();
-    //   }
-    //   // if (/^(\.|@ganache)/.test(request)) {
-    //   //   return callback();
-    //   // }
-    //   // we don't want to webpack any other modules
-    //   return callback(null, "commonjs2 " + request);
-    // }
-    // ],
   optimization: {
     minimize: false
-  },
+  }
 };
