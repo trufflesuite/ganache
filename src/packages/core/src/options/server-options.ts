4e786836b8fe5eb753196d615d5a1f93e03e8633
David Murdoch <david@davidmurdoch.com>
2020-08-11 18:47:29 -0400

    Remove keepAliveTimeout option

--- src/packages/core/src/options/server-options.ts
+++ src/packages/core/src/options/server-options.ts
@@ -3,16 +3,6 @@ export {Apis, Flavors, FlavorMap} from "@ganache/flavors";
 
 export default interface ServerOptions extends ProviderOptions {
   /**
-   * The number of milliseconds of inactivity the http server needs to wait for
-   * additional incoming data, after it has finished writing the last response,
-   * before a socket will be destroyed. This does not affect the websocket
-   * server.
-   *
-   * A value of 0 will disable the keep-alive timeout behavior on incoming connections. Defaults to `5000`
-   */
-  keepAliveTimeout: number;
-
-  /**
    * Port number to listen on when running as a server. Defaults to `8545`
    */
   port: number;
@@ -26,7 +16,6 @@ export default interface ServerOptions extends ProviderOptions {
 export const getDefault = (options?: ServerOptions) => {
   return Object.assign(
     {
-      keepAliveTimeout: 5000,
       port: 8545,
       ws: true
     },
