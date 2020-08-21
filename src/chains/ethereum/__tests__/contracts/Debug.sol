8657a85a42d509411658a2d7d5a86254936b105e
Tim Coulter <tim@timothyjcoulter.com>
2020-08-21 14:16:39 -0700

    Committing what I have so we can pair on an error. Some changes:
    1. Clean up errors caught by compled solc. Bad solc.
    2. Add a debug contract that can create a lot of instructions in a loop
    3. Add a test that tries to check memory allocation of the trace function

---
+++ src/chains/ethereum/__tests__/contracts/Debug.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.6.1;
+
+contract Debug {
+  uint public value;
+
+  event ValueSet(uint);
+
+  constructor() public payable {
+    value = 5;
+  }
+
+  function setValue(uint val) public {
+    value = val;
+    emit ValueSet(val);
+  }
+
+  function loop(uint times) public {
+    for (uint i = 0; i < times; i++) {
+      value = i;
+    }
+  }
+
+
+}
--- src/chains/ethereum/__tests__/helpers/compile.ts
+++ src/chains/ethereum/__tests__/helpers/compile.ts
@@ -1,9 +1,21 @@
 
 import solc from "solc";
+
+// Clean up after solc. Looks like this never really got fixed:
+// https://github.com/chriseth/browser-solidity/issues/167
+var listeners = process.listeners("unhandledRejection");
+var solc_listener = listeners[listeners.length - 1];
+process.removeListener("unhandledRejection", solc_listener);
+
 import { readFileSync } from "fs-extra";
 import { parse } from "path";
 
-export default function compile(contractPath: string, contractName?: string) {
+export type CompileOutput = {
+  code:string;
+  contract:solc.CompilerOutputContracts[string][string]
+}
+
+export default function compile(contractPath: string, contractName?: string):CompileOutput {
   const parsedPath = parse(contractPath);
   const content = readFileSync(contractPath, {encoding: "utf8"});
   const globalName = parsedPath.base;
