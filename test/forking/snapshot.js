fc00bfd08d2e68eefb4c27c491691bbe7aed71a7
Mike Seese <seesemichaelj@gmail.com>
2020-09-01 17:50:20 -0700

    add failing snapshot tests

---
+++ test/forking/snapshot.js
@@ -0,0 +1,119 @@
+const assert = require("assert");
+const bootstrap = require("../helpers/contract/bootstrap");
+const intializeTestProvider = require("../helpers/web3/initializeTestProvider");
+
+/**
+ * NOTE: Naming in these tests is a bit confusing. Here, the "main chain"
+ * is the main chain the tests interact with; and the "forked chain" is the
+ * chain that _was forked_. This is in contrast to general naming, where the
+ * main chain represents the main chain to be forked (like the Ethereum live
+ * network) and the fork chaing being "the fork".
+ */
+
+async function takeSnapshot(web3) {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: "2.0",
+        method: "evm_snapshot",
+        id: new Date().getTime()
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result.id);
+      }
+    );
+  });
+}
+
+async function revertToSnapShot(web3, stateId) {
+  await new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: "2.0",
+        method: "evm_revert",
+        params: [stateId],
+        id: new Date().getTime()
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      }
+    );
+  });
+}
+
+describe("Forking Snapshots", () => {
+  let forkedContext;
+  let mainContext;
+  const logger = {
+    log: function(msg) {}
+  };
+
+  before("Set up forked provider with web3 instance and deploy a contract", async function() {
+    this.timeout(5000);
+
+    const contractRef = {
+      contractFiles: ["Snapshot"],
+      contractSubdirectory: "forking"
+    };
+
+    const ganacheProviderOptions = {
+      logger,
+      seed: "main provider"
+    };
+
+    forkedContext = await bootstrap(contractRef, ganacheProviderOptions);
+  });
+
+  before("Set up main provider and web3 instance", async function() {
+    const { provider: forkedProvider } = forkedContext;
+    mainContext = await intializeTestProvider({
+      fork: forkedProvider,
+      logger,
+      seed: "forked provider"
+    });
+  });
+
+  it("successfully manages storage slot deletion", async() => {
+    const { instance: forkedInstance, abi } = forkedContext;
+    const { web3: mainWeb3 } = mainContext;
+
+    const accounts = await mainWeb3.eth.getAccounts();
+    const instance = new mainWeb3.eth.Contract(abi, forkedInstance._address);
+    const txParams = {
+      from: accounts[0]
+    };
+
+    let value;
+
+    value = await instance.methods.value().call();
+    assert.equal(value, 0);
+
+    await instance.methods.test().send(txParams);
+
+    value = await instance.methods.value().call();
+    assert.equal(value, 1);
+
+    const snapshotId = await takeSnapshot(mainWeb3);
+
+    await instance.methods.test().send(txParams);
+
+    value = await instance.methods.value().call();
+    assert.equal(value, 2);
+
+    await revertToSnapShot(mainWeb3, snapshotId);
+
+    value = await instance.methods.value().call();
+    assert.equal(value, 0);
+
+    await instance.methods.test().send(txParams);
+
+    value = await instance.methods.value().call();
+    assert.equal(value, 1);
+  });
+});
