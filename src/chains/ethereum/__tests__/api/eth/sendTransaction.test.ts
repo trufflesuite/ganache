eb3f718c9f6ae8d8505188f6f6de28ac9b69c9cf
David Murdoch <david@davidmurdoch.com>
2020-09-03 18:35:42 -0400

    Ensure bad revert strings don't cause errors

--- src/chains/ethereum/__tests__/api/eth/sendTransaction.test.ts
+++ src/chains/ethereum/__tests__/api/eth/sendTransaction.test.ts
@@ -38,14 +38,28 @@ describe("api", () => {
           }
 
           it("doesn't crash on badly encoded revert string", async () => {
-            const provider = await getProvider();
-            const accounts = await provider.send("eth_accounts");
-            const {contract, contractAddress} = await deployContract(provider, accounts);
-            const contractMethods = contract.contract.evm.methodIdentifiers;
-            const value = await provider.send("eth_call", [
-              {from: accounts[0], to: contractAddress, data: "0x" + contractMethods["invalidRevertReason()"]}
-            ]);
-            assert.strictEqual(value, "0x08c379a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc0");
+            async function test(opts) {
+              const provider = await getProvider(opts as any);
+              const accounts = await provider.send("eth_accounts");
+              const {contract, contractAddress} = await deployContract(provider, accounts);
+              const contractMethods = contract.contract.evm.methodIdentifiers;
+              const prom = provider.send("eth_call", [
+                {from: accounts[0], to: contractAddress, data: "0x" + contractMethods["invalidRevertReason()"]}
+              ]);
+
+              const revertString = "0x08c379a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc0"
+              if (opts.vmErrorsOnRPCResponse) {
+                const result = await prom.catch(e =>e);
+                assert.strictEqual(result.code, -32000, "Error code should be -32000");
+                assert.strictEqual(result.data.reason, null, "The reason is undecodable, and thus should be null");
+                assert.strictEqual(result.data.message, "revert", "The message should not have a reason string included");
+                assert.strictEqual(result.data.result, revertString, "The revert reason should be encoded as hex");
+              } else {
+                assert.strictEqual(await prom, revertString, "The revert reason should be encoded as hex");
+              }
+            }
+            await test({vmErrorsOnRPCResponse: false});
+            await test({vmErrorsOnRPCResponse: true});
           });
         });
       });
