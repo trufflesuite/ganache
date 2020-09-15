1042c74f3e22c89236864085585cb90caaada026
David Murdoch <david@davidmurdoch.com>
2020-09-14 20:52:20 -0400

    feat: add `evm_lockUnknownAccount` and corresponding tests

--- test/local/accounts.js
+++ test/local/accounts.js
@@ -285,4 +285,53 @@ describe("Accounts", () => {
       await runTests(false);
     });
   });
+
+  describe("evm_lockUnknownAccount/evm_unlockUnknownAccount", () => {
+    let accounts, web3, send;
+    before(async ()=>{
+      const context = await initializeTestProvider();
+      accounts = context.accounts;
+      web3 = context.web3;
+      send = context.send;
+    });
+
+    it("should lock any unlocked unknown account via evm_lockUnknownAccount", async () => {
+      const address = "0x742d35Cc6634C0532925a3b844Bc454e4438f44e";
+      const {result: unlockResult} = await send("evm_unlockUnknownAccount", address);
+      assert.strictEqual(unlockResult, true);
+
+      const {result: lockResult1} = await send("evm_lockUnknownAccount", address);
+      assert.strictEqual(lockResult1, true);
+
+      // bonus: also make sure we return false when the account is already locked:
+      const {result: lockResult2} = await send("evm_lockUnknownAccount", address);
+      assert.strictEqual(lockResult2, false);
+    });
+
+    it("should not lock a known account via evm_lockUnknownAccount", async () => {
+      await assert.rejects(send("evm_lockUnknownAccount", accounts[0], {
+        message: "cannot lock known/personal account"
+      }));
+    });
+
+    it("should not lock a personal account via evm_lockUnknownAccount", async () => {
+      // create a new personal account
+      const {result: address} = await send("personal_newAccount","password");
+
+      // then explicitly unlock it
+      const {result} = await send("personal_unlockAccount", address, "password", 0);
+      assert.strictEqual(result, true);
+
+      // then try to lock it via evm_lockUnknownAccount
+      await assert.rejects(send("evm_lockUnknownAccount", address), {
+        message: "cannot lock known/personal account"
+      });
+    });
+
+    it("should return `false` upon lock if account isn't locked (unknown account)", async () => {
+      const address = "0x742d35Cc6634C0532925a3b844Bc454e4438f44e";
+      const {result} = await send("evm_lockUnknownAccount", address);
+      assert.strictEqual(result, false);
+    });
+  });
 });
