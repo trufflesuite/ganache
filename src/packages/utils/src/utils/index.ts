c2a46c3852b73a4a44d95cf43103228783e84a65
David Murdoch <david@davidmurdoch.com>
2020-09-22 12:16:43 -0400

    expose a `hasOwn` helper in utils

--- src/packages/utils/src/utils/index.ts
+++ src/packages/utils/src/utils/index.ts
@@ -3,5 +3,6 @@ export * from "./executor";
 export * from "./heap";
 export * from "./request-coordinator";
 export * from "./unref";
+export * from "./has-own";
 export * from "./uint-to-buffer";
 export * from "./constants";
