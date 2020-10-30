b9e52e8c6d915d5553e6a862d821e03046ee8737
David Murdoch <david@davidmurdoch.com>
2020-10-29 21:04:24 -0400

    worth a shot

--- src/chains/ethereum/__tests__/api/bzz/bzz.test.ts
+++ src/chains/ethereum/__tests__/api/bzz/bzz.test.ts
@@ -6,6 +6,8 @@ describe("api", () => {
   describe("bzz", () => {
     let provider: EthereumProvider;
     before(async () => {
+      // will this make GitHub Action's windwos-2019 Node v14 test work
+      await new Promise(resolve => setTimeout(resolve, 1000));
       provider = await getProvider();
     });
 
