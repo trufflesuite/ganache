f2d8000b3d2fbe77d3a987f5de8c352ec8151f4c
David Murdoch <david@davidmurdoch.com>
2020-10-30 11:35:20 -0400

    add timeout to first eth test

--- src/chains/ethereum/__tests__/api/bzz/bzz.test.ts
+++ src/chains/ethereum/__tests__/api/bzz/bzz.test.ts
@@ -5,7 +5,13 @@ import EthereumProvider from "../../../src/provider";
 describe("api", () => {
   describe("bzz", () => {
     let provider: EthereumProvider;
-    beforeEach(async () => {
+    before(async function () {
+      // GitHub Actions' windows-2019 Node v14 environment can sometimes take a
+      // VERY long time to run this `before`, as it is CURRENTLY the first
+      // @ganache/ethereum test that mocha runs (alphabetically)... and for some
+      // reason it is slow.
+      this.timeout(10000);
+
       // will this make GitHub Action's windwos-2019 Node v14 test work
       console.log("here a");
       console.log(Date.now());
