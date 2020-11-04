604f5d50407ee0cf9713a75eb0c3554d2899d68d
David Murdoch <david@davidmurdoch.com>
2020-11-04 14:43:36 -0500

    move `prepare` to `postinstall`

---
+++ scripts/postinstall.ts
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+console.log("");
+console.log(
+  chalk`{bold.cyan Tip:} {cyan run} {bold.yellow.dim source completions.sh} {cyan to supply bash completions for npm scripts}`
+);
+console.log("");
