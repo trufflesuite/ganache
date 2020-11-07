389b1b8b47d906392d756c43f5f443e3d4ba1878
David Murdoch <david@davidmurdoch.com>
2020-11-07 18:13:43 -0500

    built cli

--- src/packages/ganache/webpack.config.ts
+++ src/packages/ganache/webpack.config.ts
@@ -1,4 +1,5 @@
 import webpackBrowser from "./webpack/webpack.browser.config";
 import webpackNode from "./webpack/webpack.node.config";
+import webpackCli from "./webpack/webpack.cli.config";
 
-export default [webpackBrowser, webpackNode];
+export default [webpackBrowser, webpackNode, webpackCli];
