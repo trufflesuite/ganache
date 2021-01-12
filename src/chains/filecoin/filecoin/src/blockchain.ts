bfda36b5f3c0e6162705a339101fdfc0f7be5742
Mike Seese <seesemichaelj@gmail.com>
2021-01-12 00:00:08 -0800

    implement Filecoin.ChainNotify method

--- src/chains/filecoin/filecoin/src/blockchain.ts
+++ src/chains/filecoin/filecoin/src/blockchain.ts
@@ -19,6 +19,7 @@ import { FilecoinInternalOptions } from "@ganache/filecoin-options";
 
 export type BlockchainEvents = {
   ready(): void;
+  tipset: Tipset;
 };
 
 export default class Blockchain extends Emittery.Typed<
@@ -166,6 +167,8 @@ export default class Blockchain extends Emittery.Typed<
     }
 
     this.logLatestTipset();
+
+    this.emit("tipset", newTipset);
   }
 
   async hasLocal(cid: string): Promise<boolean> {
