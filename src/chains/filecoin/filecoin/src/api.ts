9b6ee438352f08cb29856070ab1b3dd4c18104fb
Mike Seese <seesemichaelj@gmail.com>
2021-01-12 00:00:08 -0800

    implement Filecoin.ChainNotify method

--- src/chains/filecoin/filecoin/src/api.ts
+++ src/chains/filecoin/filecoin/src/api.ts
@@ -1,5 +1,5 @@
 //#region Imports
-import { types } from "@ganache/utils";
+import { types, Quantity, PromiEvent } from "@ganache/utils";
 import Blockchain from "./blockchain";
 import {
   StorageProposal,
@@ -7,19 +7,23 @@ import {
 } from "./things/storage-proposal";
 import { SerializedRootCID, RootCID } from "./things/root-cid";
 import { SerializedDeal } from "./things/deal";
-import { SerializedTipset } from "./things/tipset";
+import { SerializedTipset, Tipset } from "./things/tipset";
 import { SerializedAddress } from "./things/address";
 import { SerializedMiner } from "./things/miner";
 import {
   SerializedRetrievalOffer,
   RetrievalOffer
 } from "./things/retrieval-offer";
+import Emittery from "emittery";
+import { HeadChange, HeadChangeType } from "./things/head-change";
 
 const _blockchain = Symbol("blockchain");
 
 export default class FilecoinApi implements types.Api {
   readonly [index: string]: (...args: any) => Promise<any>;
 
+  readonly #getId = (id => () => Quantity.from(++id))(0);
+  readonly #subscriptions = new Map<string, Emittery.UnsubscribeFn>();
   private readonly [_blockchain]: Blockchain;
 
   constructor(blockchain: Blockchain) {
@@ -36,6 +40,39 @@ export default class FilecoinApi implements types.Api {
 
   async "Filecoin.ChainHead"(): Promise<SerializedTipset> {
     return this[_blockchain].latestTipset().serialize();
+  }
+
+  "Filecoin.ChainNotify"(): PromiEvent<Quantity> {
+    const subscription = this.#getId();
+    const promiEvent = PromiEvent.resolve(subscription);
+
+    // There currently isn't an unsubscribe method,
+    // but it would go here
+    this.#subscriptions.set(subscription.toString(), () => {});
+
+    const currentHead = new HeadChange({
+      type: HeadChangeType.HCCurrent,
+      val: this[_blockchain].latestTipset()
+    });
+
+    this[_blockchain].on("tipset", (tipset: Tipset) => {
+      const newHead = new HeadChange({
+        type: HeadChangeType.HCApply,
+        val: tipset
+      });
+
+      promiEvent.emit("message", {
+        type: "xrpc.ch.val",
+        data: [subscription.toString(), [newHead.serialize()]]
+      });
+    });
+
+    promiEvent.emit("message", {
+      type: "xrpc.ch.val",
+      data: [subscription.toString(), [currentHead.serialize()]]
+    });
+
+    return promiEvent;
   }
 
   async "Filecoin.StateListMiners"(): Promise<Array<SerializedMiner>> {
---
+++ src/chains/filecoin/filecoin/src/things/head-change.ts
@@ -0,0 +1,61 @@
+import {
+  SerializableObject,
+  DeserializedObject,
+  Definitions,
+  SerializedObject
+} from "./serializable-object";
+import { Tipset, SerializedTipset } from "./tipset";
+
+interface HeadChangeConfig {
+  properties: {
+    type: {
+      type: string;
+      serializedType: string;
+      serializedName: "Type";
+    };
+    val: {
+      type: Tipset;
+      serializedType: SerializedTipset;
+      serializedName: "Val";
+    };
+  };
+}
+
+class HeadChange
+  extends SerializableObject<HeadChangeConfig>
+  implements DeserializedObject<HeadChangeConfig> {
+  get config(): Definitions<HeadChangeConfig> {
+    return {
+      type: {
+        serializedName: "Type",
+        defaultValue: (options = HeadChangeType.HCCurrent) => options
+      },
+      val: {
+        serializedName: "Val",
+        defaultValue: options => new Tipset(options)
+      }
+    };
+  }
+
+  constructor(
+    options?:
+      | Partial<SerializedObject<HeadChangeConfig>>
+      | Partial<DeserializedObject<HeadChangeConfig>>
+  ) {
+    super(options);
+  }
+
+  type: string;
+  val: Tipset;
+}
+
+type SerializedHeadChange = SerializedObject<HeadChangeConfig>;
+
+// Retrieved these from https://git.io/Jtvke
+export enum HeadChangeType {
+  HCRevert = "revert",
+  HCApply = "apply",
+  HCCurrent = "current"
+}
+
+export { HeadChange, SerializedHeadChange };
