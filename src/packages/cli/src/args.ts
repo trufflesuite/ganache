b5d627752bfe9be8fe7b4292310be19d2c83a25e
Mike Seese <seesemichaelj@gmail.com>
2021-01-15 17:53:44 -0800

    add server options to CLI

--- src/packages/cli/src/args.ts
+++ src/packages/cli/src/args.ts
@@ -6,6 +6,7 @@ import {
   Definitions,
   YargsPrimitiveCliTypeStrings
 } from "@ganache/options";
+import { serverDefaults } from "@ganache/core";
 import { Command, Argv } from "./types";
 import chalk from "chalk";
 import { EOL } from "os";
@@ -40,6 +41,70 @@ const addAliases = (args: yargs.Argv<{}>, aliases: string[], key: string) => {
   const options = { hidden: true, alias: key };
   return aliases.reduce((args, a) => args.option(a, options), args);
 };
+
+function processOption(
+  state: any,
+  category: string,
+  group: string,
+  option: string,
+  optionObj: Definitions<Base.Config>[string | number],
+  argv: yargs.Argv
+) {
+  if (optionObj.disableInCLI !== true) {
+    const shortHand = [];
+    const legacyAliases = [];
+
+    let description = highlight(optionObj.cliDescription || "");
+    if (optionObj.cliAliases) {
+      optionObj.cliAliases.forEach(alias => {
+        if (alias.length === 1) shortHand.push(alias);
+        else legacyAliases.push(alias);
+      });
+      description = chalk`${description}${EOL}{dim deprecated aliases: ${legacyAliases
+        .map(a => `--${a}`)
+        .join(", ")}}`;
+    }
+
+    const generateDefaultDescription = () => {
+      // default sometimes requires a config, so we supply one
+      return (state[option] = optionObj.default
+        ? optionObj.default(state).toString()
+        : undefined);
+    };
+    const defaultDescription =
+      optionObj.defaultDescription || generateDefaultDescription();
+
+    // we need to specify the type of each array so yargs properly casts
+    // the types held within each array
+    const { cliType } = optionObj;
+    const array = cliType && cliType.startsWith("array:"); // e.g. array:string or array:number
+    const type = (array
+      ? cliType.slice(6) // remove the "array:" part
+      : cliType) as YargsPrimitiveCliTypeStrings;
+
+    const options: Options = {
+      group,
+      description,
+      alias: shortHand,
+      defaultDescription,
+      array,
+      type,
+      choices: optionObj.cliChoices,
+      coerce: optionObj.cliCoerce
+    };
+
+    const key = `${category}.${option}`;
+
+    // First, create *hidden* deprecated aliases...
+    argv = addAliases(argv, legacyAliases, key);
+
+    // and *then* create the main option, as options added later take precedence
+    // example: `-d --wallet.seed 123` is invalid (mutally exclusive). If aliases are defined _after_
+    // the main option definition the error message will be `Arguments deterministic and wallet.seed are mutually exclusive`
+    // when it should be `Arguments wallet.deterministic and wallet.seed are mutually exclusive`
+    argv = argv.option(key, options);
+  }
+}
 
 export default function (version: string, isDocker: boolean) {
   let args = yargs
@@ -86,64 +151,20 @@ export default function (version: string, isDocker: boolean) {
           const state = {};
           for (const option in categoryObj) {
             const optionObj = categoryObj[option];
-            if (optionObj.disableInCLI !== true) {
-              const shortHand = [];
-              const legacyAliases = [];
-
-              let description = highlight(optionObj.cliDescription || "");
-              if (optionObj.cliAliases) {
-                optionObj.cliAliases.forEach(alias => {
-                  if (alias.length === 1) shortHand.push(alias);
-                  else legacyAliases.push(alias);
-                });
-                description = chalk`${description}${EOL}{dim deprecated aliases: ${legacyAliases
-                  .map(a => `--${a}`)
-                  .join(", ")}}`;
-              }
-
-              const generateDefaultDescription = () => {
-                // default sometimes requires a config, so we supply one
-                return (state[option] = optionObj.default
-                  ? optionObj.default(state).toString()
-                  : undefined);
-              };
-              const defaultDescription =
-                optionObj.defaultDescription || generateDefaultDescription();
-
-              // we need to specify the type of each array so yargs properly casts
-              // the types held within each array
-              const { cliType } = optionObj;
-              const array = cliType && cliType.startsWith("array:"); // e.g. array:string or array:number
-              const type = (array
-                ? cliType.slice(6) // remove the "array:" part
-                : cliType) as YargsPrimitiveCliTypeStrings;
-
-              const options: Options = {
-                group,
-                description,
-                alias: shortHand,
-                defaultDescription,
-                array,
-                type,
-                choices: optionObj.cliChoices,
-                coerce: optionObj.cliCoerce
-              };
-
-              const key = `${category}.${option}`;
-
-              // First, create *hidden* deprecated aliases...
-              flavorArgs = addAliases(flavorArgs, legacyAliases, key);
-
-              // and *then* create the main option, as options added later take precedence
-              // example: `-d --wallet.seed 123` is invalid (mutally exclusive). If aliases are defined _after_
-              // the main option definition the error message will be `Arguments deterministic and wallet.seed are mutually exclusive`
-              // when it should be `Arguments wallet.deterministic and wallet.seed are mutually exclusive`
-              flavorArgs = flavorArgs.option(key, options);
-            }
+            processOption(state, category, group, option, optionObj, flavorArgs);
           }
         }
 
-        // TODO: combine these cli options with core's `ServerOptions`
+        const state = {};
+        const categoryObj = serverDefaults.server;
+        const options = Object.keys(categoryObj);
+        const group = "Server:";
+        for (const option of options) {
+          const optionObj = categoryObj[option];
+
+          processOption(state, "server", group, option, optionObj, flavorArgs);
+        }
+
         flavorArgs = flavorArgs
           .option("server.host", {
             group: "Server:",
