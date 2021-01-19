2364d316fc673553d010eddc5fbe90699242a9e4
David Murdoch <david@davidmurdoch.com>
2021-01-19 10:26:30 -0500

    fix: ensure path sep is always / in link-ts-references.ts
    Windows uses "\\"

--- scripts/link-ts-references.ts
+++ scripts/link-ts-references.ts
@@ -89,7 +89,7 @@ function updateConfig(config: PackageInfo) {
   // add package.json deps to tsconfig references:
   references.forEach(name => {
     const referenceConfig = getConfigByName(name);
-    // projects that are references by other projects must have the `composite: true` in their tsconfig compileOptions
+    // projects that are referenced by other projects must have the `composite: true` in their tsconfig compileOptions
     if (
       !referenceConfig.tsConfig.compilerOptions ||
       !referenceConfig.tsConfig.compilerOptions.composite
@@ -100,7 +100,7 @@ function updateConfig(config: PackageInfo) {
       referenceConfig.modified = true;
     }
     const absPath = referenceConfig.path;
-    const relPath = relative(path, absPath);
+    const relPath = relative(path, absPath).replace(/\\/g, "/"); // only posix paths, please
     const alreadyExists = tsConfigReferences.some(tsConfigReference => {
       const existingRefPath = resolve(path, tsConfigReference.path);
       if (absPath === existingRefPath) {
