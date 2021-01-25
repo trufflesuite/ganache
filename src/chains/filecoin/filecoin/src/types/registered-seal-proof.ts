8b1b78e873c15e902fba25b3b3f9123b6d7fd31e
Mike Seese <seesemichaelj@gmail.com>
2021-01-25 14:58:21 -0800

    add StateMinerInfo method

---
+++ src/chains/filecoin/filecoin/src/types/registered-seal-proof.ts
@@ -0,0 +1,13 @@
+// Reference implementation: https://git.io/JtnP0
+export enum RegisteredSealProof {
+  StackedDrg2KiBV1 = 0,
+  StackedDrg8MiBV1 = 1,
+  StackedDrg512MiBV1 = 2,
+  StackedDrg32GiBV1 = 3,
+  StackedDrg64GiBV1 = 4,
+  StackedDrg2KiBV1_1 = 5,
+  StackedDrg8MiBV1_1 = 6,
+  StackedDrg512MiBV1_1 = 7,
+  StackedDrg32GiBV1_1 = 8,
+  StackedDrg64GiBV1_1 = 9
+}
