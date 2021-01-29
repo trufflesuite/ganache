5a7ecc2f9145cdedd325ea3b6a52afd91a7ba9c9
Mike Seese <seesemichaelj@gmail.com>
2021-01-29 14:32:53 -0800

    support strictNullCheck and noImplicitAny

--- src/chains/filecoin/filecoin/src/things/miner-info.ts
+++ src/chains/filecoin/filecoin/src/things/miner-info.ts
@@ -68,40 +68,48 @@ type MinerInfoConfig = {
   };
 };
 
-class MinerInfo
-  extends SerializableObject<MinerInfoConfig>
-  implements DeserializedObject<MinerInfoConfig> {
-  get config(): Definitions<MinerInfoConfig> {
+type C = MinerInfoConfig;
+
+class MinerInfo extends SerializableObject<C> implements DeserializedObject<C> {
+  get config(): Definitions<C> {
     return {
       owner: {
+        deserializedName: "owner",
         serializedName: "Owner",
         defaultValue: "t01000"
       },
       worker: {
+        deserializedName: "worker",
         serializedName: "Worker",
         defaultValue: "t01000"
       },
       newWorker: {
+        deserializedName: "newWorker",
         serializedName: "NewWorker",
         defaultValue: "<empty>" // This is how lotus-devnet responds to StateMinerInfo
       },
       controlAddresses: {
+        deserializedName: "controlAddresses",
         serializedName: "ControlAddresses",
         defaultValue: []
       },
       workerChangeEpoch: {
+        deserializedName: "workerChangeEpoch",
         serializedName: "WorkerChangeEpoch",
         defaultValue: config => (typeof config !== "undefined" ? config : -1)
       },
       peerId: {
+        deserializedName: "peerId",
         serializedName: "PeerId",
         defaultValue: "0" // defaulting this to 0 as we don't have any p2p technology in Ganache
       },
       multiaddrs: {
+        deserializedName: "multiaddrs",
         serializedName: "Multiaddrs",
         defaultValue: []
       },
       sealProofType: {
+        deserializedName: "sealProofType",
         serializedName: "SealProofType",
         defaultValue: config =>
           typeof config !== "undefined"
@@ -109,18 +117,54 @@ class MinerInfo
             : RegisteredSealProof.StackedDrg2KiBV1_1
       },
       sectorSize: {
+        deserializedName: "sectorSize",
         serializedName: "SectorSize",
         defaultValue: 2048 // sectors/sector sizes don't really matter in Ganache; defaulting to 2 KiB (lotus-devnet default)
       },
       windowPoStPartitionSectors: {
+        deserializedName: "windowPoStPartitionSectors",
         serializedName: "WindowPoStPartitionSectors",
         defaultValue: config => (typeof config !== "undefined" ? config : 0)
       },
       consensusFaultElapsed: {
+        deserializedName: "consensusFaultElapsed",
         serializedName: "ConsensusFaultElapsed",
         defaultValue: config => (typeof config !== "undefined" ? config : -1)
       }
     };
+  }
+
+  constructor(
+    options?: Partial<SerializedObject<C>> | Partial<DeserializedObject<C>>
+  ) {
+    super();
+
+    this.owner = super.initializeValue(this.config.owner, options);
+    this.worker = super.initializeValue(this.config.worker, options);
+    this.newWorker = super.initializeValue(this.config.newWorker, options);
+    this.controlAddresses = super.initializeValue(
+      this.config.controlAddresses,
+      options
+    );
+    this.workerChangeEpoch = super.initializeValue(
+      this.config.workerChangeEpoch,
+      options
+    );
+    this.peerId = super.initializeValue(this.config.peerId, options);
+    this.multiaddrs = super.initializeValue(this.config.multiaddrs, options);
+    this.sealProofType = super.initializeValue(
+      this.config.sealProofType,
+      options
+    );
+    this.sectorSize = super.initializeValue(this.config.sectorSize, options);
+    this.windowPoStPartitionSectors = super.initializeValue(
+      this.config.windowPoStPartitionSectors,
+      options
+    );
+    this.consensusFaultElapsed = super.initializeValue(
+      this.config.consensusFaultElapsed,
+      options
+    );
   }
 
   /**
@@ -148,6 +192,6 @@ class MinerInfo
   consensusFaultElapsed: number;
 }
 
-type SerializedMinerInfo = SerializedObject<MinerInfoConfig>;
+type SerializedMinerInfo = SerializedObject<C>;
 
 export { MinerInfo, SerializedMinerInfo };
