e112f504d065d487cd10e411fdec2bc23ad9703e
Mike Seese <seesemichaelj@gmail.com>
2021-02-04 20:06:51 -0800

    change RandomNumberGenerate.getBuffer to be [0, 255] inclusive

--- src/packages/utils/src/utils/random-number-generator.ts
+++ src/packages/utils/src/utils/random-number-generator.ts
@@ -11,27 +11,27 @@ export class RandomNumberGenerator {
     }
   }
 
-  getNumber(range: number = 1): number {
+  getNumber(upperExclusiveBound: number = 1): number {
     // I believe this check may be a tiny bit faster than
     // always multiplying by 1
-    if (range !== 1) {
-      return this.rng() * range;
+    if (upperExclusiveBound !== 1) {
+      return this.rng() * upperExclusiveBound;
     } else {
       return this.rng();
     }
   }
 
-  getNumbers(length: number, range: number = 1): number[] {
+  getNumbers(length: number, upperExclusiveBound: number = 1): number[] {
     const numbers: number[] = [];
 
     for (let i = 0; i < length; i++) {
-      numbers.push(this.getNumber(range));
+      numbers.push(this.getNumber(upperExclusiveBound));
     }
 
     return numbers;
   }
 
   getBuffer(length: number): Buffer {
-    return Buffer.from(this.getNumbers(length, 255));
+    return Buffer.from(this.getNumbers(length, 256));
   }
 }
