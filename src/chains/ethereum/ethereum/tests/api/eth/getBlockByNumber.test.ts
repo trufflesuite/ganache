f77d256642dc741e772613e75835963bc1d18d2c
eggplantzzz <tmfeickert@gmail.com>
2021-02-17 11:05:06 -0500

    Update some tests

--- src/chains/ethereum/ethereum/tests/api/eth/getBlockByNumber.test.ts
+++ src/chains/ethereum/ethereum/tests/api/eth/getBlockByNumber.test.ts
@@ -3,21 +3,31 @@ import EthereumProvider from "../../../src/provider";
 import getProvider from "../../helpers/getProvider";
 import compile from "../../helpers/compile";
 const DEFAULT_DIFFICULTY = 1;
+let provider: EthereumProvider;
+
+// helper to mine an arbitrary number of blocks using dummy transactions
+const mineBlocks = async (number: number, provider: EthereumProvider) => {
+  const [from] = await provider.send("eth_accounts");
+  for (let x = number; x > 0; x--) {
+    await provider.send("eth_subscribe", ["newHeads"]);
+
+    await provider.send("eth_sendTransaction", [{ from, to: from }]);
+
+    await provider.once("message");
+  }
+};
 
 describe("api", () => {
   describe("eth", () => {
     describe("getBlockByNumber", () => {
-      let provider: EthereumProvider;
-
-      before(async () => {
-        provider = await getProvider();
-      });
-
-      after(async () => {
-        provider && (await provider.disconnect());
-      });
-
       describe("difficulty", () => {
+        beforeEach(async () => {
+          provider = await getProvider();
+        });
+
+        afterEach(async () => {
+          provider && (await provider.disconnect());
+        });
         it("returns the block difficulty", async () => {
           const block = await provider.send("eth_getBlockByNumber", ["latest"]);
           assert.strictEqual(block.difficulty, `0x${DEFAULT_DIFFICULTY}`);
@@ -25,9 +35,63 @@ describe("api", () => {
       });
 
       describe("totalDifficulty", () => {
-        it("equals the block difficulty for the genesis block", async () => {
-          const block = await provider.send("eth_getBlockByNumber", ["0x0"]);
-          assert.strictEqual(block.totalDifficulty, `0x${DEFAULT_DIFFICULTY}`);
+        describe("when the default values are used", () => {
+          beforeEach(async () => {
+            provider = await getProvider();
+          });
+          afterEach(async () => {
+            provider && (await provider.disconnect());
+          });
+
+          it("equals the block difficulty for the genesis block", async () => {
+            const block = await provider.send("eth_getBlockByNumber", ["0x0"]);
+            assert.strictEqual(
+              block.totalDifficulty,
+              `0x${DEFAULT_DIFFICULTY}`
+            );
+          });
+          it("equals the sum of the difficulty of all blocks (hex)", async () => {
+            const numberOfBlocksToMine = Math.floor(Math.random() * 10 + 1);
+            await mineBlocks(numberOfBlocksToMine, provider);
+            const block = await provider.send("eth_getBlockByNumber", [
+              `0x${numberOfBlocksToMine}`
+            ]);
+            assert.strictEqual(
+              block.totalDifficulty,
+              `0x${((numberOfBlocksToMine + 1) * DEFAULT_DIFFICULTY).toString(
+                16
+              )}`
+            );
+          });
+        });
+
+        describe("when the difficulty is set manually", () => {
+          let difficulty;
+          beforeEach(async () => {
+            difficulty = Math.floor(Math.random() * 9 + 1);
+            provider = await getProvider({
+              miner: { difficulty }
+            });
+          });
+          afterEach(async () => {
+            provider && (await provider.disconnect());
+          });
+
+          it("equals the block difficulty for the genesis block", async () => {
+            const block = await provider.send("eth_getBlockByNumber", ["0x0"]);
+            assert.strictEqual(block.totalDifficulty, `0x${difficulty}`);
+          });
+          it("equals the sum of the difficulty of all blocks (hex)", async () => {
+            const numberOfBlocksToMine = Math.floor(Math.random() * 10 + 1);
+            await mineBlocks(numberOfBlocksToMine, provider);
+            const block = await provider.send("eth_getBlockByNumber", [
+              `0x${numberOfBlocksToMine}`
+            ]);
+            assert.strictEqual(
+              block.totalDifficulty,
+              `0x${((numberOfBlocksToMine + 1) * difficulty).toString(16)}`
+            );
+          });
         });
       });
     });
