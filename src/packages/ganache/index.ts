385f5e8b0fb76b8b2ad5e9f6de261bda866f60b9
HL <36248745+honestbonsai@users.noreply.github.com>
2021-02-22 16:54:04 +0000

    Add comment

--- src/packages/ganache/index.ts
+++ src/packages/ganache/index.ts
@@ -1,3 +1,9 @@
+/*
+ * This file is the entry point for the resultant bundle dist/node/ganache.min.js
+ * dist/cli/ganache.min.js will then point dist/node/ganache.min.js
+ * whenever it references @ganache/core.
+ * This is so we avoid an extra set of native node modules in dist/cli, just use what's in dist/node.
+ */
 import Ganache from "@ganache/core";
 
 export default {
