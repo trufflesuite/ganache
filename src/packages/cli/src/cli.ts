1c4bea3b480b48087e0364d2b681849c7d7726a5
satyajeetkolhapure <satyajeet.kolhapure@consensys.net>
2021-02-24 18:13:00 +0000

    fixed typescript error

--- src/packages/cli/src/cli.ts
+++ src/packages/cli/src/cli.ts
@@ -7,8 +7,8 @@ import args from "./args";
 import { EthereumFlavorName, TezosFlavorName } from "@ganache/flavors";
 import initializeEthereum from "./initialize/ethereum";
 import initializeTezos from "./initialize/tezos";
-import TezosProvider from "@ganache/tezos/src/provider";
-import EthereumProvider from "@ganache/ethereum/src/provider";
+import { Provider as TezosProvider } from "@ganache/tezos";
+import { Provider as EthereumProvider } from "@ganache/ethereum";
 
 const logAndForceExit = (messages: any[], exitCode = 0) => {
   // https://nodejs.org/api/process.html#process_process_exit_code
