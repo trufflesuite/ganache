05bc5f4a2d4d546518d8f2a9f67fa89bea0ce3ca
eggplantzzz <tmfeickert@gmail.com>
2021-02-24 11:15:02 -0500

    Add some more tests for legacy input formats

--- src/chains/ethereum/options/tests/index.test.ts
+++ src/chains/ethereum/options/tests/index.test.ts
@@ -3,7 +3,7 @@ import { ethereumDefaults, EthereumOptionsConfig } from "../src";
 
 describe("EthereumOptionsConfig", () => {
   describe(".normalize", () => {
-    it("returns an options object with default namespaces", () => {
+    it("returns an options object with all default namespaces", () => {
       const options = EthereumOptionsConfig.normalize({});
       for (const namespace in ethereumDefaults) {
         assert(options[namespace]);
@@ -15,14 +15,40 @@ describe("EthereumOptionsConfig", () => {
       });
       assert.equal(options.wallet.totalAccounts, 7);
     });
-    describe("option conflicts", () => {
-      it("throws an error when an option conflict is found", () => {
+    it("throws an error when an option conflict is found", () => {
+      assert.throws(() => {
+        EthereumOptionsConfig.normalize({
+          wallet: {
+            deterministic: true,
+            seed: "there I oft spent narrow nightwatch nigh the ship's head"
+          }
+        } as Object);
+      });
+    });
+
+    describe("legacy input formats", () => {
+      it("accepts some legacy input formats", () => {
+        const seed = "from their voids, cry to the dolphined sea";
+        const options = EthereumOptionsConfig.normalize({ seed } as Object);
+        assert.equal(options.wallet.seed, seed);
+      });
+      it("errors if there is a conflict with legacy inputs", () => {
+        const seed = "I ate three cookies";
+        const mnemonic = "fee fi fo fum";
         assert.throws(() => {
-          EthereumOptionsConfig.normalize({
-            wallet: {
-              deterministic: true,
-              seed: "there I oft spent narrow nightwatch nigh the ship's head"
-            }
+          const options = EthereumOptionsConfig.normalize({
+            seed,
+            mnemonic
+          } as Object);
+        });
+      });
+      it("errors if there is a conflict with legacy and modern inputs", () => {
+        const seed = "I ate three cookies";
+        const mnemonic = "fee fi fo fum";
+        assert.throws(() => {
+          const options = EthereumOptionsConfig.normalize({
+            seed,
+            wallet: { mnemonic }
           } as Object);
         });
       });
