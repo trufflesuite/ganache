e0c397537e6a3471bb8d6e89a6ea40c78de73a8e
Erin Shaben <eshaben@icloud.com>
2021-02-27 01:17:02 -0700

    add address description and example for eth_getTransactionCount

--- src/chains/ethereum/ethereum/src/api.ts
+++ src/chains/ethereum/ethereum/src/api.ts
@@ -2194,18 +2194,18 @@ export default class EthereumApi implements types.Api {
   /**
    * Returns the number of transactions sent from an address.
    *
-   * @param address `DATA`, 20 Bytes - The address to get number of transactions sent from
-   * @param blockNumber Integer block number, or the string "latest", "earliest"
-   * or "pending".
-   * @returns Number of transactions sent from this address.
+   * @param address DATA, 20 bytes - the address to get number of transactions sent from
+   * @param blockNumber integer block number, or the string "latest", "earliest"
+   * or "pending", see the default block parameter
+   * @returns integer of the number of transactions sent from this address.
    * @example
    * ```javascript
-   * const [from, to] = await provider.request({ method: "eth_accounts", params: [] });
+   * const accounts = await provider.request({ method: "eth_accounts", params: [] });
    * await provider.request({ method: "eth_subscribe", params: ["newHeads"] });
-   * await provider.request({ method: "eth_sendTransaction", params: [{ from, to, gas: "0x5b8d80" }] });
+   * await provider.request({ method: "eth_sendTransaction", params: [{ from: accounts[0], to: accounts[1], gas: "0x5b8d80" }] });
    * await provider.once("message"); // Note: `await provider.once` is non-standard
    *
-   * const txCount = await provider.request({ method: "eth_getTransactionCount", params: [ from, "latest" ] });
+   * const txCount = await provider.request({ method: "eth_getTransactionCount", params: [ accounts[0], "latest" ] });
    * console.log(txCount);
    * ```
    */
