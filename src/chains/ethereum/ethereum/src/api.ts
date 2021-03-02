753fd9f8ce81acae7424bf8342f53a420ad9b729
Erin Shaben <eshaben@icloud.com>
2021-03-02 00:52:15 -0700

    add example for evm_lockUnknownAccount

--- src/chains/ethereum/ethereum/src/api.ts
+++ src/chains/ethereum/ethereum/src/api.ts
@@ -497,8 +497,8 @@ export default class EthereumApi implements types.Api {
    * @example
    * ```javascript
    * const address = "0x742d35Cc6634C0532925a3b844Bc454e4438f44e";
-   * const result = await provider.send("evm_lockUnknownAccount", [address] );
-   * console.log(result);
+   * const result = await provider.send("evm_lockUnknownAccount", [address]);
+   * console.log(result)
    * ```
    */
   async evm_lockUnknownAccount(address: DATA) {
