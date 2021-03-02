fbb412966c38b679036a789cf43fa5098a4d5bff
Erin Shaben <eshaben@icloud.com>
2021-03-02 00:45:01 -0700

    add example for evm_increaseTime

--- src/chains/ethereum/ethereum/src/api.ts
+++ src/chains/ethereum/ethereum/src/api.ts
@@ -329,7 +329,7 @@ export default class EthereumApi implements types.Api {
    * @example
    * ```javascript
    * const seconds = 10;
-   * const timeAdjustment = await provider.send("evm_increaseTime", [seconds] );
+   * const timeAdjustment = await provider.send("evm_increaseTime", [seconds]);
    * console.log(timeAdjustment);
    * ```
    */
