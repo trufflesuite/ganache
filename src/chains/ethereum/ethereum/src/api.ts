f745bb3a7867f7fa8890abca90beaaf00c87b248
Erin Shaben <eshaben@icloud.com>
2021-03-02 00:52:15 -0700

    add example for evm_lockUnknownAccount

--- src/chains/ethereum/ethereum/src/api.ts
+++ src/chains/ethereum/ethereum/src/api.ts
@@ -500,6 +500,12 @@ export default class EthereumApi implements types.Api {
    * @returns `true` if the account was locked successfully, `false` if the
    * account was already locked. Throws an error if the account could not be
    * locked.
+   * @example
+   * ```javascript
+   * const address = "0x742d35Cc6634C0532925a3b844Bc454e4438f44e";
+   * const result = await provider.send("evm_lockUnknownAccount", [address]);
+   * console.log(result)
+   * ```
    */
   async evm_lockUnknownAccount(address: string) {
     const lowerAddress = address.toLowerCase();
