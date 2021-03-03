0f7dd184a11f78d36a618353f33ebdfa916c2f94
Erin Shaben <eshaben@icloud.com>
2021-03-02 22:28:34 -0700

    add example for shh_addToGroup

--- src/chains/ethereum/ethereum/src/api.ts
+++ src/chains/ethereum/ethereum/src/api.ts
@@ -2496,10 +2496,14 @@ export default class EthereumApi implements types.Api {
   }
 
   /**
-   * Adds a whisper identity to the group
+   * Adds a whisper identity to the group.
    *
    * @param {DATA, 60 Bytes} - The identity address to add to a group.
-   * @returns true if the identity was successfully added to the group, otherwise false.
+   * @returns True if the identity was successfully added to the group, otherwise false.
+   * @example
+   * ```javascript
+   * console.log(await provider.send("shh_addToGroup", ["0x0"]));
+   * ```
    */
   @assertArgLength(1)
   async shh_addToGroup(address: string) {
