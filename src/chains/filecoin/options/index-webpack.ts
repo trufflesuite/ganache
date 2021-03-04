081e62de662985e520ccd5e27c367410197d511f
Mike Seese <seesemichaelj@gmail.com>
2021-03-04 09:27:20 -0800

    webpack @ganache/filecoin-options to use in UI

---
+++ src/chains/filecoin/options/index-webpack.ts
@@ -0,0 +1,14 @@
+/*!
+ * @ganache/filecoin-options
+ *
+ * @copyright Truffle Blockchain Group
+ * @author Tim Coulter
+ * @license MIT
+ */
+
+import { FilecoinDefaults, FilecoinOptionsConfig } from "./src";
+
+export default {
+  FilecoinDefaults,
+  FilecoinOptionsConfig
+};
