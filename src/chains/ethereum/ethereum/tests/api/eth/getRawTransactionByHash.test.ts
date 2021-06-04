6d33f4e901369ce583a1e51cf3cd3c0fe4a065d6
MicaiahReid <micaiahreid@gmail.com>
2021-06-04 16:53:49 -0400

    Write tests for eth_getRawTransactionByHash

--- src/chains/ethereum/ethereum/tests/api/eth/getRawTransactionByHash.test.ts
+++ src/chains/ethereum/ethereum/tests/api/eth/getRawTransactionByHash.test.ts
@@ -1,6 +1,6 @@
 import assert from "assert";
-import Common from "ethereumjs-common";
-import Transaction from "ethereumjs-tx/dist/transaction";
+import Transaction from "@ethereumjs/tx/dist/legacyTransaction";
+import Common from "@ethereumjs/common";
 import EthereumProvider from "../../../src/provider";
 import getProvider from "../../helpers/getProvider";
 
@@ -10,6 +10,16 @@ describe("api", () => {
       let provider: EthereumProvider;
       let tx;
       let txHash;
+      const common = Common.forCustomChain(
+        "mainnet",
+        {
+          name: "ganache",
+          chainId: 1337,
+          comment: "Local test network",
+          bootstrapNodes: []
+        },
+        "petersburg"
+      );
 
       before(async () => {
         provider = await getProvider();
@@ -23,21 +33,21 @@ describe("api", () => {
         await provider.once("message");
       });
 
-      after(async () => {
-        provider && (await provider.disconnect());
-      });
-
       it("should return the tx as a hex string", async () => {
         const rawTx = await provider.send("eth_getRawTransactionByHash", [
           txHash
         ]);
-        const common = new Common(1337, "muirGlacier");
-        const t = new Transaction(rawTx, { common });
-        console.log(t);
         assert.strictEqual(
           rawTx,
           "0xf86680847735940083015f9094c8879bccbe23ab4d188de4bbdc87b8992368f8b98080820a95a034dfad968bde79b31e360b1659d34172cd95e46248c86def51e3479ff09deb2aa01644668d0ea3ae5ce6ab3bb112c64b13c37dd2f5c2c7a217b586be591dbd2b06"
         );
+      });
+      it("should return null if the transaction hash can't be found", async () => {
+        const invalidHash = txHash.concat("0");
+        const rawTx = await provider.send("eth_getRawTransactionByHash", [
+          invalidHash
+        ]);
+        assert.strictEqual(rawTx, null);
       });
     });
   });
