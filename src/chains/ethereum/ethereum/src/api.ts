fb6ec4f0d9742732fbdb11281c096e644a328701
MicaiahReid <micaiahreid@gmail.com>
2021-06-15 10:29:14 -0400

    Remove todo

--- src/chains/ethereum/ethereum/src/api.ts
+++ src/chains/ethereum/ethereum/src/api.ts
@@ -2684,7 +2684,6 @@ export default class EthereumApi implements types.Api {
     );
   }
 
-  // TODO: example doesn't return correct value
   /**
    * Validate the given passphrase and submit transaction.
    *
