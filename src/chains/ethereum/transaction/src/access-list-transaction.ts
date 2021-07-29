23cbdd1adc056e593d27d154f0b80a89442b78b9
MicaiahReid <micaiahreid@gmail.com>
2021-07-29 12:36:35 -0400

    customize sign/hash function for type. process raw data

--- src/chains/ethereum/transaction/src/access-list-transaction.ts
+++ src/chains/ethereum/transaction/src/access-list-transaction.ts
@@ -3,15 +3,13 @@ import { Address } from "@ganache/ethereum-address";
 import type Common from "@ethereumjs/common";
 import { BaseTransaction } from "./base-transaction";
 import { BN } from "ethereumjs-util";
+import { ecsign } from "ethereumjs-util";
 import { Hardfork } from "./hardfork";
 import { Params } from "./params";
 import { TypedRpcTransaction } from "./rpc-transaction";
+import { encodeRange, digest } from "@ganache/rlp";
 import { RuntimeTransaction } from "./runtime-transaction";
-import {
-  EthereumRawAccessListTx,
-  EthereumRawTx,
-  TypedRawTransaction
-} from "./raw";
+import { RawAccessListTx, RawLegacyTx, TypedRawTransaction } from "./raw";
 import {
   AccessListEIP2930Transaction,
   AccessList,
@@ -20,7 +18,7 @@ import {
 import { AccessLists } from "./access-lists";
 import { computeInstrinsicsAccessListTx } from "./signing";
 
-const { BUFFER_EMPTY, BUFFER_32_ZERO } = utils;
+const { keccak, BUFFER_EMPTY, BUFFER_32_ZERO, RPCQUANTITY_EMPTY } = utils;
 
 const MAX_UINT64 = 1n << (64n - 1n);
 
@@ -29,14 +27,42 @@ export class AccessListTransaction extends RuntimeTransaction {
   public accessList: AccessListBuffer;
   public accessListJSON: AccessList;
   public gasPrice: Quantity;
+  public type: Quantity = Quantity.from("0x1");
 
   public constructor(
-    data: EthereumRawAccessListTx | TypedRpcTransaction,
+    data: RawAccessListTx | TypedRpcTransaction,
     common: Common
   ) {
     super(data, common);
     if (Array.isArray(data)) {
+      this.type = Quantity.from(data[0]);
+      this.nonce = Quantity.from(data[1], true);
       this.gasPrice = Quantity.from(data[2]);
+      this.gas = Quantity.from(data[3]);
+      this.to = data[3].length == 0 ? RPCQUANTITY_EMPTY : Address.from(data[4]);
+      this.value = Quantity.from(data[5]);
+      const accessListData = AccessLists.getAccessListData(data[6]);
+      this.accessList = accessListData.accessList;
+      this.accessListJSON = accessListData.AccessListJSON;
+      this.data = Data.from(data[7]);
+      this.v = Quantity.from(data[8]);
+      this.r = Quantity.from(data[9]);
+      this.s = Quantity.from(data[10]);
+
+      const {
+        from,
+        serialized,
+        hash,
+        encodedData,
+        encodedSignature
+      } = this.computeIntrinsics(this.v, data, this.common.chainId());
+
+      this.from = from;
+      this.raw = data;
+      this.serialized = serialized;
+      this.hash = hash;
+      this.encodedData = encodedData;
+      this.encodedSignature = encodedSignature;
     } else {
       this.gasPrice = Quantity.from(data.gasPrice);
       const accessListData = AccessLists.getAccessListData(data.accessList);
@@ -64,7 +90,10 @@ export class AccessListTransaction extends RuntimeTransaction {
     };
   };
 
-  public static fromTxData(data: TypedRpcTransaction, common: Common) {
+  public static fromTxData(
+    data: RawAccessListTx | TypedRpcTransaction,
+    common: Common
+  ) {
     return new AccessListTransaction(data, common);
   }
 
@@ -113,18 +142,63 @@ export class AccessListTransaction extends RuntimeTransaction {
       }
     };
   }
+  /**
+   * sign a transaction with a given private key, then compute and set the `hash`.
+   *
+   * @param privateKey - Must be 32 bytes in length
+   */
+  public signAndHash(privateKey: Buffer) {
+    if (this.v != null) {
+      throw new Error(
+        "Internal Error: RuntimeTransaction `sign` called but transaction has already been signed"
+      );
+    }
 
+    const chainId = this.common.chainId();
+    const raw: RawAccessListTx = this.toEthRawTransaction(
+      Quantity.from(chainId).toBuffer(),
+      BUFFER_EMPTY,
+      BUFFER_EMPTY
+    );
+    const data = encodeRange(raw, 0, 8);
+    const dataLength = data.length;
+
+    const ending = encodeRange(raw, 8, 3);
+    const msgHash = keccak(
+      digest([data.output, ending.output], dataLength + ending.length)
+    );
+    const sig = ecsign(msgHash, privateKey, chainId);
+    this.v = Quantity.from(sig.v);
+    this.r = Quantity.from(sig.r);
+    this.s = Quantity.from(sig.s);
+
+    raw[8] = this.v.toBuffer();
+    raw[9] = this.r.toBuffer();
+    raw[10] = this.s.toBuffer();
+
+    this.raw = raw;
+    const encodedSignature = encodeRange(raw, 6, 3);
+    this.serialized = digest(
+      [data.output, encodedSignature.output],
+      dataLength + encodedSignature.length
+    );
+    this.hash = Data.from(keccak(this.serialized));
+    this.encodedData = data;
+    this.encodedSignature = encodedSignature;
+  }
   public toEthRawTransaction(
     v?: Buffer,
     r?: Buffer,
     s?: Buffer
-  ): EthereumRawTx {
+  ): RawAccessListTx {
     return [
+      this.type.toBuffer(),
       this.nonce.toBuffer(),
       this.gasPrice.toBuffer(),
       this.gas.toBuffer(),
       this.to.toBuffer(),
       this.value.toBuffer(),
+      this.accessList,
       this.data.toBuffer(),
       v ? v : this.v.toBuffer(),
       r ? r : this.r.toBuffer(),
@@ -137,6 +211,6 @@ export class AccessListTransaction extends RuntimeTransaction {
     raw: TypedRawTransaction,
     chainId: number
   ) {
-    computeInstrinsicsAccessListTx(v, <EthereumRawAccessListTx>raw, chainId);
+    return computeInstrinsicsAccessListTx(v, <RawAccessListTx>raw, chainId);
   }
 }
