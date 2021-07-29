69dd2859dae412b34ce58c2c7248b79617707387
MicaiahReid <micaiahreid@gmail.com>
2021-07-29 12:37:48 -0400

    clean tx factory logic and add helper functions

--- src/chains/ethereum/transaction/src/transaction-factory.ts
+++ src/chains/ethereum/transaction/src/transaction-factory.ts
@@ -9,16 +9,13 @@ import { RuntimeTransaction } from "./runtime-transaction";
 import { LegacyTransaction } from "./legacy-transaction";
 import { AccessListTransaction } from "./access-list-transaction";
 import { TypedRpcTransaction } from "./rpc-transaction";
-import {
-  EthereumRawAccessListTx,
-  EthereumRawTx,
-  TypedRawTransaction
-} from "./raw";
+import { RawAccessListTx, RawLegacyTx, TypedRawTransaction } from "./raw";
 import { TypedTransaction } from "./transaction-types";
 
 const { BUFFER_EMPTY, BUFFER_32_ZERO } = utils;
 
 const MAX_UINT64 = 1n << (64n - 1n);
+
 export class TransactionFactory {
   /**
    * Create a transaction from a `txData` object
@@ -31,31 +28,73 @@ export class TransactionFactory {
     common: Common
   ): TypedTransaction {
     if (Array.isArray(txData)) {
-      // 9 is the length of the legacy EthereumRawTx
-      // if it's 9, there's no type. Otherwise, the first
-      // item in the array is the type.
-      if (txData.length === 9) {
-        return LegacyTransaction.fromTxData(txData, common);
+      const txType = this.typeOfRaw(txData);
+      if (txType === LegacyTransaction) {
+        return LegacyTransaction.fromTxData(<RawLegacyTx>txData, common);
+      } else if (txType === AccessListTransaction) {
+        return AccessListTransaction.fromTxData(
+          <RawAccessListTx>txData,
+          common
+        );
       } else {
+        throw new Error(`Tx instantiation with supplied type not supported`);
       }
     } else {
-      if (!("type" in txData) || txData.type === undefined) {
-        // Assume legacy transaction
+      const txType = this.typeOfRPC(txData);
+      if (txType === LegacyTransaction) {
         return LegacyTransaction.fromTxData(txData, common);
+      } else if (txType === AccessListTransaction) {
+        return AccessListTransaction.fromTxData(txData, common);
       } else {
-        const txType = parseInt(txData.type); // TODO
-        if (txType === 0) {
-          return LegacyTransaction.fromTxData(txData, common);
-        } else if (txType === 1) {
-          return AccessListTransaction.fromTxData(txData, common);
-          // } else if (txType === 2) {
-          //   return FeeMarketEIP1559Transaction.fromTxData(
-          //     <FeeMarketEIP1559TxData>txData,
-          //     txOptions
-          //   );
-        } else {
-          throw new Error(`Tx instantiation with type ${txType} not supported`);
-        }
+        throw new Error(
+          `Tx instantiation with type ${txData.type} not supported`
+        );
+      }
+    }
+  }
+  public static typeOfRaw(raw: TypedRawTransaction) {
+    // 9 is the length of the legacy EthereumRawTx
+    // if it's 9, there's no type. Otherwise, the first
+    // item in the array is the type.
+    if (raw.length === 9) {
+      return LegacyTransaction;
+    } else {
+      const first = raw[0];
+      let txType;
+      if (first.length > 0) {
+        txType = first.readInt8(0);
+      } else {
+        // a tx type of 0x0 stored as a buffer will just be empty
+        return LegacyTransaction;
+      }
+      if (txType === 1) {
+        return AccessListTransaction;
+      }
+    }
+  }
+  public static typeOfRPC(rpc: TypedRpcTransaction) {
+    if (!("type" in rpc) || rpc.type === undefined) {
+      // Assume legacy transaction
+      return LegacyTransaction;
+    } else {
+      const txType = parseInt(rpc.type); // TODO
+      if (txType === 0) {
+        return LegacyTransaction;
+      } else if (txType === 1) {
+        return AccessListTransaction;
+      }
+    }
+  }
+  public static txIncludesType(tx: TypedRpcTransaction | TypedRawTransaction) {
+    if (Array.isArray(tx)) {
+      if (tx.length === 9) {
+        return false;
+      } else return true;
+    } else {
+      if (!("type" in tx) || tx.type === undefined) {
+        return false;
+      } else {
+        return true;
       }
     }
   }
