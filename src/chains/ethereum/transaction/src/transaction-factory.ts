f836e1b65e9db69e23da76f9ec39757179ae465d
MicaiahReid <micaiahreid@gmail.com>
2021-08-05 12:42:31 -0400

    separate factory methods for txs by data type

--- src/chains/ethereum/transaction/src/transaction-factory.ts
+++ src/chains/ethereum/transaction/src/transaction-factory.ts
@@ -9,6 +9,7 @@ import { TypedRpcTransaction } from "./rpc-transaction";
 import {
   RawAccessListPayload,
   RawAccessListTx,
+  RawLegacyPayload,
   RawLegacyTx,
   TypedRawTransaction
 } from "./raw";
@@ -17,115 +18,100 @@ import { decode } from "@ganache/rlp";
 
 const MAX_UINT64 = 1n << (64n - 1n);
 
-const UNTYPED_TX_START_BYTE = parseInt("0xc0"); // all txs with first byte >= 0xc0 are untyped
-const LEGACY_TX_TYPE_ID = parseInt("0x0");
-const ACCESS_LIST_TX_TYPE_ID = parseInt("0x1");
+const UNTYPED_TX_START_BYTE = 0xc0; // all txs with first byte >= 0xc0 are untyped
+const LEGACY_TX_TYPE_ID = 0x0;
+const ACCESS_LIST_TX_TYPE_ID = 0x1;
 
 export class TransactionFactory {
   /**
    * Create a transaction from a `txData` object
    *
-   * @param txData - The transaction data. The `type` field will determine which transaction type is returned (if undefined, creates a legacy transaction)
+   * @param txData - The rpc transaction data. The `type` field will determine which transaction type is returned (if undefined, creates a legacy transaction)
    * @param common - Options to pass on to the constructor of the transaction
    */
-  public static fromTxData(
-    txData: TypedRpcTransaction | TypedRawTransaction | PrefixedHexString,
-    common: Common
-  ): TypedTransaction {
-    if (typeof txData === "string") {
-      let data = Data.from(txData).toBuffer();
-      const txType = this.typeOfString(txData);
-      if (txType === LegacyTransaction) {
-        const raw = decode<RawLegacyTx>(data);
-        return LegacyTransaction.fromTxData(raw, common);
-      } else if (txType === AccessListTransaction) {
-        const type = data.slice(0, 1);
-        data = data.slice(1, data.length); // remove type because it's not rlp encoded and thus can't be decoded
-        const decoded = decode<RawAccessListPayload>(data);
-        const raw = <RawAccessListTx>[type, ...decoded.slice(0)]; // add the type back to our raw data
+  public static fromRpc(txData: TypedRpcTransaction, common: Common) {
+    const txType = this.typeOfRPC(txData);
+    if (txType === LegacyTransaction) {
+      return LegacyTransaction.fromTxData(txData, common);
+    } else if (txType === AccessListTransaction) {
+      return AccessListTransaction.fromTxData(txData, common);
+    } else {
+      throw new Error(`Tx instantiation with supplied type not supported`);
+    }
+  }
+  /**
+   * Create a transaction from a `txData` object
+   *
+   * @param txData - The raw transaction data. The `type` field will determine which transaction type is returned (if undefined, creates a legacy transaction)
+   * @param common - Options to pass on to the constructor of the transaction
+   */
+  public static fromRaw(txData: TypedRawTransaction, common: Common) {
+    const type = txData[0][0];
+    const data = txData.slice(1, txData.length); // remove type because it's not rlp encoded and thus can't be decoded
+    const txType = this.typeOf(type);
+    if (txType === LegacyTransaction) {
+      return LegacyTransaction.fromTxData(<RawLegacyPayload>data, common);
+    } else if (txType === AccessListTransaction) {
+      return AccessListTransaction.fromTxData(
+        <RawAccessListPayload>data,
+        common
+      );
+    } else {
+      throw new Error(`Tx instantiation with type ${type} not supported`);
+    }
+  }
+  /**
+   * Create a transaction from a `txData` object
+   *
+   * @param txData - The raw hex string transaction data. The `type` field will determine which transaction type is returned (if undefined, creates a legacy transaction)
+   * @param common - Options to pass on to the constructor of the transaction
+   */
+  public static fromString(txData: string, common: Common) {
+    let data = Data.from(txData).toBuffer();
+    const type = data[0];
+    const txType = this.typeOf(type);
+    if (txType === LegacyTransaction) {
+      // type 0x0 toBuffer() is going to be empty, meaning
+      // a type was sent. strip it away
+      if (type === undefined) {
+        data = data.slice(1, data.length);
+      }
+      const raw = decode<RawLegacyPayload>(data);
+      return LegacyTransaction.fromTxData(<RawLegacyPayload>raw, common);
+    } else {
+      data = data.slice(1, data.length); // remove type because it's not rlp encoded and thus can't be decoded
+      if (txType === AccessListTransaction) {
+        const raw = decode<RawAccessListPayload>(data);
         return AccessListTransaction.fromTxData(raw, common);
       } else {
-        throw new Error(`Tx instantiation with supplied type not supported`);
-      }
-    } else if (Array.isArray(txData)) {
-      const txType = this.typeOfRaw(txData);
-      if (txType === LegacyTransaction) {
-        return LegacyTransaction.fromTxData(<RawLegacyTx>txData, common);
-      } else if (txType === AccessListTransaction) {
-        return AccessListTransaction.fromTxData(
-          <RawAccessListTx>txData,
-          common
-        );
-      } else {
-        throw new Error(`Tx instantiation with supplied type not supported`);
-      }
-    } else {
-      const txType = this.typeOfRPC(txData);
-      if (txType === LegacyTransaction) {
-        return LegacyTransaction.fromTxData(txData, common);
-      } else if (txType === AccessListTransaction) {
-        return AccessListTransaction.fromTxData(txData, common);
-      } else {
-        throw new Error(
-          `Tx instantiation with type ${txData.type} not supported`
-        );
+        throw new Error(`Tx instantiation with type ${type} not supported`);
       }
     }
   }
 
-  public static typeOfString(str: PrefixedHexString) {
-    const firstByte = parseInt(str.substr(0, 4));
-    if (firstByte >= UNTYPED_TX_START_BYTE || firstByte === LEGACY_TX_TYPE_ID) {
+  public static typeOf(type: number) {
+    if (
+      type >= UNTYPED_TX_START_BYTE ||
+      type === LEGACY_TX_TYPE_ID ||
+      type === undefined
+    ) {
       return LegacyTransaction;
-    } else if (firstByte === ACCESS_LIST_TX_TYPE_ID) {
+    } else if (type === ACCESS_LIST_TX_TYPE_ID) {
       return AccessListTransaction;
     }
   }
 
   public static typeOfRaw(raw: TypedRawTransaction) {
-    // 9 is the length of the legacy EthereumRawTx
-    // if it's 9, there's no type. Otherwise, the first
-    // item in the array is the type.
-    if (raw.length === 9) {
-      return LegacyTransaction;
-    } else {
-      const first = raw[0];
-      let txType;
-      if (first.length > 0) {
-        txType = first.readInt8(0);
-      } else {
-        // a tx type of 0x0 stored as a buffer will just be empty
-        return LegacyTransaction;
-      }
-      if (txType === 1) {
-        return AccessListTransaction;
-      }
-    }
+    const type = raw[0][0];
+    return this.typeOf(type);
   }
+
   public static typeOfRPC(rpc: TypedRpcTransaction) {
     if (!("type" in rpc) || rpc.type === undefined) {
-      // Assume legacy transaction
       return LegacyTransaction;
     } else {
       const txType = parseInt(rpc.type);
-      if (txType === 0) {
-        return LegacyTransaction;
-      } else if (txType === 1) {
-        return AccessListTransaction;
-      }
-    }
-  }
-  public static txIncludesType(tx: TypedRpcTransaction | TypedRawTransaction) {
-    if (Array.isArray(tx)) {
-      if (tx.length === 9) {
-        return false;
-      } else return true;
-    } else {
-      if (!("type" in tx) || tx.type === undefined) {
-        return false;
-      } else {
-        return true;
-      }
+      return this.typeOf(txType);
     }
   }
 }
