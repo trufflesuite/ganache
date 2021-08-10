e643c017e8f24adc67ce45bbc0e435ef0055eaa4
David Murdoch <david@davidmurdoch.com>
2021-08-10 13:46:50 -0400

    try fixing shrinkwraps

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,6 +1,40 @@
 // using `require` because everything in scripts uses typescript's default
 // compiler settings, and this module requires enabling `esModuleInterop`
 const chalk = require("chalk");
+import { execSync, ExecSyncOptions } from "child_process";
+
+const cwd = process.cwd();
+const execArgs: ExecSyncOptions = { cwd, stdio: "inherit" };
+
+// run `lerna bootstrap`
+let lernaUseCi: boolean = false;
+if (process.env.npm_config_argv) {
+  let npmArgs: { cooked?: string[] };
+  try {
+    npmArgs = JSON.parse(process.env.npm_config_argv);
+  } catch {}
+  if (
+    npmArgs != null &&
+    Array.isArray(npmArgs.cooked) &&
+    npmArgs.cooked.length > 0
+  ) {
+    lernaUseCi = npmArgs.cooked.includes("ci");
+  }
+}
+
+execSync(`$(npm bin)/lerna bootstrap --ci=${lernaUseCi}`, execArgs);
+
+// lerna breaks yarn.lock/package-lock.json/npm-shrnikwrap.json files
+// by omitting our internal packages from them. We can fix this by
+// running `npm i --only-package-lock` in each package
+if (!lernaUseCi) {
+  execSync(`$(npm bin)/lerna exec --concurrency 1 -- npm shrinkwrap`, execArgs);
+}
+
+// update typescript project references
+require("./link-ts-references");
+
+execSync("npm run tsc", execArgs);
 
 console.log("");
 console.log(
