1fccc7858041d551e2f733f971cf884647f165e6
MicaiahReid <micaiahreid@gmail.com>
2021-08-26 23:52:47 -0400

    add eip1559 raw and rpc types

--- src/chains/ethereum/transaction/src/raw.ts
+++ src/chains/ethereum/transaction/src/raw.ts
@@ -33,17 +33,39 @@ export type EIP2930AccessListDatabasePayload = [
   s: Buffer
 ];
 
+export type EIP1559FeeMarketDatabasePayload = [
+  chainId: Buffer,
+  nonce: Buffer,
+  maxPriorityFeePerGas: Buffer,
+  maxFeePerGas: Buffer,
+  gas: Buffer,
+  to: Buffer,
+  value: Buffer,
+  data: Buffer,
+  accessList: AccessListBuffer,
+  v: Buffer,
+  r: Buffer,
+  s: Buffer
+];
+
 export type EIP2930AccessListDatabaseTx = Concat<
   TxType,
   EIP2930AccessListDatabasePayload
 >;
 
+export type EIP1559FeeMarketDatabaseTx = Concat<
+  TxType,
+  EIP1559FeeMarketDatabasePayload
+>;
+
 export type TypedDatabaseTransaction =
   | LegacyDatabasePayload
-  | EIP2930AccessListDatabaseTx;
+  | EIP2930AccessListDatabaseTx
+  | EIP1559FeeMarketDatabaseTx;
 export type TypedDatabasePayload =
   | LegacyDatabasePayload
-  | EIP2930AccessListDatabasePayload;
+  | EIP2930AccessListDatabasePayload
+  | EIP1559FeeMarketDatabasePayload;
 
 /**
  * Extra data Ganache stores as part of a transaction in order to support
