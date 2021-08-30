d5d7df183a1094049275fbe81f3852a3ca1dfbea
MicaiahReid <micaiahreid@gmail.com>
2021-08-30 14:59:41 -0400

    refine baseFeePerGas formula

--- src/chains/ethereum/block/src/block-params.ts
+++ src/chains/ethereum/block/src/block-params.ts
@@ -7,14 +7,14 @@ export const BlockParams = {
   /**
    *  Base fee per gas for blocks without a parent containing a base fee per gas.
    */
-  INITIAL_BASE_FEE_PER_GAS: Quantity.from(1000000000).toBuffer(),
+  INITIAL_BASE_FEE_PER_GAS: 1000000000n,
   /**
    * Divisor used to set a block's target gas usage.
    */
-  ELASTICITY: 2,
+  ELASTICITY: 2n,
 
   /**
    * Divisor used to limit the amount the base fee per gas can change from one block to another.
    */
-  BASE_FEE_MAX_CHANGE_DENOMINATOR: 8
+  BASE_FEE_MAX_CHANGE_DENOMINATOR: 8n
 };
