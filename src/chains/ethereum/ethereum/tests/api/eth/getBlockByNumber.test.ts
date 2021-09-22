a5e3fd4b1ddcab77d6e8e24262a3307ca03b0800
MicaiahReid <micaiahreid@gmail.com>
2021-09-22 08:13:12 -0400

    initial pass at tests for pending block

--- src/chains/ethereum/ethereum/tests/api/eth/getBlockByNumber.test.ts
+++ src/chains/ethereum/ethereum/tests/api/eth/getBlockByNumber.test.ts
@@ -1,6 +1,7 @@
 import assert from "assert";
 import { EthereumProvider } from "../../../src/provider";
 import getProvider from "../../helpers/getProvider";
+import { Quantity } from "@ganache/utils";
 const DEFAULT_DIFFICULTY = 1;
 let provider: EthereumProvider;
 
@@ -95,6 +96,112 @@ describe("api", () => {
               `Mined total difficulty, ${block.totalDifficulty} differs from sum of preceding block's difficulties.`
             );
           });
+        });
+      });
+
+      describe(`passing "pending" tag`, () => {
+        beforeEach(async () => {
+          const optionsJson = {
+            wallet: {
+              deterministic: true
+            }
+          };
+          provider = await getProvider(optionsJson);
+        });
+        afterEach(async () => {
+          provider && (await provider.disconnect());
+        });
+
+        describe(`before transactions are sent`, () => {
+          it(`gets the block after "latest" block`, async () => {
+            const pendingBlock = await provider.send("eth_getBlockByNumber", ["pending"]); // prettier-ignore
+            const latestBlock = await provider.send("eth_getBlockByNumber", ["latest"]); // prettier-ignore
+            assert.strictEqual(
+              Quantity.from(pendingBlock.number).toNumber(),
+              Quantity.from(latestBlock.number).toNumber() + 1
+            );
+          });
+        });
+
+        describe(`with transactions queued to be mined`, () => {
+          beforeEach(async () => {
+            const optionsJson = {
+              wallet: {
+                deterministic: true
+              },
+              miner: {
+                blockGasLimit: "0xF618", // 63000, or sending three empty txs
+                blockTime: 60
+              }
+            };
+            provider = await getProvider(optionsJson);
+          });
+          it(`returns a block with as many queued transactions that will fit in the block`, async () => {
+            await provider.send("miner_stop"); // pause the miner so we can queue up some txs
+            const [to, from] = await provider.send("eth_accounts");
+            const tx = {
+              to: to,
+              from: from,
+              gasPrice: "0x3B9ACAFF",
+              nonce: null,
+              gas: "0x5208"
+            };
+            // we'll send 4 txs, more than will fit into the block
+            for (let i = 0; i < 4; i++) {
+              tx.nonce = "0x" + i.toString();
+              await provider.send("eth_sendTransaction", [tx]);
+            }
+            const pendingBlock = await provider.send("eth_getBlockByNumber", ["pending", true]); // prettier-ignore
+            const pendingBlockTxs = pendingBlock.transactions;
+            // only 3 of the 4 txs are on the pending block, cause that's all that fits in a block
+            assert.strictEqual(pendingBlockTxs.length, 3);
+            for (let i = 0; i < 3; i++) {
+              // the txs are ordered how they would be in a real block, by nonce
+              assert.strictEqual(pendingBlockTxs[i].nonce, "0x" + i.toString());
+            }
+          });
+        });
+
+        describe.only(`while transactions are being mined`, () => {
+          it(`returns a block that is identical to (the "latest" block after the transactions are mined)`, async () => {
+            await provider.send("miner_stop"); // pause the miner so we can queue up some txs
+            await provider.send("eth_subscribe", ["newHeads"]);
+            const [to, from] = await provider.send("eth_accounts");
+            const tx = {
+              to: to,
+              from: from,
+              gasPrice: "0x3B9ACAFF",
+              nonce: null,
+              gas: "0x5208"
+            };
+
+            // send 3 txs
+            for (let i = 0; i < 3; i++) {
+              tx.nonce = "0x" + i.toString();
+              await provider.send("eth_sendTransaction", [tx]);
+            }
+            provider.send("miner_start"); // don't await so we can get the pending block while mining is taking place
+
+            const pendingBlock = await provider.send("eth_getBlockByNumber", ["pending", true]); // prettier-ignore
+            const pendingBlockTxs = pendingBlock.transactions;
+            //await provider.once("message"); // make sure all the txs are mined and a new block is made
+            const latestBlock = await provider.send("eth_getBlockByNumber", ["latest", true]); // prettier-ignore
+            const latestBlockTxs = latestBlock.transactions;
+
+            // pending block before mining completes is equal to latest after mining completes
+            assert.strictEqual(pendingBlock.number, latestBlock.number);
+            // all three txs were mined for creation of the pending block
+            assert.strictEqual(pendingBlockTxs.length, 3);
+            // all three txs were mined for the latest block
+            assert.strictEqual(latestBlockTxs.length, 3);
+            for (let i = 0; i < 3; i++) {
+              // the txs in each block are identical
+              assert.strictEqual(
+                pendingBlockTxs[i].hash,
+                latestBlockTxs[i].hash
+              );
+            }
+          }).timeout(50000);
         });
       });
     });
