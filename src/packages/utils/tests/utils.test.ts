4a9ccbc84da7d2c25326cc09c11aa68c40b5b634
MicaiahReid <micaiahreid@gmail.com>
2021-10-28 14:47:09 -0400

    fix tests

--- src/packages/utils/tests/utils.test.ts
+++ src/packages/utils/tests/utils.test.ts
@@ -1,27 +1,28 @@
 "use strict";
 const oldWarn = console.warn;
+let failedToLoad = false;
+console.warn = e => {
+  console.log(e);
+  if (
+    e ===
+    "bigint: Failed to load bindings, pure JS will be used (try npm run rebuild?)"
+  ) {
+    failedToLoad = true;
+  }
+};
+
 import { bufferToBigInt } from "../src/utils/buffer-to-bigint";
-import "assert";
 import assert from "assert";
 
 const utils = require("..");
 
-describe("@ganache/utils", () => {
+describe.only("@ganache/utils", () => {
   describe("bigint-buffer library", () => {
-    before(() => {
-      console.warn = e => {
-        if (
-          e ===
-          "bigint: Failed to load bindings, pure JS will be used (try npm run rebuild?)"
-        ) {
-          assert.fail(e);
-        }
-      };
-    });
+    before(() => {});
     it("loads binaries on all platforms", () => {
       const buf = Buffer.from([255, 0]);
       const bigint = bufferToBigInt(buf);
-      console.warn("test warn");
+      assert.strictEqual(failedToLoad, false);
       assert.strictEqual(bigint, 65280n);
     });
     after(() => {
