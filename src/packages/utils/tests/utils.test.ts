acd60e8582421d028deb3b83d13ecd00a8bc5138
MicaiahReid <micaiahreid@gmail.com>
2021-11-01 15:19:10 -0400

    correct bigint-buffer import

--- src/packages/utils/tests/utils.test.ts
+++ src/packages/utils/tests/utils.test.ts
@@ -12,10 +12,10 @@ describe.only("@ganache/utils", () => {
 
     it("loads without warnings", () => {
       // make sure we're actually loading this module and not using a cached version
-      delete require.cache[require.resolve("bigint-buffer")];
+      delete require.cache[require.resolve("@trufflesuite/bigint-buffer")];
       // if prebuilt binaries aren't properly installed, we'll get a warning from
       // this lib saying that the JS fallback is being used.
-      require("bigint-buffer");
+      require("@trufflesuite/bigint-buffer");
       // so we'll spy on console.warn to ensure that our bigint-buffer warning
       // is never called when loading this library
       assert.strictEqual(spy.withArgs(BIGINT_ERROR).callCount, 0);
