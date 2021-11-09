6faae0e133bb33a7a1888e1a1f599428f5075f2a
David Murdoch <david@davidmurdoch.com>
2021-11-09 13:01:54 -0500

    fix commit suggestion typo

--- src/chains/ethereum/options/src/fork-options.ts
+++ src/chains/ethereum/options/src/fork-options.ts
@@ -73,8 +73,9 @@ export type ForkConfig = {
     };
 
     /**
-     * When the `fork.blockNumber` is set to "latest" (default), the number 
-     * of blocks before the remote node's "latest" block to fork from.
+     * Minimum age in seconds of the "latest" block. If the "latest" block is
+     * younger than this amount the block immediately preceding the latest block
+     * will be used instead.
      */
     preLatestConfirmations: {
       type: number;
@@ -172,7 +173,7 @@ export type ForkConfig = {
     };
 
     /**
-     * Disables caching of all forking requests.
+     * Don't cache forking requests in a persistent db.
      *
      * @default false
      */
@@ -305,7 +306,8 @@ Alternatively, you can use the \`fork.username\` and \`fork.password\` options.`
   },
   preLatestConfirmations: {
     normalize,
-    cliDescription: `When the `fork.blockNumber` is set to "latest" (default), the number of blocks before the remote node's "latest" block to fork from.`,
+    cliDescription:
+      'When the `fork.blockNumber` is set to "latest" (default), the number of blocks before the remote node\'s "latest" block to fork from.',
     default: () => 5,
     defaultDescription: "5",
     cliType: "number"
