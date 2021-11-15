65aa049ece133c3b0682feb07e0830d9e7ed31ed
David Murdoch <david@davidmurdoch.com>
2021-11-15 15:08:25 -0500

    add infura-credentials.ts file

---
+++ src/chains/ethereum/ethereum/src/forking/infura-credentials.ts
@@ -0,0 +1,5 @@
+// This file is replaced during release.
+// This file is in .gitignore, so changes aren't committed by default. If you
+// need to commit changes to this file you must use `git add --force`.
+export const INFURA_KEY = process.env.INFURA_KEY;
+export const INFURA_SECRET = process.env.INFURA_SECRET;
