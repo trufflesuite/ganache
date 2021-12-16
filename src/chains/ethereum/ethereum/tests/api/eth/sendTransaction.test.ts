31c26ad52432b144aab77b0295dd6814074767f3
David Murdoch <david@davidmurdoch.com>
2021-12-15 21:11:40 -0500

    add test

--- src/chains/ethereum/ethereum/tests/api/eth/sendTransaction.test.ts
+++ src/chains/ethereum/ethereum/tests/api/eth/sendTransaction.test.ts
@@ -47,14 +47,14 @@ describe("api", () => {
       });
 
       describe("insufficient funds", () => {
-        it("returns an error when account has insufficient funds to send the transaction", async () => {
+        const types = ["0x0", "0x1", "0x2"] as const;
+        it("returns an error when account has insufficient funds to transfer the value", async () => {
           const p = await getProvider({
             miner: { legacyInstamine: true },
             chain: { vmErrorsOnRPCResponse: true }
           });
           const [from, to] = await p.send("eth_accounts");
           const balance = await p.send("eth_getBalance", [from]);
-          const types = ["0x0", "0x1", "0x2"] as const;
           for (let i = 0; i < types.length; i++) {
             await assert.rejects(
               p.send("eth_sendTransaction", [
@@ -65,6 +65,24 @@ describe("api", () => {
                   balance
                 )} \\(vm hf=london -> block -> tx\\)`
               )
+            );
+          }
+        });
+
+        it("returns insufficient funds error when the package doesn't have enough funds to send the transaction", async () => {
+          const p = await getProvider();
+          const [from, to] = await p.send("eth_accounts");
+          for (let i = 0; i < types.length; i++) {
+            let tx = {
+              type: types[i],
+              from,
+              to,
+              // a potentially super expensive transaction
+              gasLimit: `0x${(12_000_000 - 1).toString(16)}`
+            };
+            await assert.rejects(
+              p.send("eth_sendTransaction", [tx]),
+              new RegExp(`insufficient funds`)
             );
           }
         });
