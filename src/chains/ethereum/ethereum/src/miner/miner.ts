25e63d81df3e76c93909e9d5cabce43b88203eb2
David Murdoch <david@davidmurdoch.com>
2022-03-02 21:45:28 -0500

    wait for blocks to be 100% included before mining the next

--- src/chains/ethereum/ethereum/src/miner/miner.ts
+++ src/chains/ethereum/ethereum/src/miner/miner.ts
@@ -247,7 +247,7 @@ export default class Miner extends Emittery<{
           [],
           storageKeys
         );
-        this.emit("block", finalizedBlockData);
+        await this.emit("block", finalizedBlockData);
         this.#reset();
         return { block: finalizedBlockData.block, transactions: [] };
       }
@@ -414,7 +414,7 @@ export default class Miner extends Emittery<{
         storageKeys
       );
       block = finalizedBlockData.block;
-      this.emit("block", finalizedBlockData);
+      await this.emit("block", finalizedBlockData);
 
       if (onlyOneBlock) {
         this.#currentlyExecutingPrice = 0n;
