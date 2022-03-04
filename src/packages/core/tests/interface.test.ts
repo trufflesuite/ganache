dd99e2f827a935a686c97703e2d815ea8baf780d
David Murdoch <david@davidmurdoch.com>
2022-03-04 17:30:40 -0500

    improve the tests

--- src/packages/core/tests/interface.test.ts
+++ src/packages/core/tests/interface.test.ts
@@ -1,11 +1,14 @@
 import Ganache from "..";
 import * as assert from "assert";
+import { KNOWN_CHAINIDS } from "@ganache/utils";
 
 describe("interface", () => {
-  it("has an interface", () => {
+  it.only("has an interface", () => {
     assert.ok(Ganache.server);
     assert.ok(Ganache.provider);
-    assert.ok(Ganache.__experimental_info);
+    const info = Ganache.__experimental_info();
+    assert.deepStrictEqual(info.fork.supportedChainIds, Array.from(KNOWN_CHAINIDS));
+    assert.deepStrictEqual(info.version, "DEV");
     assert.strictEqual(Object.keys(Ganache).length, 3);
 
     // in v3 these two properties were *removed* because it was confusing.
