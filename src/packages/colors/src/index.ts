9bafbb909154dabeef35a9188b0b5f3177f4a4b9
David Murdoch <david@davidmurdoch.com>
2022-04-08 13:31:52 -0400

    Add some missing colors

--- src/packages/colors/src/index.ts
+++ src/packages/colors/src/index.ts
@@ -12,5 +12,14 @@ export const TruffleColors = {
   /**
    * Infura orange
    */
-  infura: "#ff6b4a"
+  infura: "#ff6b4a",
+
+  /**
+   * Drizzle pink
+   */
+  drizzle: "#e911bd",
+
+  green: "#00D717",
+  yellow: "#F2E941",
+  watermelon: "#E86591"
 };
