9cff47c98bdaa72e59b6a2c883c2df9730e63e3f
MicaiahReid <micaiahreid@gmail.com>
2022-05-26 22:54:07 -0400

    remove boilerplate comment

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,4 @@ export = (app: Probot) => {
     });
     await context.octokit.issues.createComment(issueComment);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
