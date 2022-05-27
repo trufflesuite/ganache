2cd4848e772ca9d61ef76999a29992c77ac844cb
MicaiahReid <micaiahreid@gmail.com>
2022-05-26 23:04:39 -0400

    make user say please on trufbot request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Probot } from "probot";
 import QueryManager from "./query-manager";
+import { sayPlease } from "./utils/utils";
 
 export = (app: Probot) => {
   app.on("issue_comment.created", async context => {
@@ -8,6 +9,16 @@ export = (app: Probot) => {
     const issue = context.payload.issue;
     const pr = await queryManager.fetchPr(issue.number);
     if (!pr) return;
+
+    if ("pull_request" in issue) {
+      const comment = context.payload.comment;
+      // this is probably a request for TrufBot to do something
+      if (comment.body.includes("@TrufBot")) {
+        const lower = comment.body.toLocaleLowerCase();
+        if (!lower.includes("please")) {
+          return await queryManager.makeIssueComment(sayPlease());
+        }
+      }
     }
   });
 };
