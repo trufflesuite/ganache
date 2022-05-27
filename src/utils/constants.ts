b5e7a10acc291b9f9ea3d38fadf3437e654c7309
MicaiahReid <micaiahreid@gmail.com>
2022-05-26 23:06:36 -0400

    add merge issue comment constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+export const MERGE_DIRTY_COMMENT =
+  "This PR cannot be merged; all conflicts must be resolved before merging.";
+
+export const MERGE_BLOCKED_COMMENT =
+  "This PR cannot be merged; all checks must pass before merging.";
+
+export const MERGE_UNKNOWN_COMMENT =
+  "This PR cannot be merged; the PR's status is unknown because Github still hasn't computed its mergeability status. Try again later.";
+
+export const MERGE_BEHIND_COMMENT =
+  "This PR cannot be merged; head branch is behind the base branch. Rebase your branch and try again.";
+
+export const MERGE_NOT_COLLABORATOR =
+  "You must be a collaborator on this repository to merge PRs.";
+
