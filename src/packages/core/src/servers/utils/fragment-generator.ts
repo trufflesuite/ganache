50d04b90167385d861fb0193a0a6bc64d6ab04ba
David Murdoch <david@davidmurdoch.com>
2022-05-31 15:46:25 -0400

    add clarifying comments

--- src/packages/core/src/servers/utils/fragment-generator.ts
+++ src/packages/core/src/servers/utils/fragment-generator.ts
@@ -8,6 +8,8 @@ export function* getFragmentGenerator(
 
   for (const value of data) {
     const { byteLength } = value;
+    // if we don't have enough space in the buffer for this next value `yield`
+    // the buffer as is.
     if (offset > 0 && byteLength + offset > bufferSize) {
       yield buf.subarray(0, offset);
       // Reset the buffer. Since `uws` sends packets asynchronously,
@@ -17,10 +19,11 @@ export function* getFragmentGenerator(
       // handle backpressure. Part of handling backpressure will
       // involve the drain event and only sending while
       // `ws.getBufferedAmount() < ACCEPTABLE_BACKPRESSURE`.
-      // See https://github.com/trufflesuite/ganache/issues/FIX THIS
+      // See https://github.com/trufflesuite/ganache/issues/2790
       buf = null;
       offset = 0;
     }
+
     // Store value in buffer if it fits (but don't store it if it is the exact
     // same size as bufferSize)
     if (byteLength < bufferSize) {
@@ -30,8 +33,9 @@ export function* getFragmentGenerator(
       offset += byteLength;
     } else {
       // Cannot fit this value in buffer, send it directly.
-      // Buffer has just been flushed so we do not need to worry about
-      // out-of-order send.
+      // Buffer has just been flushed (since the condition `byteLength + offset > bufferSize`
+      // will always be true here, which will have triggered the flush above) so
+      // we do not need to worry about out-of-order send.
       yield value;
     }
   }
