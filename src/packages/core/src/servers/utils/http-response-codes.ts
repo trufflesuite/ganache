34789aa9593c330881126290bed130c332fabf26
David Murdoch <david@davidmurdoch.com>
2022-07-22 11:53:39 -0400

    add a comment in lieu of tests
    we'd have to write a mock for this, as JS libraries parse the Status Line loosly; omitting the " " doesn't matter.
    
    It seems like a lot of work to write a mock to test for the space character. So I wrote this comment instead. Please forgive me.

--- src/packages/core/src/servers/utils/http-response-codes.ts
+++ src/packages/core/src/servers/utils/http-response-codes.ts
@@ -1,3 +1,19 @@
+/**
+ * HTTP/1.1 Response Status-Codes, including the _required_ space character.
+ *
+ * e.g., `"200 "` or `"404 "`
+ *
+ * RFC Grammar:
+ *
+ * ```ebnf
+ * Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+ * ```
+ *
+ * The Status-Codes defined here fullfill the `Status-Code SP` part of the above
+ * grammar.
+ *
+ * See https://datatracker.ietf.org/doc/html/rfc2616#section-6.1 for details.
+ */
 enum HttpResponseCodes {
   OK = "200 ",
   NO_CONTENT = "204 ",
