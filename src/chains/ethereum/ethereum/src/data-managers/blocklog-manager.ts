5fbf238838baa9dae400f510006aa140195fa179
Alex Sherbuck <AlexSherbuck@gmail.com>
2022-08-03 22:18:46 -0500

    Update src/chains/ethereum/ethereum/src/data-managers/blocklog-manager.ts
    Co-authored-by: Micaiah Reid <micaiahreid@gmail.com>

--- src/chains/ethereum/ethereum/src/data-managers/blocklog-manager.ts
+++ src/chains/ethereum/ethereum/src/data-managers/blocklog-manager.ts
@@ -66,7 +66,6 @@ export default class BlockLogManager extends Manager<BlockLogs> {
           // TODO(perf): this loops over all addresses for every block.
           // Maybe make it loop only once?
           // Issue: https://github.com/trufflesuite/ganache/issues/3482
-
           if (blockLogs)
             filteredBlockLogs.push(...blockLogs.filter(addresses, topics));
         });
