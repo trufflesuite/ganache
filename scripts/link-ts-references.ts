beee9523f760c45de32cd73867e7faad3da681ec
David Murdoch <david@davidmurdoch.com>
2022-08-18 15:54:02 -0400

    ensure link-ts-references script prettiers files after writing them

--- scripts/link-ts-references.ts
+++ scripts/link-ts-references.ts
@@ -5,6 +5,7 @@
 
 import { readFileSync, existsSync, writeFileSync } from "fs-extra";
 import { resolve, join, relative, sep } from "path";
+import prettier from "prettier";
 
 // using `require` because everything in scripts uses typescript's default
 // compiler settings, and these two modules require enabling `esModuleInterop`
@@ -133,11 +134,20 @@ function updateConfigs(configs: PackageInfo[]) {
   configs.forEach(updateConfig);
 }
 
-function saveConfigs(configs: PackageInfo[]) {
+async function saveConfigs(configs: PackageInfo[]) {
+  const prettierConfig = await prettier.resolveConfig(process.cwd());
   configs.forEach(({ modified, path, tsConfig }) => {
     if (modified) {
       const tsConfigFile = join(path, "tsconfig.json");
-      writeFileSync(tsConfigFile, JSON5.stringify(tsConfig, null, 2));
+      writeFileSync(
+        tsConfigFile,
+        prettier.format(JSON5.stringify(tsConfig, null, 2), {
+          ...prettierConfig,
+          // neccessary as json5's default is to strip quotes
+          quoteProps: "preserve",
+          parser: "json5"
+        })
+      );
     }
   });
 }
