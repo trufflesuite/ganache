0cd4d7f0925c8bdc08078810eb3e2bbd5178d351
MicaiahReid <micaiahreid@gmail.com>
2022-08-25 09:21:37 -0400

    add pending block check to eth_getStorageAt test

--- src/chains/ethereum/ethereum/tests/api/eth/getStorageAt.test.ts
+++ src/chains/ethereum/ethereum/tests/api/eth/getStorageAt.test.ts
@@ -9,8 +9,13 @@ describe("api", () => {
     describe("getStorageAt", () => {
       let provider: EthereumProvider;
       let contract: CompileOutput;
-      let contractAddress: string;
+      // we can preset the contract address because we have the wallet in
+      // deterministic mode. this allows us to get the storage at the contract
+      // address for the pending block before the contract is actually
+      // deployed
+      let contractAddress = "0xe78a0f7e598cc8b0bb87894b0f60dd2a88d6a8ab";
       let contractMethods: any;
+      let pendingStorage0: string, pendingStorage1: string;
 
       before(() => {
         contract = compile(join(__dirname, "./contracts/GetStorageAt.sol"));
@@ -18,11 +23,13 @@ describe("api", () => {
 
       beforeEach(async () => {
         provider = await getProvider({
-          miner: { defaultTransactionGasLimit: 6721975 }
+          miner: { defaultTransactionGasLimit: 6721975 },
+          wallet: { deterministic: true }
         });
         const accounts = await provider.send("eth_accounts");
         const from = accounts[0];
 
+        await provider.send("miner_stop");
         await provider.send("eth_subscribe", ["newHeads"]);
 
         const transactionHash = await provider.send("eth_sendTransaction", [
@@ -31,14 +38,28 @@ describe("api", () => {
             data: contract.code
           }
         ]);
-
+        pendingStorage0 = await provider.send("eth_getStorageAt", [
+          contractAddress,
+          "0x0",
+          "pending"
+        ]);
+        pendingStorage1 = await provider.send("eth_getStorageAt", [
+          contractAddress,
+          "0x1",
+          "pending"
+        ]);
+        await provider.send("miner_start");
         await provider.once("message");
 
         const receipt = await provider.send("eth_getTransactionReceipt", [
           transactionHash
         ]);
 
-        contractAddress = receipt.contractAddress;
+        assert.strictEqual(
+          contractAddress,
+          receipt.contractAddress,
+          "Deployed contract address is not the expected address."
+        );
         contractMethods = contract.contract.evm.methodIdentifiers;
       });
 
@@ -55,6 +76,19 @@ describe("api", () => {
         assert.strictEqual(
           result2,
           "0x0000000000000000000000000000000000000000000000000000000000000001"
+        );
+      });
+
+      it("returns the value at the hex position when 'pending' tag is passed, before the contract is deployed", async () => {
+        assert.strictEqual(
+          BigInt(pendingStorage0),
+          123n,
+          "Unexpected storage value for key 0 in pending block."
+        );
+        assert.strictEqual(
+          pendingStorage1,
+          "0x01",
+          "Unexpected storage value for key 1 in pending block."
         );
       });
 
