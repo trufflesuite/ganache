9c04422fa948f668997079fb68924c6df62eb2b3
MicaiahReid <micaiahreid@gmail.com>
2022-09-14 20:28:32 -0400

    simplify type

--- src/chains/ethereum/ethereum/src/leveldown-to-level.ts
+++ src/chains/ethereum/ethereum/src/leveldown-to-level.ts
@@ -5,25 +5,28 @@ import type { LevelUp } from "levelup";
 import { LEVEL_OPTIONS } from "./database";
 const levelup = require("levelup");
 
+type GanacheLevelDown = AbstractLevelDOWN<Buffer, Buffer>;
 export class UpgradedLevelDown {
-  public db: LevelUp<AbstractLevelDOWN<string | Buffer, string | Buffer>> =
-    null;
-  #rootStore: AbstractLevelDOWN<string | Buffer, string | Buffer>;
-  constructor(store: AbstractLevelDOWN<string | Buffer, string | Buffer>) {
+  public db: LevelUp<GanacheLevelDown> = null;
+  #rootStore: GanacheLevelDown;
+
+  constructor(store: GanacheLevelDown) {
     this.#rootStore = store;
     this.db = levelup(store, LEVEL_OPTIONS);
   }
+
   get location() {
     console.log("LOCATION RETRIEVED");
     return "";
   }
+
   async open(): Promise<void> {
     await this.db.open();
   }
   async get(key: Buffer): Promise<Buffer | null> {
     let val = null;
     try {
-      val = (await this.db.get(key)) as Buffer;
+      val = await this.db.get(key);
     } catch (error: any) {
       if (!error.notFound) {
         throw error;
