ea0cc48c229e2a6f6c2ede390b741f1fcfd65d2d
Alex Sherbuck <AlexSherbuck@gmail.com>
2022-10-03 08:37:03 -0500

    Update src/packages/version-check/tests/ci.test.ts
    Co-authored-by: David Murdoch <187813+davidmurdoch@users.noreply.github.com>

--- src/packages/version-check/tests/ci.test.ts
+++ src/packages/version-check/tests/ci.test.ts
@@ -5,7 +5,6 @@ import { detectCI } from "../src/ci";
 import assert from "assert";
 
 describe("detectCI", () => {
-  beforeEach(() => {});
   it("returns true if envVar is found", () => {
     process.env.TRUFFLE_SHUFFLE_TEST = true;
     assert.equal(detectCI(), true);
