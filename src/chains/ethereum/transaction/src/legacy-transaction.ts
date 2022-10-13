795d1fa25e75f97381d4cc0c10298efca960fe54
MicaiahReid <micaiahreid@gmail.com>
2022-10-13 15:05:05 -0400

    fix transaction to address defaults

--- src/chains/ethereum/transaction/src/legacy-transaction.ts
+++ src/chains/ethereum/transaction/src/legacy-transaction.ts
@@ -75,7 +75,7 @@ export class LegacyTransaction extends RuntimeTransaction {
       blockNumber: this.blockNumber ? this.blockNumber : null,
       transactionIndex: this.index ? this.index : null,
       from: this.from,
-      to: this.to.isNull() ? null : this.to,
+      to: this.to ? this.to : null,
       value: this.value,
       gas: this.gas,
       gasPrice: this.gasPrice,
