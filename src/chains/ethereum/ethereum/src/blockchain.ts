9aaf55ab6fa546acf0d68b865849824a983846cd
MicaiahReid <micaiahreid@gmail.com>
2022-10-13 13:45:46 -0400

    remove errant commit

--- src/chains/ethereum/ethereum/src/blockchain.ts
+++ src/chains/ethereum/ethereum/src/blockchain.ts
@@ -1090,7 +1090,6 @@ export default class Blockchain extends Emittery<BlockchainTypedEvents> {
     // simulating the tx, because `runCall` doesn't account for raw gas costs.
     const hasToAddress = transaction.to != null;
     const to = hasToAddress ? new Address(transaction.to.toBuffer()) : null;
-    const test: Address = Address.from(transaction.to.toString());
 
     const common = this.fallback
       ? this.fallback.getCommonForBlockNumber(
