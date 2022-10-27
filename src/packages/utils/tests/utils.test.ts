92ffeb39c425366bc3aa94d4bf1d82b9ef80a7a1
tenthirtyone <alexsherbuck@gmail.com>
2022-10-26 19:43:49 -0500

    add unit tests

--- src/packages/utils/tests/utils.test.ts
+++ src/packages/utils/tests/utils.test.ts
@@ -1,6 +1,6 @@
 import assert from "assert";
 import sinon from "sinon";
-import { min, max } from "../";
+import { min, max, findInsertPosition } from "../";
 
 const BIGINT_ERROR =
   "bigint: Failed to load bindings, pure JS will be used (try npm run rebuild?)";
@@ -41,6 +41,98 @@ describe("@ganache/utils", () => {
       assert.strictEqual(max(...ascending), ascending[ascending.length - 1]);
       assert.strictEqual(max(...descending), descending[0]);
       assert.strictEqual(max(...mixedTypes), mixedTypes[mixedTypes.length - 1]);
+    });
+  });
+
+  /*
+   * Nice piece of custom you got there, where'd yah get it?
+   */
+  describe("find-insert-position", () => {
+    const compareAscendingBoolean = (a: number, b: number): boolean => a < b;
+    const compareAscendingNumber = (a: number, b: number): number => a - b;
+
+    const totalNumbersToTest = 1000;
+    const numbersToTest = Array.from({ length: totalNumbersToTest }).map(_ =>
+      Math.round(Math.random() * 10000)
+    );
+
+    function stockSortAndFilter(startingNumber: number, maxResult: number) {
+      const keys: number[] = [];
+      numbersToTest.forEach(number => {
+        // ignore anything that comes before our starting point
+        if (startingNumber > number) return;
+
+        // #4 - sort and filter keys
+        // insert the key exactly where it needs to go in the array
+        const position = findInsertPosition(
+          keys,
+          number,
+          compareAscendingBoolean
+        );
+        // ignore if the value couldn't possibly be relevant
+        if (position > maxResult) return;
+        keys.splice(position, 0, number);
+      });
+      return keys;
+    }
+
+    function stockRangeAt(startingNumber: number, maxResult: number) {
+      return stockSortAndFilter(startingNumber, maxResult).slice(0, maxResult);
+    }
+
+    function customRangeAt(startingNumber: number, maxResult: number) {
+      return customSortAndFilter(startingNumber, maxResult).slice(0, maxResult);
+    }
+    function customSortAndFilter(startingNumber: number, maxResult: number) {
+      const keys: number[] = [];
+      numbersToTest.forEach(number => {
+        // ignore anything that comes before our starting point
+        if (startingNumber > number) return;
+
+        // #4 - sort and filter keys
+        // insert the key exactly where it needs to go in the array
+        const position = findInsertPosition(
+          keys,
+          number,
+          compareAscendingBoolean
+        );
+        // ignore if the value couldn't possibly be relevant
+        if (position > maxResult) return;
+        // This is the only line that changes
+        if (keys.length === maxResult && number > keys[maxResult - 1]) return;
+        keys.splice(position, 0, number);
+      });
+      return keys;
+    }
+
+    it("sorts an array of numbers as Array.sort with a comparable compare", () => {
+      const numbers: number[] = [];
+
+      numbersToTest.forEach(number => {
+        const position = findInsertPosition(
+          numbers,
+          number,
+          compareAscendingBoolean
+        );
+
+        numbers.splice(position, 0, number);
+      });
+
+      assert.deepStrictEqual(
+        numbers,
+        numbersToTest.sort(compareAscendingNumber)
+      );
+    });
+    it("stockRangeAt range test", () => {
+      const min = 5000;
+      const max = 5;
+
+      const stockStorage = stockRangeAt(min, max);
+
+      const customStorage = customRangeAt(min, max);
+      console.log(stockStorage);
+      console.log(customStorage);
+      assert.deepStrictEqual(stockStorage, customStorage);
     });
   });
 });
