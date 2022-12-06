3ffe434b97a83b93e7e554ae3f5ac676680f68fb
MicaiahReid <micaiahreid@gmail.com>
2022-12-05 22:27:11 -0500

    add test for truncating address

--- src/chains/ethereum/address/tests/index.test.ts
+++ src/chains/ethereum/address/tests/index.test.ts
@@ -13,6 +13,18 @@ describe("@ganache/ethereum-address", () => {
       );
     });
 
+    it("should truncate an address to 20 bytes", () => {
+      const address = Address.from(
+        "0x210485939460435937843386536094711670787612" // it's got an extra byte
+      );
+      const stringifiedAddress = address.toString();
+
+      assert.strictEqual(
+        stringifiedAddress,
+        "0x2104859394604359378433865360947116707876"
+      );
+    });
+
     it("should stringify a 20 byte address string", () => {
       const address = Address.from(
         "0x2104859394604359378433865360947116707876"
@@ -45,6 +57,18 @@ describe("@ganache/ethereum-address", () => {
       assert.deepStrictEqual(bufferAddress, expected);
     });
 
+    it("should truncate an address to 20 bytes", () => {
+      const address = Address.from(
+        "0x210485939460435937843386536094711670787612" // it's got an extra byte
+      );
+      const bufferAddress = address.toBuffer();
+      const expected = Buffer.from([
+        0x21, 0x04, 0x85, 0x93, 0x94, 0x60, 0x43, 0x59, 0x37, 0x84, 0x33, 0x86,
+        0x53, 0x60, 0x94, 0x71, 0x16, 0x70, 0x78, 0x76
+      ]);
+
+      assert.deepStrictEqual(bufferAddress, expected);
+    });
     it("should convert a 20 byte address to a buffer", () => {
       const address = Address.from(
         "0x2104859394604359378433865360947116707876"
