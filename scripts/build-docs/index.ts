08d411aa65bc16024ee5ff54331d15a17e601f41
MicaiahReid <micaiahreid@gmail.com>
2022-12-14 11:00:51 -0500

    remove CI check. add try/catch

--- scripts/build-docs/index.ts
+++ scripts/build-docs/index.ts
@@ -258,12 +258,13 @@ function renderTag(method: Method, tag: Tag, i: number) {
 function renderSource(method: Method) {
   const source = method.sources[0];
   let branch = "master";
-  if (!process.env.CI) {
+  try {
     branch =
       execSync("git rev-parse HEAD", {
         encoding: "utf8"
       }).trim() || "master";
-  }
+  } catch (e) {}
+
   return `<a href="https://github.com/trufflesuite/ganache/blob/${branch}/src/chains/ethereum/${source.fileName}#L${source.line}" target="_blank" rel="noopener">source</a>`;
 }
 
