af3738216d3fa92f7a9c246db767827a0d0807b9
David Murdoch <david@davidmurdoch.com>
2022-12-15 17:14:15 -0500

    add EADDRINUSE code to server Error

--- src/packages/core/src/server.ts
+++ src/packages/core/src/server.ts
@@ -245,7 +245,9 @@ export class Server<
             `listen EADDRINUSE: address already in use ${
               host || DEFAULT_HOST
             }:${portNumber}.`
-          );
+          ) as any;
+          // emulate part of node's EADDRINUSE error:
+          err.code = "EADDRINUSE";
           throw err;
         }
       })
