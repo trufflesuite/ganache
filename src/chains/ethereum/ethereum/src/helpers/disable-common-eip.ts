aae62bfbeaaf44184598b3a9ea444f4b57b38417
David Murdoch <david@davidmurdoch.com>
2023-03-16 12:50:10 -0400

    gotta stop mutating things that arne't mind to mutate

--- src/chains/ethereum/ethereum/src/helpers/disable-common-eip.ts
+++ src/chains/ethereum/ethereum/src/helpers/disable-common-eip.ts
@@ -17,13 +17,15 @@ export function disableCommonEip(
     const [name, spec] = changes[i];
     if (name === hardfork) {
       if (spec.eips) {
+        const newSpec = JSON.parse(JSON.stringify(spec)) as typeof spec;
         // remove the EIP from the list of EIPs for the hardfork
-        const newEips = [];
-        for (let i = 0, l = spec.eips.length; i < l; i++) {
-          const eip = spec.eips[i];
-          if (eip !== eipToRemove) newEips.push(eip);
+        // by splicing `eipToRemove` from the `newSpec.eips` array:
+        const index = newSpec.eips.indexOf(eipToRemove);
+        if (index !== -1) {
+          newSpec.eips.splice(index, 1);
         }
-        spec.eips = newEips;
+
+        changes[i][1] = newSpec;
       }
       // we found the hardfork, so we can stop iterating
       break;
