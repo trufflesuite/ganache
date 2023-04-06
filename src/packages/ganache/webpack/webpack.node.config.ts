80210bf0396828f6bc91edab8636c906ae96c46e
David Murdoch <david@davidmurdoch.com>
2023-04-05 20:25:16 -0400

    remove more ethers

--- src/packages/ganache/webpack/webpack.node.config.ts
+++ src/packages/ganache/webpack/webpack.node.config.ts
@@ -22,8 +22,9 @@ const config: webpack.Configuration = merge({}, base, {
       debug: require.resolve("./polyfills/debug"),
       // the `setimmediate` package is only used in the browser
       setimmediate: false,
-      // TODO: remove once https://github.com/trufflesuite/ganache/issues/3899 is resolved
-      ethers: false
+      // TODO: remove these ethers related aliases once https://github.com/trufflesuite/ganache/issues/3899 is resolved
+      ethers: false,
+      "@ethersproject/providers": false
     }
   },
   plugins: [
