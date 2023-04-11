8428d5f8867bf662652905269648c4ffe89422cf
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2023-04-11 10:29:24 -0400

    Update src/chains/ethereum/block/src/block.ts
    Co-authored-by: jeffsmale90 <6363749+jeffsmale90@users.noreply.github.com>

--- src/chains/ethereum/block/src/block.ts
+++ src/chains/ethereum/block/src/block.ts
@@ -161,7 +161,7 @@ export class Block {
       transactions,
       uncles: [] as Data[], // this.value.uncleHeaders.map(function(uncleHash) {return to.hex(uncleHash)})
       // if `this._rawWithdrawals` is not set we should not include it in the
-      // JSON response ()`undefined` gets stripped when JSON.stringify is called).
+      // JSON response (`undefined` gets stripped when JSON.stringify is called).
       withdrawals: this._rawWithdrawals?.map(convertRawWithdrawals) || undefined
     };
   }
