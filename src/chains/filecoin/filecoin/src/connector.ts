d40f30a5370a7de61256799681dcb260e54a6a80
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2023-04-14 20:56:16 -0400

    Update src/chains/filecoin/filecoin/src/connector.ts

--- src/chains/filecoin/filecoin/src/connector.ts
+++ src/chains/filecoin/filecoin/src/connector.ts
@@ -35,7 +35,7 @@ export class Connector<
   }
 
   constructor(
-    providerOptions: FilecoinProviderOptions | null = {},
+    providerOptions: FilecoinProviderOptions = {},
     executor: Executor
   ) {
     this.#provider = new FilecoinProvider(providerOptions, executor);
