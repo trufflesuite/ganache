295ab1a6fb9b1f58a00a7df3a375d6d41383d8d8
Jeff Smale <6363749+jeffsmale90@users.noreply.github.com>
2023-04-17 14:30:00 +1200

    New line at end of tsconfig files

--- scripts/link-ts-references.ts
+++ scripts/link-ts-references.ts
@@ -5,6 +5,7 @@
 
 import { readFileSync, existsSync, writeFileSync } from "fs-extra";
 import { resolve, join, relative, sep } from "path";
+import { EOL } from "os";
 
 // using `require` because everything in scripts uses typescript's default
 // compiler settings, and these two modules require enabling `esModuleInterop`
@@ -93,12 +94,12 @@ function updateConfig(config: PackageInfo) {
   // add package.json deps to tsconfig references:
   references.forEach(name => {
     const referenceConfig = getConfigByName(name);
-    if(!referenceConfig) throw new Error(`missing config ${name}`);
+    if (!referenceConfig) throw new Error(`missing config ${name}`);
 
     // projects that are referenced by other projects must have the `composite: true` in their tsconfig compileOptions
     if (
-      (!referenceConfig.tsConfig.compilerOptions ||
-      !referenceConfig.tsConfig.compilerOptions.composite)
+      !referenceConfig.tsConfig.compilerOptions ||
+      !referenceConfig.tsConfig.compilerOptions.composite
     ) {
       if (!referenceConfig.tsConfig.compilerOptions)
         referenceConfig.tsConfig.compilerOptions = {};
@@ -137,7 +138,8 @@ function saveConfigs(configs: PackageInfo[]) {
   configs.forEach(({ modified, path, tsConfig }) => {
     if (modified) {
       const tsConfigFile = join(path, "tsconfig.json");
-      writeFileSync(tsConfigFile, JSON5.stringify(tsConfig, null, 2));
+      const body = JSON5.stringify(tsConfig, null, 2) + EOL;
+      writeFileSync(tsConfigFile, body);
     }
   });
 }
