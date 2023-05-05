b8b9d31469314eb79c288b565e5e2c9ba7f9fa58
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2023-05-05 11:40:50 -0400

    we don't allow old flavors anymore, so only support `ready`

--- src/packages/cli/src/cli.ts
+++ src/packages/cli/src/cli.ts
@@ -149,12 +149,7 @@ if (argv.action === "start") {
     }
     started = true;
 
-    if (flavor.ready) {
-      await flavor.ready(server.provider, cliSettings);
-    } else if ("initialized" in flavor) {
-      // @ts-ignore old filecoin used `initialized` instead of `ready`
-      await flavor.initialized(server.provider, cliSettings);
-    }
+    await flavor.ready(server.provider, cliSettings);
 
     // if process.send is defined, this is a child_process (we assume a detached
     // instance), so we need to notify that we are ready.
