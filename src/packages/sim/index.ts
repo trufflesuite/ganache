150bbf071efb4cc20faa51c3399f8bbeca45480a
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2023-06-16 17:07:07 -0400

    change sim port to 9009

--- src/packages/sim/index.ts
+++ src/packages/sim/index.ts
@@ -81,6 +81,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${3000}`);
+server.listen(9009, () => {
+  console.log(`Server is running on http://localhost:${9009}`);
 });
