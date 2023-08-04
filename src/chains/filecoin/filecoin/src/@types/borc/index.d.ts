5a1ac6c3a9f6f7aa66cad339ff61aeafee2567fa
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2023-08-04 17:36:17 -0400

    chore: remove filecoin and tezos from monorepo (#4466)

--- src/chains/filecoin/filecoin/src/@types/borc/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "borc" {
-  type Borc = {
-    encode(obj: any): Buffer;
-  };
-
-  let cbor: Borc;
-
-  export default cbor;
-}
--- src/chains/filecoin/filecoin/src/types/subscriptions.ts
+++
@@ -1,7 +0,0 @@
-export enum SubscriptionMethod {
-  ChannelUpdated = "xrpc.ch.val",
-  ChannelClosed = "xrpc.ch.close",
-  SubscriptionCanceled = "xrpc.cancel"
-}
-
-export type SubscriptionId = string;
